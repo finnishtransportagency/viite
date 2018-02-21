d672fa2faa0f55f9cc3ad6d1c6bff381ebc8b7d7
Lourenço <pedro.m.lourenco@cgi.com>
2018-02-21 12:02:58 +0200

    VIITE-1135 java increase memory to apply changes

--- fixture-reset.sh
+++ fixture-reset.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
-./sbt ${2} 'project digiroad2-oracle' "test:run-main fi.liikennevirasto.digiroad2.util.DataFixture ${1}"
+#!/usr/bin/env bash
+set -e
+JAVA_OPTS="-Xms1512M -Xmx4096m -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=1024M -XX:+HeapDumpOnOutOfMemoryError -javaagent:newrelic.jar"
+java $JAVA_OPTS -jar `dirname $0`/sbt-launch.jar ${2} 'project digiroad2-viite' "test:run-main fi.liikennevirasto.digiroad2.util.DataFixture ${1}"
