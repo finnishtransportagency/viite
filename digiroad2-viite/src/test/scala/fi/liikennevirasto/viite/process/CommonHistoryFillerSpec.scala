0325e9800539ebc53d8c8aea6acfd81825266102
Campanha <ricardo.campanha.marques@cgi.com>
2018-03-08 13:24:01 +0200

    VIITE-1171 Added spec class

---
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/process/CommonHistoryFillerSpec.scala
@@ -0,0 +1,11 @@
+package fi.liikennevirasto.viite.process
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+  * Created by marquesrf on 08-03-2018.
+  */
+class CommonHistoryFillerSpec extends FunSuite with Matchers {
+
+
+}
