4a1ad3baba3eb53d585678f83ebfd251c456378b
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2018-03-14 20:07:19 +0200

    First iteration of the road names list.
    Onwards it is needed to change the labels to input type.
    Add a save button (disabled) that gets enabled when an edition is detected.
    Add an road creation event to the "+" button.

--- viite-UI/src/view/RoadNamingToolWindow.js
+++ viite-UI/src/view/RoadNamingToolWindow.js
@@ -21,6 +21,24 @@
             '</div>' +
             '</div>');
 
+        nameToolSearchWindow.append('<div id="road-list" style="width:810px; height:400px; overflow:auto;"></div>');
+
+        var staticFieldRoadNumber = function (dataField) {
+            var field;
+            field = '<div>' +
+                '<label class="control-label-projects-list" style="width: 300px">' + dataField + '</label>' +
+                '</div>';
+            return field;
+        };
+
+        var staticFieldRoadList = function (dataField) {
+            var field;
+            field = '<div>' +
+                '<label class="control-label-projects-list">' + dataField + '</label>' +
+                '</div>';
+            return field;
+        };
+
         function toggle() {
             $('.container').append('<div class="modal-overlay confirm-modal"><div class="modal-dialog"></div></div>');
             $('.modal-dialog').append(nameToolSearchWindow.toggle());
@@ -51,6 +69,28 @@
             });
 
             eventbus.on("roadNameTool: roadsFetched", function (roadData) {
+                var html = '<table style="align-content: left;align-items: left;table-layout: fixed;width: 100%;">';
+                if (!_.isEmpty(roadData)) {
+                    _.each(roadData, function (road) {
+                        html += '<tr class="project-item">' +
+                            '<td style="width: 310px;">' + staticFieldRoadNumber(road.roadNumber) + '</td>' +
+                            '<td style="width: 110px;">' + staticFieldRoadList(road.roadNameFi) + '</td>' +
+                            '<td style="width: 110px;">' + staticFieldRoadList(road.startDate) + '</td>' +
+                            '<td style="width: 110px;">' + staticFieldRoadList(road.endDate) + '</td>';
+                        if (road.endDate === "") {
+                            html += '<td>' + '<button class="project-open btn btn-new" style="alignment: right; margin-bottom:6px; margin-left: 70px" id="new-road-name-' + road.roadNumber + '" value="' + road.roadNumber + '"">+</button>' + '</td>' +
+                                '</tr>' + '<tr style="border-bottom:1px solid darkgray; "><td colspan="100%"></td></tr>';
+                        } else {
+                            html += '<td>' + '<button class="project-open btn btn-new" style="visibility:hidden; alignment: right; margin-bottom:6px; margin-left: 70px" id="spaceFillerButton">+</button>' + '</td>' +
+                                '</tr>' + '<tr style="border-bottom:1px solid darkgray; "><td colspan="100%"></td></tr>';
+                        }
+                    });
+                    html += '</table>';
+                    $('#road-list').html($(html));
+                } else {
+                    html += '</table>';
+                    $('#road-list').html($(html));
+                }
                 alert("RoadData returned to UI");
             });
         }
