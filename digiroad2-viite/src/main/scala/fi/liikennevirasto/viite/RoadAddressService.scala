6556ea75c2aad84f6e0a49d613a70c965e40e5da
Lourenço <pedro.m.lourenco@cgi.com>
2018-03-22 12:55:46 +0200

    VIITE-1212 unit test

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/RoadAddressService.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/RoadAddressService.scala
@@ -602,7 +602,7 @@ class RoadAddressService(roadLinkService: RoadLinkService, eventbus: DigiroadEve
   }
 
   def mergeRoadAddressHistoryInTX(data: RoadAddressMerge): Unit = {
-    val unMergedCount = RoadAddressDAO.queryById(data.merged, includeHistory = true).count(_.endDate.nonEmpty)
+    val unMergedCount = RoadAddressDAO.queryById(data.merged).count(_.endDate.nonEmpty)
     if (unMergedCount != data.merged.size)
       throw new InvalidAddressDataException("Data modified while updating, rolling back transaction: some source rows no longer valid")
     val mergedCount = expireRoadAddresses(data.merged)
