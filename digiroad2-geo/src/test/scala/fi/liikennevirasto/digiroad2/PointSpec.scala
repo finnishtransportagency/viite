5a0dcd2503f2053fd6618bdcf9027f4e4d4050a8
Sami Kosonen <sami.kosonen@cgi.com>
2018-04-05 11:05:57 +0300

    Added tests for the angleXYWithNegativeValues

--- digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/PointSpec.scala
+++ digiroad2-geo/src/test/scala/fi/liikennevirasto/digiroad2/PointSpec.scala
@@ -6,6 +6,8 @@ class PointSpec extends FunSuite with Matchers {
 
   val tolerance = 0.00001
 
+  // Vector3d.angle
+
   test("Angle should be π / 2 (90 degrees) xy") {
     val vector1 = Vector3d(1, 0, 0)
     val vector2 = Vector3d(0, -1, 0)
@@ -46,6 +48,65 @@ class PointSpec extends FunSuite with Matchers {
     val vector2 = Vector3d(0, 0, -1)
     vector1.angle(vector2) should be(Math.toRadians(180) +- tolerance)
     vector2.angle(vector1) should be(Math.toRadians(180) +- tolerance)
+  }
+
+  // Vector3d.angleXYWithNegativeValues
+
+  test("angleXYWithNegativeValues should be π / 2 (90 degrees) xy") {
+    val vector1 = Vector3d(1, 0, 0)
+    val vector2 = Vector3d(0, -1, 0)
+    vector1.angleXYWithNegativeValues(vector2) should be(Math.toRadians(90) +- tolerance)
+  }
+
+  test("angleXYWithNegativeValues should be -π / 2 (-90 degrees) xy") {
+    val vector1 = Vector3d(1, 0, 0)
+    val vector2 = Vector3d(0, -1, 0)
+    vector2.angleXYWithNegativeValues(vector1) should be(Math.toRadians(-90) +- tolerance)
+  }
+
+  test("angleXYWithNegativeValues should be 0 xz") {
+    val vector1 = Vector3d(1, 0, 0)
+    val vector2 = Vector3d(0, 0, -1)
+    vector1.angleXYWithNegativeValues(vector2) should be(0.0 +- tolerance)
+    vector2.angleXYWithNegativeValues(vector1) should be(0.0 +- tolerance)
+  }
+
+  test("angleXYWithNegativeValues should be 0 yz") {
+    val vector1 = Vector3d(0, 1, 0)
+    val vector2 = Vector3d(0, 0, -1)
+    vector1.angleXYWithNegativeValues(vector2) should be(0.0 +- tolerance)
+    vector2.angleXYWithNegativeValues(vector1) should be(0.0 +- tolerance)
+  }
+
+  test("angleXYWithNegativeValues should be π (180 degrees) x") {
+    val vector1 = Vector3d(1, 0, 0)
+    val vector2 = Vector3d(-1, 0, 0)
+    vector2.angleXYWithNegativeValues(vector1) should be(Math.toRadians(180) +- tolerance)
+  }
+
+  test("angleXYWithNegativeValues should be -π (-180 degrees) x") {
+    val vector1 = Vector3d(1, 0, 0)
+    val vector2 = Vector3d(-1, 0, 0)
+    vector1.angleXYWithNegativeValues(vector2) should be(Math.toRadians(-180) +- tolerance)
+  }
+
+  test("angleXYWithNegativeValues should be π (180 degrees) y") {
+    val vector1 = Vector3d(0, 1, 0)
+    val vector2 = Vector3d(0, -1, 0)
+    vector1.angleXYWithNegativeValues(vector2) should be(Math.toRadians(180) +- tolerance)
+  }
+
+  test("angleXYWithNegativeValues should be -π (-180 degrees) y") {
+    val vector1 = Vector3d(0, 1, 0)
+    val vector2 = Vector3d(0, -1, 0)
+    vector2.angleXYWithNegativeValues(vector1) should be(Math.toRadians(-180) +- tolerance)
+  }
+
+  test("angleXYWithNegativeValues should be 0 z") {
+    val vector1 = Vector3d(0, 0, 1)
+    val vector2 = Vector3d(0, 0, -1)
+    vector1.angleXYWithNegativeValues(vector2) should be(0.0 +- tolerance)
+    vector2.angleXYWithNegativeValues(vector1) should be(0.0 +- tolerance)
   }
 
 }
