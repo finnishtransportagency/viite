b7e266016a97f11195a331807323f1b75fc453fb
leirasf <faustino.leiras@cgi.com>
2018-04-24 17:51:05 +0100

    VIITE 1291 Add done function to tests

--- viite-UI/test/integration-tests/RoadAddressProjectSpec.js
+++ viite-UI/test/integration-tests/RoadAddressProjectSpec.js
@@ -70,9 +70,10 @@ define(['chai', 'eventbus', 'TestHelpers'], function(chai, eventbus, testHelpers
 
     // 4-fourth -click in the next-Seuraava button
     describe('when clicking in next aka Seuraava button and select one reserved link', function() {
-      before(function () {
+      before(function (done) {
           var ol3Feature = testHelpers.getFeatureByLinkId(openLayersMap, testHelpers.getRoadAddressProjectLayerName(), 1717275);
           testHelpers.selectSingleFeatureByInteraction(openLayersMap, ol3Feature, testHelpers.getSingleClickNameProjectLinkLayer());
+          done();
       });
 
       it('Check if the project link was selected ', function(){
