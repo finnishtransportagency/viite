7ef804166a9e0804ac2dda9db117247bf8725ed2
Sami Kosonen <sami.kosonen@cgi.com>
2018-05-09 14:07:16 +0300

    VIITE-1324 Fixed TestServer

--- src/test/scala/fi/liikennevirasto/digiroad2/TestServer.scala
+++ src/test/scala/fi/liikennevirasto/digiroad2/TestServer.scala
@@ -3,14 +3,13 @@ package fi.liikennevirasto.digiroad2
 import org.eclipse.jetty.webapp.WebAppContext
 
 object TestServer extends App with DigiroadServer {
-  override val contextPath: String = "/"
   override val viiteContextPath: String = "/viite"
 
-  override def setupWebContext(): WebAppContext ={
-    val context = super.setupWebContext()
+  override def createViiteContext(): WebAppContext = {
+    val context = super.createViiteContext()
+    context.addServlet(classOf[ViiteTierekisteriMockApi], "/trrest/*")
     context
   }
 
   startServer()
 }
-
