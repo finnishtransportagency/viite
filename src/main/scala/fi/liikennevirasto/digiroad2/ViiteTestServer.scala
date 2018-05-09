517a5e1effb638f441df50f81fb6a1d278931564
Sami Kosonen <sami.kosonen@cgi.com>
2018-05-09 13:01:00 +0300

    VIITE-1324 Removed Vionice. Removed OTH web.xml and context.

--- src/main/scala/fi/liikennevirasto/digiroad2/ViiteTestServer.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/ViiteTestServer.scala
@@ -3,14 +3,13 @@ package fi.liikennevirasto.digiroad2
 import org.eclipse.jetty.webapp.WebAppContext
 
 object ViiteTestServer extends App with DigiroadServer {
-  override val contextPath: String = "/"
   override val viiteContextPath: String = "/viite"
 
-    override def setupWebContext(): WebAppContext ={
-      val context = super.setupWebContext()
-      context.addServlet(classOf[ViiteTierekisteriMockApi], "/trrest/*")
-      context
-    }
+  override def createViiteContext(): WebAppContext = {
+    val context = super.createViiteContext()
+    context.addServlet(classOf[ViiteTierekisteriMockApi], "/trrest/*")
+    context
+  }
 
   startServer()
 }
