a1faff2a8f448629d3504d37ef8833f7383bcea2
Sami Kosonen <sami.kosonen@cgi.com>
2018-05-16 13:34:15 +0300

    VIITE-1316 Formatted and refactored a bit.

--- viite-UI/src/utils/LinkValues.js
+++ viite-UI/src/utils/LinkValues.js
@@ -83,7 +83,7 @@
         ErroredInTR:                {value: 3, description: "Virhe tierekisterissä"},
         TRProcessing:               {value: 4, description: "Tierekisterissä käsittelyssä"},
         Saved2TR:                   {value: 5, description: "Viety tierekisteriin"},
-      ErrorInViite: {value: 8, description: "Virhe viitteessä"},
+        ErrorInViite:               {value: 8, description: "Virhe Viite-sovelluksessa"},
         Unknown:                    {value: 99, description: "Tuntematon"}
     };
 
