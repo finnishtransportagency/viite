a7bb4bda354378d098e8b40ad3365f3433e7aa08
hestrompa <hugo.estrompa@cgi.com>
2018-05-22 13:00:44 +0300

    Using full list of links to see connections on partition

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/model/ProjectLinkPartitioner.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/model/ProjectLinkPartitioner.scala
@@ -20,29 +20,29 @@ object ProjectLinkPartitioner extends GraphPartitioner {
     val (outside, inProject) = links.partition(_.status == LinkStatus.Unknown)
     val inProjectGroups = inProject.groupBy(l => (l.status, l.roadNumber, l.roadPartNumber, l.trackCode, l.roadType))
     val (outsideWithRoadName, outsideWithoutRoadName) = outside.partition(link => link.VVHRoadName.get != "none")
-    val groupedUnnamedRoads = groupRoadsWithoutName(Seq(), Seq(), outsideWithoutRoadName)
+    val groupedUnnamedRoads = groupRoadsWithoutName(Seq(), Seq(), outsideWithoutRoadName, outsideWithoutRoadName)
     val outsideGroup = outsideWithRoadName.groupBy(link => (link.roadLinkSource, link.partitioningName))
     val clusters = for (linkGroup <- inProjectGroups.values.toSeq ++ outsideGroup.values.toSeq;
                         cluster <- clusterLinks(linkGroup, fi.liikennevirasto.viite.MaxDistanceForConnectedLinks)) yield cluster
     clusters.map(linksFromCluster) ++ splitGroups.values.toSeq ++ groupedUnnamedRoads
   }
 
-  def groupRoadsWithoutName[T <: ProjectAddressLinkLike](ready: Seq[Seq[T]], prepared: Seq[T],  unprocessed: Seq[T]): Seq[Seq[T]] = {
+  def groupRoadsWithoutName[T <: ProjectAddressLinkLike](ready: Seq[Seq[T]], prepared: Seq[T],  unprocessed: Seq[T], allLinks: Seq[T]): Seq[Seq[T]] = {
     if(unprocessed.isEmpty){
       ready ++ Seq(prepared)
     }
 
     else if(prepared.isEmpty){
       val initialLink = findNotConnectedLink(unprocessed).getOrElse(unprocessed.head)
-      groupRoadsWithoutName(ready, Seq(initialLink), unprocessed.filterNot(_.linkId == initialLink.linkId))
+      groupRoadsWithoutName(ready, Seq(initialLink), unprocessed.filterNot(_.linkId == initialLink.linkId), allLinks)
     }
     else{
-      val linksConnectedToPrepared = unprocessed.filter(link => GeometryUtils.areAdjacent(link.geometry, prepared.last.geometry))
+      val linksConnectedToPrepared = allLinks.filterNot(link => prepared.map(_.linkId).contains(link.linkId)).filter(link => GeometryUtils.areAdjacent(link.geometry, prepared.last.geometry))
       if(linksConnectedToPrepared.lengthCompare(1) == 0){
-        groupRoadsWithoutName(ready, prepared ++ Seq(linksConnectedToPrepared.head), unprocessed.filterNot(_.linkId == linksConnectedToPrepared.head.linkId))
+        groupRoadsWithoutName(ready, prepared ++ Seq(linksConnectedToPrepared.head), unprocessed.filterNot(_.linkId == linksConnectedToPrepared.head.linkId), allLinks)
       }
       else {
-        groupRoadsWithoutName(ready ++ Seq(prepared), Seq(), unprocessed)
+        groupRoadsWithoutName(ready ++ Seq(prepared), Seq(), unprocessed, allLinks)
       }
     }
   }
