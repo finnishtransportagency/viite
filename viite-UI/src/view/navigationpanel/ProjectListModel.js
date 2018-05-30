05cd424449c07ce975fbb29a0a6823820a8cfe14
Guilherme Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2018-05-30 18:26:46 +0300

    VIITE-1262 - Projektilistalle painike "Näytä vain omat projektit" / "Näytä vain omat projektit" (Show only own projects) button to the project list
    Modified slightly the backend for getRoles in order to also return the username.
    Said username will be extremely useful in order to filter by the current session user immediately.
    Modified ProjectListModel in order to add a filter to the project list by the current session user.
    Filter is acessed by clicking "Käyttäjä" once the project lists are loaded.

--- viite-UI/src/view/navigationpanel/ProjectListModel.js
+++ viite-UI/src/view/navigationpanel/ProjectListModel.js
@@ -7,7 +7,10 @@
     projectList.append('<div class="content-new">' +
       '<label class="content-new label">PROJEKTIN NIMI</label>' +
       '<label class="content-new label" style="width: 100px">ELY</label>' +
-      '<label class="content-new label" style="width: 100px">KÄYTTÄJÄ</label>' +
+        '<label id="userSearch" class="content-new label" style="width: 100px">KÄYTTÄJÄ</label>' +
+        '<span class="smallPopupContainer" id="userFilterSpan" style="display:none">' +
+        '<input type="text" id="userNameBox" placeholder="Käyttäjätunnus">' +
+        '</span>' +
       '<label class="content-new label" style="width: 100px">TILA</label>' +
       '<div class="actions">' +
     '<button class="new btn btn-primary" style="margin-top:-5px;">Uusi tieosoiteprojekti</button></div>' +
@@ -48,6 +51,43 @@
       projectCollection.getProjects();
     }
 
+    var filterByUser = function () {
+      var input = $('#userNameBox').val();
+      var rows = $('#project-list').find('tr');
+      if (input === "") {
+        rows.show();
+        return;
+      }
+      rows.hide();
+      var filteredRowIds = _.uniq(rows.filter(function () {
+        var $t = $(this);
+        return $t.find('#innerCreatedBy').find("label").text().toLowerCase().indexOf(input.toLowerCase()) !== -1;
+      }).map(function () {
+        var row = $(this);
+        return row.attr('id');
+      }));
+      rows.filter(function () {
+        var $t = $(this);
+        return _.contains(filteredRowIds, $t.attr('id'));
+      }).show();
+    };
+
+
+    var userFilterVisibility = function (showFilters) {
+      if (showFilters) {
+        $('#userFilterSpan').css("display", "block");
+        $('#userFilterSpan').css("visibility", "visible");
+        if ($('#userNameBox').val() === "") {
+          $('#userNameBox').val(applicationModel.getSessionUser());
+          filterByUser();
+        }
+      } else {
+        $('#userFilterSpan').css("display", "none");
+        $('#userFilterSpan').css("visibility", "hidden");
+
+      }
+    };
+
     function bindEvents() {
 
       eventbus.once('roadAddressProjects:fetched', function(projects) {
@@ -56,23 +96,25 @@
         });
         var html = '<table style="align-content: left; align-items: left; table-layout: fixed; width: 100%;">';
         if (!_.isEmpty(unfinishedProjects)) {
+          var uniqueId = 0;
           _.each(unfinishedProjects, function(proj) {
             var info = typeof(proj.statusInfo) !== "undefined" ? proj.statusInfo : 'Ei lisätietoja';
-              html += '<tr class="project-item">' +
-                '<td style="width: 310px;">' + staticFieldProjectName(proj.name) + '</td>' +
-                '<td style="width: 110px;" title="' + info + '">' + staticFieldProjectList(proj.ely) + '</td>' +
-                '<td style="width: 110px;" title="' + info + '">' + staticFieldProjectList(proj.createdBy) + '</td>' +
-                '<td style="width: 110px;" title="' + info + '">' + staticFieldProjectList(proj.statusDescription) + '</td>';
+            html += '<tr id="' + uniqueId + '" class="project-item">' +
+                '<td id="innerName" style="width: 310px;">' + staticFieldProjectName(proj.name) + '</td>' +
+                '<td id="innerEly" style="width: 110px;" title="' + info + '">' + staticFieldProjectList(proj.ely) + '</td>' +
+                '<td id="innerCreatedBy" style="width: 110px;" title="' + info + '">' + staticFieldProjectList(proj.createdBy) + '</td>' +
+                '<td id="innerStatus" style="width: 110px;" title="' + info + '">' + staticFieldProjectList(proj.statusDescription) + '</td>';
             if (proj.statusCode === projectStatus.ErrorInViite.value) {
               html += '<td>' + '<button class="project-open btn btn-new-error" style="alignment: right; margin-bottom: 6px; margin-left: 45px; visibility: hidden">Avaa uudelleen</button>' + '</td>' +
                   '</tr>' + '<tr style="border-bottom: 1px solid darkgray;"><td colspan="100%"></td></tr>';
             } else if (proj.statusCode === projectStatus.ErroredInTR.value) {
-              html += '<td>' + '<button class="project-open btn btn-new-error" style="alignment: right; margin-bottom: 6px; margin-left: 45px" id="reopen-project-' + proj.id + '" value="' + proj.id + '">Avaa uudelleen</button>' + '</td>' +
+              html += '<td id="innerOpenProjectButton">' + '<button class="project-open btn btn-new-error" style="alignment: right; margin-bottom: 6px; margin-left: 45px" id="reopen-project-' + proj.id + '" value="' + proj.id + '">Avaa uudelleen</button>' + '</td>' +
                 '</tr>' + '<tr style="border-bottom: 1px solid darkgray;"><td colspan="100%"></td></tr>';
             } else {
-              html += '<td>' + '<button class="project-open btn btn-new" style="alignment: right; margin-bottom: 6px; margin-left: 70px" id="open-project-' + proj.id + '" value="' + proj.id + '">Avaa</button>' + '</td>' +
-                '</tr>' + '<tr style="border-bottom: 1px solid darkgray;"><td colspan="100%"></td></tr>';
+              html += '<td id="innerOpenProjectButton">' + '<button class="project-open btn btn-new" style="alignment: right; margin-bottom: 6px; margin-left: 70px" id="open-project-' + proj.id + '" value="' + proj.id + '">Avaa</button>' + '</td>' +
+                  '</tr>' + '<tr id="' + uniqueId + '" style="border-bottom: 1px solid darkgray;"><td colspan="100%"></td></tr>';
             }
+            uniqueId = uniqueId + 1;
           });
           html += '</table>';
           $('#project-list').html($(html));
@@ -91,6 +133,10 @@
           $('#project-list').html($(html));
         }
         applicationModel.removeSpinner();
+        $('#userSearch').click(function () {
+          var spanIsInvisible = $('#userFilterSpan').css('display') === 'none';
+          userFilterVisibility(spanIsInvisible);
+        });
       });
 
       var openProjectSteps = function(event) {
@@ -116,8 +162,14 @@
       });
 
       projectList.on('click', 'button.close', function() {
+        userFilterVisibility(false);
+        $('#project-list').find('table').remove();
         $('.project-item').remove();
         hide();
+      });
+
+      $('#userNameBox').keyup(function () {
+        filterByUser();
       });
     }
 
