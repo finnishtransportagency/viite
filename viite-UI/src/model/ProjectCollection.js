4b5f3fc8f94979e72b789050656ac165d19c1177
tomixii <tomi.panula@cgi.com>
2018-06-07 13:24:41 +0300

    fixed if statement

--- viite-UI/src/model/ProjectCollection.js
+++ viite-UI/src/model/ProjectCollection.js
@@ -329,7 +329,7 @@
         roadEly: Number(roadAddressProjectForm.find('#ely')[0].value),
         roadLinkSource: Number(_.first(changedLinks).roadLinkSource),
         roadType: Number(roadAddressProjectForm.find('#roadTypeDropDown')[0].value),
-        userDefinedEndAddressM: (!isNaN(Number(roadAddressProjectForm.find('#endDistance')[0].value)) ?  Number(roadAddressProjectForm.find('#endDistance')[0].value) : null),
+        userDefinedEndAddressM: (!isNaN(roadAddressProjectForm.find('#endDistance')[0]) ?  Number(roadAddressProjectForm.find('#endDistance')[0].value) : null),
           coordinates: coordinates,
           roadName: roadAddressProjectForm.find('#roadName')[0].value
       };
