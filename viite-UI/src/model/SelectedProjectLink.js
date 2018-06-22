8abe7f7c3f21d4fdf2488bed200dcaf09cb3525f
leirasf <faustino.leiras@cgi.com>
2018-06-22 14:35:09 +0100

    VIITE-1390 Fix client side compile errors

--- viite-UI/src/model/SelectedProjectLink.js
+++ viite-UI/src/model/SelectedProjectLink.js
@@ -231,16 +231,15 @@
             eventbus.trigger('split:cutPointFeature', data.split, terminatedC);
         });
 
-      var isSplit = function () {
-        return get().length > 1 && !_.isUndefined(get()[0].connectedLinkId)
-      };
+        var isSplit = function () {
+            return get().length > 1 && !_.isUndefined(get()[0].connectedLinkId);
+        };
 
-      var isMultiLink = function () {
-        return get().length > 1 && _.isUndefined(get()[0].connectedLinkId)
-      };
+        var isMultiLink = function () {
+            return get().length > 1 && _.isUndefined(get()[0].connectedLinkId);
+        };
 
-
-      return {
+        return {
             open: open,
             openWithErrorMessage: openWithErrorMessage,
             openShift: openShift,
@@ -252,8 +251,8 @@
             isSelected: isSelected,
             setCurrent: setCurrent,
             getCurrent: getCurrent,
-        isSplit: isSplit,
-        isMultiLink: isMultiLink,
+            isSplit: isSplit,
+            isMultiLink: isMultiLink,
             isDirty: isDirty,
             setDirty: setDirty,
             preSplitSuravageLink: preSplitSuravageLink,
