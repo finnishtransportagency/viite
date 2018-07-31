c9e66b444c0248cf4d4d254f612f952b3b8e569c
hestrompa <hugo.estrompa@cgi.com>
2018-07-31 16:31:52 +0300

    Fix to tests

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/process/DefloatMapperSpec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/process/DefloatMapperSpec.scala
@@ -38,7 +38,7 @@ class DefloatMapperSpec extends FunSuite with Matchers{
   test("test apply mapping") {
     val roadAddressSource = sources.map(roadAddressLinkToRoadAddress(true))
     val mapping = DefloatMapper.createAddressMap(sources, targets)
-    val roadAddressTarget = roadAddressSource.flatMap(DefloatMapper.mapRoadAddresses(mapping))
+    val roadAddressTarget = roadAddressSource.flatMap(DefloatMapper.mapRoadAddresses(mapping, roadAddressSource))
     roadAddressTarget.size should be (4)
   }
 
@@ -46,12 +46,12 @@ class DefloatMapperSpec extends FunSuite with Matchers{
     val roadAddressSource = sources.map(roadAddressLinkToRoadAddress(true))
     // Note: this mapping doesn't make sense, it's only for unit testing on complex situation
     val mapping = Seq(
-      RoadAddressMapping(1021200L, 1021217L, 0.0, 10.0, 2.214, 5.0, Seq(Point(0.0, 0.0), Point(0.0,10.0)), Seq(Point(0.0, 0.0), Point(0.0,10.0))),
-      RoadAddressMapping(1021200L, 1021200L, 10.0, 40.0, 40.0, 0.0, Seq(Point(0.0, 10.0), Point(0.0,40.0)), Seq(Point(0.0, 40.0), Point(0.0,0.0))),
-      RoadAddressMapping(1021200L, 1021217L, 40.0, 82.925, 5.0, 17.215, Seq(Point(0.0, 40.0), Point(0.0,143.345)), Seq(Point(0.0, 5.0), Point(0.0,17.215))),
-      RoadAddressMapping(1021217L, 1021217L, 0.0, 40.345, 0.0, 2.214/17.215*40.345, Seq(Point(0.0, 40.0), Point(0.0,143.345)), Seq(Point(0.0, 5.0), Point(0.0,17.215)))
+      RoadAddressMapping(1021200L, 1021217L, 193080L, 0.0, 10.0, 2.214, 5.0, Seq(Point(0.0, 0.0), Point(0.0,10.0)), Seq(Point(0.0, 0.0), Point(0.0,10.0))),
+      RoadAddressMapping(1021200L, 1021200L, 193080L, 10.0, 40.0, 40.0, 0.0, Seq(Point(0.0, 10.0), Point(0.0,40.0)), Seq(Point(0.0, 40.0), Point(0.0,0.0))),
+      RoadAddressMapping(1021200L, 1021217L, 193080L, 40.0, 82.925, 5.0, 17.215, Seq(Point(0.0, 40.0), Point(0.0,143.345)), Seq(Point(0.0, 5.0), Point(0.0,17.215))),
+      RoadAddressMapping(1021217L, 1021217L, 233578L, 0.0, 40.345, 0.0, 2.214/17.215*40.345, Seq(Point(0.0, 40.0), Point(0.0,143.345)), Seq(Point(0.0, 5.0), Point(0.0,17.215)))
     )
-    val roadAddressTarget = roadAddressSource.flatMap(DefloatMapper.mapRoadAddresses(mapping))
+    val roadAddressTarget = roadAddressSource.flatMap(DefloatMapper.mapRoadAddresses(mapping, roadAddressSource))
     roadAddressTarget.size should be (4)
     roadAddressTarget.find(r => r.linkId == 1021200L)
       .map(r => r.startMValue).getOrElse(Double.NaN) should be (0.0 +- .00001)
@@ -129,7 +129,7 @@ class DefloatMapperSpec extends FunSuite with Matchers{
     mappings.exists(m => m.sourceStartM == 0.0 && m.targetStartM == 0.0) should be (true)
     mappings.exists(m => m.sourceEndM == 604.285 && m.targetEndM == 604.285) should be (true)
     mappings.size should be (1)
-    val mapped = sources.map(roadAddressLinkToRoadAddress(true)).flatMap(DefloatMapper.mapRoadAddresses(mappings))
+    val mapped = sources.map(roadAddressLinkToRoadAddress(true)).flatMap(DefloatMapper.mapRoadAddresses(mappings, sources.map(roadAddressLinkToRoadAddress(true))))
     mapped.exists(r =>
       mapped.filterNot(_ == r).exists(r2 =>
         !(r2.startAddrMValue >= r.endAddrMValue || r2.endAddrMValue <= r.startAddrMValue))
