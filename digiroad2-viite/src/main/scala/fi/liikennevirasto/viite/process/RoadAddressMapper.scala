8822f64b40945868631aa112575ca102ed10c267
hestrompa <hugo.estrompa@cgi.com>
2018-08-07 10:50:52 +0300

    Removing duplicated code and not necessary validation

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadAddressMapper.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadAddressMapper.scala
@@ -44,23 +44,13 @@ trait RoadAddressMapper {
   private def splitRoadAddressValues(roadAddress: RoadAddress, mapping: RoadAddressMapping): (Long, Long) = {
     if (withinTolerance(roadAddress.startMValue, mapping.sourceStartM) && withinTolerance(roadAddress.endMValue, mapping.sourceEndM)) {
       (roadAddress.startAddrMValue, roadAddress.endAddrMValue)
-    } else if (mapping.sourceLinkId == mapping.targetLinkId) {
-    val (startM, endM) = if (Math.abs((roadAddress.endMValue - roadAddress.startMValue) - (mapping.sourceEndM - mapping.sourceStartM)) <= MaxAllowedMValueError)
-    (roadAddress.startMValue, roadAddress.endMValue)
-    else
-    (mapping.sourceStartM, mapping.sourceEndM)
-      if (mapping.coefficient <= 1 + MinAllowedRoadAddressLength) {
-        val (startAddrM, endAddrM) = roadAddress.addressBetween(startM, Math.max(mapping.targetEndM, endM))
-        (Math.max(startAddrM, roadAddress.startAddrMValue), Math.min(endAddrM, roadAddress.endAddrMValue))
-      } else {
-        val (startAddrM, endAddrM) = roadAddress.addressBetween(startM, endM)
-        (Math.max(startAddrM, roadAddress.startAddrMValue), Math.min(endAddrM, roadAddress.endAddrMValue))
-      }
     } else {
-      val (startM, endM) = if (Math.abs((roadAddress.endMValue - roadAddress.startMValue) - (mapping.sourceEndM - mapping.sourceStartM)) <= MaxAllowedMValueError)
-        (roadAddress.startMValue, roadAddress.endMValue)
-      else
-        (mapping.sourceStartM, mapping.sourceEndM)
+      val (startM, endM) =
+        if (Math.abs((roadAddress.endMValue - roadAddress.startMValue) - (mapping.sourceEndM - mapping.sourceStartM)) <= MaxAllowedMValueError)
+          (roadAddress.startMValue, roadAddress.endMValue)
+        else
+          (mapping.sourceStartM, mapping.sourceEndM)
+
       val (startAddrM, endAddrM) = roadAddress.addressBetween(startM, endM)
       (Math.max(startAddrM, roadAddress.startAddrMValue), Math.min(endAddrM, roadAddress.endAddrMValue))
     }
