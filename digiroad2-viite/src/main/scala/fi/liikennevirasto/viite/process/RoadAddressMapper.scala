230d4c404ca99bc010ebd859a476b47eba2718e3
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2018-08-29 17:37:34 +0300

    VIITE-1507_2_segments_on_top_of_each_other_+_wrong_growing_direction_when_made_not_floating
    Moved the geometric transformations to the GeometryUtils class, divided in 2 different, move to origin and move to a specific point.

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadAddressMapper.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadAddressMapper.scala
@@ -214,10 +214,8 @@ trait RoadAddressMapper {
     */
 
   def distancesBetweenEndPointsInOrigin(geom1: Seq[Point], geom2: Seq[Point]): (Double, Double) = {
-    val sourcePoint = geom1.minBy(p => p.distance2DTo(Point(0,0,0)))
-    val targetPoint = geom2.minBy(p => p.distance2DTo(Point(0,0,0)))
-    val movedGeom1 = geom1.map(p => p.minus(sourcePoint))
-    val movedGeom2 = geom2.map(p => p.minus(targetPoint))
+    val movedGeom1 = GeometryUtils.moveGeomToOrigin(geom1)
+    val movedGeom2 = GeometryUtils.moveGeomToOrigin(geom2)
     (movedGeom1.head.distance2DTo(movedGeom2.head) + movedGeom1.last.distance2DTo(movedGeom2.last),
       movedGeom1.last.distance2DTo(movedGeom2.head) + movedGeom1.head.distance2DTo(movedGeom2.last))
   }
