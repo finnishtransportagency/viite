41a58429c1addb3e6f69ec97ec1e45afb077ae42
Pedro Lourenco <pedro.m.lourenco@cgi.com>
2018-08-31 15:50:41 +0300

    VIITE-1516 function base constraint

--- digiroad2-oracle/src/main/resources/db/migration/V0_16__duplicated_road_address_constraint.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V0_16__duplicated_road_address_constraint.sql
@@ -1,26 +1,21 @@
-CREATE UNIQUE INDEX duplicate_road_address ON
-    road_address (
-        road_number,
-        road_part_number,
-        track_code,
-        discontinuity,
-        start_addr_m,
-        end_addr_m,
-        start_date,
-    nvl(
-        end_date,'01.01.0001'),
-        valid_from,
-    nvl(
-        valid_to,'01.01.0001'),
-        calibration_points,
-        floating,
-        ely,
-        road_type,
-        terminated,
-        common_history_id,
-        side_code,
-        start_measure,
-        end_measure,
-        link_id,
-        link_source
-    );
+ALTER TABLE road_address
+    ADD CONSTRAINT road_address_duplicate UNIQUE ( road_number,
+                                                   road_part_number,
+                                                   track_code,
+                                                   discontinuity,
+                                                   start_addr_m,
+                                                   end_addr_m,
+                                                   start_date,
+                                                   end_date,
+                                                   valid_to,
+                                                   calibration_points,
+                                                   floating,
+                                                   ely,
+                                                   road_type,
+                                                   terminated,
+                                                   common_history_id,
+                                                   side_code,
+                                                   start_measure,
+                                                   end_measure,
+                                                   link_id,
+                                                   link_source );
