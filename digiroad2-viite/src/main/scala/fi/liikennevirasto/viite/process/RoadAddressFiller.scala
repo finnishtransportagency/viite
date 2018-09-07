acf108ba87d8d8439538d29070444f094260783c
Campanha <ricardo.campanha.marques@gi.com>
2018-09-07 11:49:53 +0300

    create new unknown links for floating GeometryChanged adjustedSegments in filler

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadAddressFiller.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadAddressFiller.scala
@@ -81,8 +81,11 @@ object RoadAddressFiller {
   }
 
   def generateUnknownRoadAddressesForRoadLink(roadLink: RoadLinkLike, adjustedSegments: Seq[RoadAddressLink]): Seq[MissingRoadAddress] = {
-    if (adjustedSegments.filterNot(s => s.anomaly == Anomaly.GeometryChanged).isEmpty)
-      generateUnknownLink(roadLink)
+    //If NoAddressGiven given, then there is no adjust,which lead to one new unknown link
+    val adjustedSegs = adjustedSegments.filterNot(s => s.anomaly == Anomaly.GeometryChanged)
+    if (adjustedSegs.isEmpty)
+      //headOption could have GeometryChanged -> which should create one new unknown link
+      generateUnknownLink(roadLink, adjustedSegments.headOption)
     else
       Seq()
   }
@@ -91,11 +94,13 @@ object RoadAddressFiller {
     roadLink.administrativeClass == State || roadLink.attributes.get("ROADNUMBER").exists(_.toString.toInt > 0)
   }
 
-  private def generateUnknownLink(roadLink: RoadLinkLike) = {
+  private def generateUnknownLink(roadLink: RoadLinkLike, roadAdressLink: Option[RoadAddressLink]) = {
     val geom = GeometryUtils.truncateGeometry3D(roadLink.geometry, 0.0, roadLink.length)
-    Seq(MissingRoadAddress(roadLink.linkId, None, None, PublicRoad, None, None, Some(0.0), Some(roadLink.length), isPublicRoad(roadLink) match {
-      case true => Anomaly.NoAddressGiven
-      case false => Anomaly.None
+    Seq(MissingRoadAddress(roadLink.linkId, None, None, PublicRoad, None, None, Some(0.0), Some(roadLink.length), (roadAdressLink.nonEmpty, isPublicRoad(roadLink)) match {
+      case (true, _) => Anomaly.GeometryChanged
+      case (_ , true)=> Anomaly.NoAddressGiven
+      case (_ , false)=> Anomaly.None
+
     }, geom))
   }
 
