13463db159f0f30a2156605a26bf6b4ed9c69943
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2018-09-14 11:54:13 +0100

    VIITE-1461_part2_the_value_of_the_continuity-code_changes_in_view_mode
    Modified the extracting of parameters on the SelectedLinkProperty.
    Modified the source of the information on the LinkPropertyForm.

--- viite-UI/src/view/LinkPropertyForm.js
+++ viite-UI/src/view/LinkPropertyForm.js
@@ -269,8 +269,8 @@
 
     var template = function(firstSelectedLinkProperty, linkProperties) {
       var roadTypes = selectedLinkProperty.count() === 1 ? staticField('TIETYYPPI', firstSelectedLinkProperty.roadTypeId) : roadTypeDynamicField();
-      var startAddress = selectedLinkProperty.count() === 1 ? staticField('ALKUETÄISYYS', firstSelectedLinkProperty.startAddressM) : measureDynamicField('ALKUETÄISYYS', 'startAddressM');
-      var endAddress = selectedLinkProperty.count() === 1 ? staticField('LOPPUETÄISYYS', firstSelectedLinkProperty.endAddressM) : measureDynamicField('LOPPUETÄISYYS', 'endAddressM');
+      var startAddress = selectedLinkProperty.count() === 1 ? staticField('ALKUETÄISYYS', firstSelectedLinkProperty.startAddressM) : staticField('ALKUETÄISYYS', linkProperties.startAddressM);
+      var endAddress = selectedLinkProperty.count() === 1 ? staticField('LOPPUETÄISYYS', firstSelectedLinkProperty.endAddressM) : staticField('LOPPUETÄISYYS', linkProperties.endAddressM);
       return _.template('' +
         '<header>' +
           title() +
@@ -295,7 +295,7 @@
             endAddress +
             staticField('ELY', firstSelectedLinkProperty.elyCode) +
             roadTypes +
-            staticField('JATKUVUUS', firstSelectedLinkProperty.discontinuity) +
+            staticField('JATKUVUUS', linkProperties.discontinuity) +
           '</div>' +
           revertToFloatingButton()+
         '</div>' +
