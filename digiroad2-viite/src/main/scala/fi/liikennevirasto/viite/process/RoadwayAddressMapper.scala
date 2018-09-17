384281d48642cb33b138e792574eac6b21878c7c
leirasf <faustino.leiras@cgi.com>
2018-09-17 17:40:53 +0100

    VIITE-1549 Refactoring and documenting the roadway addresses

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadwayAddressMapper.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadwayAddressMapper.scala
@@ -5,12 +5,19 @@ import fi.liikennevirasto.viite.dao._
 
 class RoadwayAddressMapper(roadAddressDAO: RoadAddressDAO) {
 
-  private def recalculateHistoryCalibrationPoints(roadAddress: RoadwayAddress, linearLocations: Seq[LinearLocation]): Seq[LinearLocation] = {
-    val currentRoadwayAddress = roadAddressDAO.fetchByRoadwayId(roadAddress.roadwayId).
-      getOrElse(throw new NoSuchElementException(s"Could not find any current road address for roadway ${roadAddress.roadwayId}"))
+  /**
+    * Recalculate address value of all the history road address calibration points
+    * @param historyRoadwayAddress The history roadway address
+    * @param linearLocations The current linear locations
+    * @return The linear location with recalculated calibration points
+    */
+  private def recalculateHistoryCalibrationPoints(historyRoadwayAddress: RoadwayAddress, linearLocations: Seq[LinearLocation]): Seq[LinearLocation] = {
+    val currentRoadwayAddress = roadAddressDAO.fetchByRoadwayId(historyRoadwayAddress.roadwayId).
+      getOrElse(throw new NoSuchElementException(s"Could not find any current road address for roadway ${historyRoadwayAddress.roadwayId}"))
 
     //Fix calibration points in history road addresses
-    val addressLength = roadAddress.endAddrMValue - roadAddress.startAddrMValue
+    val addressLength = historyRoadwayAddress.endAddrMValue - historyRoadwayAddress.startAddrMValue
+
     assert(addressLength >= 0)
 
     linearLocations.map {
@@ -20,23 +27,42 @@ class RoadwayAddressMapper(roadAddressDAO: RoadAddressDAO) {
             linearLocation
           case _ =>
             val (stCp, enCp) = linearLocation.calibrationPoints
-            linearLocation.copy(calibrationPoints = (stCp.map(calculateHistoryCalibrationPoint(currentRoadwayAddress, roadAddress)), enCp.map(calculateHistoryCalibrationPoint(currentRoadwayAddress, roadAddress))))
+            linearLocation.copy(calibrationPoints = (
+              stCp.map(calculateAddressHistoryCalibrationPoint(currentRoadwayAddress, historyRoadwayAddress)),
+              enCp.map(calculateAddressHistoryCalibrationPoint(currentRoadwayAddress, historyRoadwayAddress)))
+            )
         }
     }
   }
 
-  private def calculateHistoryCalibrationPoint(currentRoadwayAddress: RoadwayAddress, historyRoadwayAddress: RoadwayAddress)(calibrationAddress: Long): Long = {
+  /**
+    * Calculate the history calibration point address using a coeficient between the current and the history roadway address
+    * @param currentRoadwayAddress The current (endDate is null) road address
+    * @param historyRoadwayAddress The history road address
+    * @param currentCalibrationAddress The calibration point address of the current to be calculated in the history
+    * @return The address of the history calibration point
+    */
+  private def calculateAddressHistoryCalibrationPoint(currentRoadwayAddress: RoadwayAddress, historyRoadwayAddress: RoadwayAddress)(currentCalibrationAddress: Long): Long = {
     val currentAddressLength = currentRoadwayAddress.endAddrMValue - currentRoadwayAddress.startAddrMValue
     val historyAddressLength = historyRoadwayAddress.endAddrMValue - historyRoadwayAddress.startAddrMValue
-    val calibrationLength = calibrationAddress - currentRoadwayAddress.startAddrMValue
+    val calibrationLength = currentCalibrationAddress - currentRoadwayAddress.startAddrMValue
 
     historyRoadwayAddress.startAddrMValue + (historyAddressLength * calibrationLength / currentAddressLength)
   }
 
-  private def recalculateBetweenCalibrationPoints(roadAddress: RoadwayAddress, segmentLinearLocations: Seq[LinearLocation], startAddress: Long, endAddress: Long) = {
-    val coef = endAddress - startAddress / segmentLinearLocations.map(l => l.endMValue - l.startMValue).sum
+  /**
+    * Map roadway address into road addresses using linear locations in between given start and end address values
+    * @param roadwayAddress The roadway address
+    * @param linearLocations The linear locations in between given start and end address values
+    * @param startAddress The boundary start address value
+    * @param endAddress The boundary end address value
+    * @return Returns the mapped road addresses
+    */
+  private def boundaryAddressMap(roadwayAddress: RoadwayAddress, linearLocations: Seq[LinearLocation], startAddress: Long, endAddress: Long) : Seq[RoadAddress] = {
 
-    val sortedLinearLocations = segmentLinearLocations.sortBy(_.orderNumber)
+    val coef = endAddress - startAddress / linearLocations.map(l => l.endMValue - l.startMValue).sum
+
+    val sortedLinearLocations = linearLocations.sortBy(_.orderNumber)
 
     val addresses = sortedLinearLocations.init.scanLeft(startAddress) {
       case (address, location) =>
@@ -45,7 +71,6 @@ class RoadwayAddressMapper(roadAddressDAO: RoadAddressDAO) {
 
     sortedLinearLocations.zip(addresses.zip(addresses.tail)).map {
       case (linearLocation, (st, en)) =>
-        //TODO the start date and the created by should not be optional on the road address case class
         val geometryLength = linearLocation.endMValue - linearLocation.startMValue
         val (stCalibration, enCalibration) = linearLocation.calibrationPoints
 
@@ -54,39 +79,51 @@ class RoadwayAddressMapper(roadAddressDAO: RoadAddressDAO) {
           enCalibration.map(address => CalibrationPoint(linearLocation.linkId, if(linearLocation.sideCode == SideCode.AgainstDigitizing) 0 else geometryLength, address))
         )
 
-        val sideCode = if(roadAddress.reverted) SideCode.switch(linearLocation.sideCode) else linearLocation.sideCode
-        RoadAddress(roadAddress.id, linearLocation.id, roadAddress.roadNumber, roadAddress.roadPartNumber, roadAddress.roadType, roadAddress.track, Discontinuity.Continuous, st, en,
-          Some(roadAddress.startDate), roadAddress.endDate, Some(roadAddress.createdBy), linearLocation.linkId, linearLocation.startMValue, linearLocation.endMValue, sideCode,
-          linearLocation.adjustedTimestamp, calibrationPoints, linearLocation.floating, linearLocation.geometry, linearLocation.linkGeomSource, roadAddress.ely, roadAddress.terminated,
-          roadAddress.roadwayId, linearLocation.validFrom, linearLocation.validTo, blackUnderline = false, roadAddress.roadName)
+        val sideCode = if(roadwayAddress.reverted) SideCode.switch(linearLocation.sideCode) else linearLocation.sideCode
+        RoadAddress(roadwayAddress.id, linearLocation.id, roadwayAddress.roadNumber, roadwayAddress.roadPartNumber, roadwayAddress.roadType, roadwayAddress.track, Discontinuity.Continuous, st, en,
+          Some(roadwayAddress.startDate), roadwayAddress.endDate, Some(roadwayAddress.createdBy), linearLocation.linkId, linearLocation.startMValue, linearLocation.endMValue, sideCode,
+          linearLocation.adjustedTimestamp, calibrationPoints, linearLocation.floating, linearLocation.geometry, linearLocation.linkGeomSource, roadwayAddress.ely, roadwayAddress.terminated,
+          roadwayAddress.roadwayId, linearLocation.validFrom, linearLocation.validTo, blackUnderline = false, roadwayAddress.roadName)
     }
   }
 
-  private def recursiveRecalculator(roadAddress: RoadwayAddress, segmentLinearLocations: Seq[LinearLocation]) : Seq[RoadAddress] = {
+  /**
+    * Recursively map road addresses in between calibration points
+    * @param roadwayAddress The current roadway address
+    * @param linearLocations The linear location in between the calibration points
+    * @return Returns the mapped road addresses in between the calibration points
+    */
+  private def recursiveMapRoadAddresses(roadwayAddress: RoadwayAddress, linearLocations: Seq[LinearLocation]) : Seq[RoadAddress] = {
 
     def getUntilCalibrationPoint(seq: Seq[LinearLocation]): (Seq[LinearLocation], Seq[LinearLocation]) = {
       val linearLocationsUntilCp = seq.takeWhile(l => l.calibrationPoints._2.nonEmpty)
       (linearLocationsUntilCp, seq.drop(linearLocationsUntilCp.size))
     }
 
-    val (toProcess, others) =  getUntilCalibrationPoint(segmentLinearLocations)
+    val (toProcess, others) =  getUntilCalibrationPoint(linearLocations)
 
-    val startAddrMValue = if(toProcess.head.calibrationPoints._1.isDefined) toProcess.head.calibrationPoints._1.get else roadAddress.startAddrMValue
-    val endAddrMValue = if(toProcess.last.calibrationPoints._2.isDefined) toProcess.last.calibrationPoints._1.get else roadAddress.endAddrMValue
+    val startAddrMValue = if(toProcess.head.calibrationPoints._1.isDefined) toProcess.head.calibrationPoints._1.get else roadwayAddress.startAddrMValue
+    val endAddrMValue = if(toProcess.last.calibrationPoints._2.isDefined) toProcess.last.calibrationPoints._1.get else roadwayAddress.endAddrMValue
 
-    recalculateBetweenCalibrationPoints(roadAddress, toProcess, startAddrMValue, endAddrMValue)  ++ recursiveRecalculator(roadAddress, others)
+    boundaryAddressMap(roadwayAddress, toProcess, startAddrMValue, endAddrMValue) ++ recursiveMapRoadAddresses(roadwayAddress, others)
   }
 
-  def map(roadAddress: RoadwayAddress, linearLocations: Seq[LinearLocation]) : Seq[RoadAddress] = {
+  /**
+    * Map roadway address into road addresses using given linear locations
+    * @param roadwayAddress The current roadway address
+    * @param linearLocations The roadway linear locations
+    * @return Returns the mapped road addresses
+    */
+  def mapRoadAddresses(roadwayAddress: RoadwayAddress, linearLocations: Seq[LinearLocation]) : Seq[RoadAddress] = {
 
     val groupedLinearLocations = linearLocations.groupBy(_.roadwayId)
-    val roadwayLinearLocations = groupedLinearLocations.getOrElse(roadAddress.roadwayId, throw new IllegalArgumentException("Linear locations belong to the road address roadway"))
+    val roadwayLinearLocations = groupedLinearLocations.getOrElse(roadwayAddress.roadwayId, throw new IllegalArgumentException("Any linear locations found that belongs to the given roadway address"))
 
     //If is a roadway address history should recalculate all the calibration points
-    val roadAddresses = recursiveRecalculator(roadAddress, if(roadAddress.endDate.nonEmpty) recalculateHistoryCalibrationPoints(roadAddress, linearLocations) else roadwayLinearLocations)
+    val roadAddresses = recursiveMapRoadAddresses(roadwayAddress, if(roadwayAddress.endDate.nonEmpty) recalculateHistoryCalibrationPoints(roadwayAddress, linearLocations) else roadwayLinearLocations)
 
     //Set the discontinuity to the last road address
-    roadAddresses.init :+ roadAddresses.last.copy(discontinuity = roadAddress.discontinuity)
+    roadAddresses.init :+ roadAddresses.last.copy(discontinuity = roadwayAddress.discontinuity)
   }
 
 }
