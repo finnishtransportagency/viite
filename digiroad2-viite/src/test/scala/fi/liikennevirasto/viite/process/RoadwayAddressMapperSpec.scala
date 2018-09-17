81303a07a00fec116b815f35db2511a79ec72ebc
leirasf <faustino.leiras@cgi.com>
2018-09-17 18:55:49 +0100

    VIITE-1549 Fix unit tests of RoadwayAddressMapper

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/process/RoadwayAddressMapperSpec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/process/RoadwayAddressMapperSpec.scala
@@ -13,13 +13,13 @@ import fi.liikennevirasto.viite.dao.TerminationCode.NoTermination
 import fi.liikennevirasto.viite.dao._
 import fi.liikennevirasto.viite.model.{Anomaly, RoadAddressLink}
 import org.joda.time.DateTime
+import org.mockito.Mockito.when
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FunSuite, Matchers}
 
 class RoadwayAddressMapperSpec extends FunSuite with Matchers{
 
   val mockRoadAddressDAO: RoadAddressDAO = MockitoSugar.mock[RoadAddressDAO]
-
 
   test("Should map current roadway linear location into road addresses"){
     val roadwayAddressMapper = new RoadwayAddressMapper(mockRoadAddressDAO)
@@ -43,17 +43,21 @@ class RoadwayAddressMapperSpec extends FunSuite with Matchers{
     val roadwayAddressMapper = new RoadwayAddressMapper(mockRoadAddressDAO)
 
     val roadwayId = 12L
-    val startDate = new DateTime(1975, 7, 1, 0, 0)
-    val endDate = new DateTime(2006, 12, 31, 0, 0)
+    val currentStartDate = new DateTime(2007, 1, 1, 0, 0)
+    val historyStartDate = new DateTime(1975, 7, 1, 0, 0)
+    val historyEndDate = new DateTime(2006, 12, 31, 0, 0)
 
-    val roadwayAddress = RoadwayAddress(3L, roadwayId, 1551, 2, RoadType.PublicRoad, Track.Combined, Discontinuity.Discontinuous, 240, 400, false, startDate, Some(endDate), "test_user", None, 0, NoTermination, startDate)
+    val currentRoadwayAddress = RoadwayAddress(2L, roadwayId, 1551, 2, RoadType.PublicRoad, Track.Combined, Discontinuity.Discontinuous, 140, 300, false, currentStartDate, None, "test_user", None, 0, NoTermination, currentStartDate)
+    val historyRoadwayAddress = RoadwayAddress(3L, roadwayId, 1551, 2, RoadType.PublicRoad, Track.Combined, Discontinuity.Discontinuous, 240, 400, false, historyStartDate, Some(historyEndDate), "test_user", None, 0, NoTermination, historyStartDate)
     val linearLocations = Seq(
       LinearLocation(2L, 1, 125, 45.0, 105.0, SideCode.TowardsDigitizing, 0, (None, None), NoFloating, Seq(Point(45.0,0.0), Point(105.0,0.0)), LinkGeomSource.NormalLinkInterface, roadwayId),
       LinearLocation(3L, 2, 123, 0.0, 49.0, SideCode.TowardsDigitizing, 0, (None, Some(250)), NoFloating, Seq(Point(105.0,0.0), Point(154.0,0.0)), LinkGeomSource.NormalLinkInterface, roadwayId),
       LinearLocation(4L, 3, 124, 0.0, 51.0, SideCode.TowardsDigitizing, 0, (Some(250), Some(300)), NoFloating, Seq(Point(154.0,0.0), Point(205.0,0.0)), LinkGeomSource.NormalLinkInterface, roadwayId)
     )
 
-    val roadAddresses = roadwayAddressMapper.mapRoadAddresses(roadwayAddress, linearLocations)
+    when(mockRoadAddressDAO.fetchByRoadwayId(roadwayId)).thenReturn(Some(currentRoadwayAddress))
+
+    val roadAddresses = roadwayAddressMapper.mapRoadAddresses(historyRoadwayAddress, linearLocations)
 
     roadAddresses.size should be (3)
   }
