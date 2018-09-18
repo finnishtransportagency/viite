6570be96844ea1535646b4e6fb31c88d8334bda5
Campanha <ricardo.campanha.marques@gi.com>
2018-09-18 17:06:38 +0300

    VIITE-1578 removed unnecessary disable attribute

--- viite-UI/src/view/ProjectEditForm.js
+++ viite-UI/src/view/ProjectEditForm.js
@@ -72,7 +72,7 @@
         '<div class="form-group editable form-editable-roadAddressProject"> '+
 
         selectionForm(selection, selected, road) +
-        formCommon.changeDirection(selected, projectCollection.isDirty()) +
+        formCommon.changeDirection(selected) +
         formCommon.actionSelectedField()+
         '</div>'+
         '</div>' +
