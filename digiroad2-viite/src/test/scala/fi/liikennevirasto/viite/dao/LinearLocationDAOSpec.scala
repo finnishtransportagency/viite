330f5b0e4436e7eeeefcd9f6d7b65f678b0905d1
Sami Kosonen <sami.kosonen@cgi.com>
2018-09-18 16:02:43 +0300

    VIITE-1535 Fixed names of calibration point columns.

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/LinearLocationDAOSpec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/LinearLocationDAOSpec.scala
@@ -1,9 +1,12 @@
 package fi.liikennevirasto.viite.dao
 
+import fi.liikennevirasto.digiroad2.Point
+import fi.liikennevirasto.digiroad2.asset.{LinkGeomSource, SideCode}
 import fi.liikennevirasto.digiroad2.oracle.OracleDatabase
 import org.scalatest.{FunSuite, Matchers}
 import slick.driver.JdbcDriver.backend.Database
 import slick.driver.JdbcDriver.backend.Database.dynamicSession
+import fi.liikennevirasto.viite._
 
 class LinearLocationDAOSpec extends FunSuite with Matchers {
 
@@ -14,6 +17,16 @@ class LinearLocationDAOSpec extends FunSuite with Matchers {
     }
   }
 
-  // TODO
+  test("Create new linear location") {
+    runWithRollback {
+      val id = LinearLocationDAO.getNextLinearLocationId
+      val linearLocation = LinearLocation(id, 1, 1000, 0, 100, SideCode.TowardsDigitizing, 10000000,
+        (Some(0), None), FloatingReason.NoFloating, Seq(Point(0.0, 0.0), Point(0.0, 100.0)), LinkGeomSource.NormalLinkInterface, 200)
+      LinearLocationDAO.create(Seq(linearLocation))
+      val loc = LinearLocationDAO.fetchById(id).getOrElse(fail())
+      loc.validFrom.nonEmpty should be(true)
+
+    }
+  }
 
 }
