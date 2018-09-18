4f56501e407c7513dc219c5647022c581db727fc
Sami Kosonen <sami.kosonen@cgi.com>
2018-09-18 14:47:54 +0300

    VIITE-1535 Class for LinearLocationDAO tests.

---
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/LinearLocationDAOSpec.scala
@@ -0,0 +1,19 @@
+package fi.liikennevirasto.viite.dao
+
+import fi.liikennevirasto.digiroad2.oracle.OracleDatabase
+import org.scalatest.{FunSuite, Matchers}
+import slick.driver.JdbcDriver.backend.Database
+import slick.driver.JdbcDriver.backend.Database.dynamicSession
+
+class LinearLocationDAOSpec extends FunSuite with Matchers {
+
+  def runWithRollback(f: => Unit): Unit = {
+    Database.forDataSource(OracleDatabase.ds).withDynTransaction {
+      f
+      dynamicSession.rollback()
+    }
+  }
+
+  // TODO
+
+}
