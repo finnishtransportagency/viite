e3c921c6b826f445d0fe3f7646f34e1f6f8332f3
Campanha <ricardo.campanha.marques@gi.com>
2018-10-08 12:37:35 +0300

    VIITE-1616 use methods from proper DAOs

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectValidator.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectValidator.scala
@@ -284,7 +284,7 @@ object ProjectValidator {
   def error(id: Long, validationError: ValidationError, info: String = "N/A")(pl: Seq[ProjectLink]): Option[ValidationErrorDetails] = {
     val (splitLinks, nonSplitLinks) = pl.partition(_.isSplit)
     val splitIds = splitLinks.flatMap(s => Seq(s.connectedLinkId.get, s.linkId))
-    val connectedSplitLinks = ProjectDAO.getProjectLinksByConnectedLinkId(splitIds)
+    val connectedSplitLinks = ProjectLinkDAO.getProjectLinksByConnectedLinkId(splitIds)
     val (ids, points) = (nonSplitLinks ++ connectedSplitLinks).map(pl => (pl.id, GeometryUtils.midPointGeometry(pl.geometry))).unzip
     if (ids.nonEmpty) {
       Some(ValidationErrorDetails(id, validationError, ids,
@@ -603,7 +603,7 @@ object ProjectValidator {
     */
   def checkRoadContinuityCodes(project: RoadAddressProject, roadProjectLinks: Seq[ProjectLink], isRampValidation: Boolean = false): Seq[ValidationErrorDetails] = {
 
-    val allProjectLinks = ProjectDAO.getProjectLinks(project.id)
+    val allProjectLinks = ProjectLinkDAO.getProjectLinks(project.id)
 
     def isConnectingRoundabout(pls: Seq[ProjectLink]): Boolean = {
       throw new NotImplementedError("Will be implemented at VIITE-1540")
