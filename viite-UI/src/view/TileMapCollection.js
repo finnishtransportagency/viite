e969919ac7c14f60b0964c0c84bd77ad89707ebd
hestrompa <hugo.estrompa@cgi.com>
2018-10-18 19:29:59 +0300

    Calling correct capability function after changing gray scale maps origin

--- viite-UI/src/view/TileMapCollection.js
+++ viite-UI/src/view/TileMapCollection.js
@@ -1,5 +1,5 @@
 (function(root) {
-  root.TileMapCollection = function(arcgisConfig) {
+  root.TileMapCollection = function(OAGGreyConfig) {
     var layerConfig = {
       // minResolution: ?,
       // maxResolution: ?,
@@ -80,11 +80,12 @@
           propertyBorder : propertyBorderLayer
       };
 
-    if(arcgisConfig) { //check is not needed it, will almost always be true and checks old getcapabilities
+    if(OAGGreyConfig) {
       var parser = new ol.format.WMTSCapabilities();
-      var result = parser.read(arcgisConfig);
-      var config = {layer: "Taustakartat_Harmaasavy"};
+      var result = parser.read(OAGGreyConfig);
+      var config = {layer: 'liikennevirasto:PTP_Taustakartta_Harmaa', matrixSet:'EPSG:3067_PTP_JHS180'};
       var options = ol.source.WMTS.optionsFromCapabilities(result, config);
+      options.urls= ['rasteripalvelu/wmts?'];
       var greyscaleLayer = new ol.layer.Tile({source: new ol.source.WMTS(options)});
       greyscaleLayer.set('name', 'greyScaleLayer');
       tileMapLayers.greyscale = greyscaleLayer;
