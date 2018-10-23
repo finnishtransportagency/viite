cfd706bb96374263ca55b99866471a4febff0f8f
Pedro Lourenco <pedro.m.lourenco@cgi.com>
2018-10-23 19:23:07 +0300

    VIITE-1540 styler changes

--- viite-UI/src/view/ProjectLinkStyler.js
+++ viite-UI/src/view/ProjectLinkStyler.js
@@ -9,6 +9,42 @@
     var terminatedStatus = 5;
     var unknownStatus = 99;
 
+    var strokeWidthRules = [
+      new StyleRule().where('zoomLevel').is(5).use({stroke: {width: 4 }}),
+      new StyleRule().where('zoomLevel').is(6).use({stroke: {width: 4 }}),
+      new StyleRule().where('zoomLevel').is(7).use({stroke: {width: 4 }}),
+      new StyleRule().where('zoomLevel').is(8).use({stroke: {width: 5 }}),
+      new StyleRule().where('zoomLevel').is(9).use({stroke: {width: 5 }}),
+      new StyleRule().where('zoomLevel').is(10).use({stroke: {width: 5 }}),
+      new StyleRule().where('zoomLevel').is(11).use({stroke: {width: 5 }}),
+      new StyleRule().where('zoomLevel').is(12).use({stroke: {width: 8 }}),
+      new StyleRule().where('zoomLevel').is(13).use({stroke: {width: 8 }}),
+      new StyleRule().where('zoomLevel').is(14).use({stroke: {width: 10 }}),
+      new StyleRule().where('zoomLevel').is(15).use({stroke: {width: 10 }})
+    ];
+
+    var fillRules = [
+      new StyleRule().where('roadClass').is(1).use({stroke: {color: '#FF0000', lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(2).use({stroke: {color: '#FF6600',  lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(3).use({stroke: {color: '#FF9933',  lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(4).use({stroke: {color: '#0011BB',  lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(5).use({stroke: {color: '#33CCCC',  lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(6).use({stroke: {color: '#E01DD9',  lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(7).use({stroke: {color:'#fff', lineCap: 'butt', lineDash: [10, 10]}}),
+      new StyleRule().where('roadClass').is(8).use({stroke: {color:'#fff', lineCap: 'butt', lineDash: [10, 10]}}),
+      new StyleRule().where('roadClass').is(9).use({stroke: {color:'#fff', lineCap: 'butt', lineDash: [10, 10]}}),
+      new StyleRule().where('roadClass').is(10).use({stroke: {color: '#FF55DD', lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(11).use({stroke: {color: '#444444', lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(97).use({stroke: {color: '#1E1E1E', lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(98).use({stroke: {color: '#FAFAFA', lineCap: 'round'}}),
+      new StyleRule().where('constructionType').is(LinkValues.ConstructionType.UnderConstruction.value).use({stroke: {color: '#ff9900', lineCap: 'round'}}),
+      new StyleRule().where('gapTransfering').is(true).use({stroke: {color: '#00FF00', lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(99).and('roadLinkSource').isNot(3).use({stroke: {color: '#A4A4A2', lineCap: 'round'}}),
+      new StyleRule().where('roadClass').is(99).and('anomaly').is(1).use({stroke: {color: '#1E1E1E', lineCap: 'round'}}),
+      new StyleRule().where('floating').is(1).use({stroke: {color: '#F7FE2E', lineCap: 'round'}}),
+      new StyleRule().where('roadLinkSource').is(3).and('roadClass').is(99).use({stroke: {color: '#D3AFF6', lineCap: 'round'}})
+    ];
+
     var projectLinkRules = [
       new StyleRule().where('status').is(notHandledStatus).use({stroke: {color: '#F7FE2E', lineCap: 'round'}}),
       new StyleRule().where('status').is(unchangedStatus).use({stroke: {color: '#0000FF', lineCap: 'round'}}),
@@ -32,8 +68,10 @@
       new StyleRule().where('floating').is(1).use({stroke: {color: '#F7FE2E', lineCap: 'round'}})
     ];
 
-    var projectLinkStyle = new StyleRuleProvider({});
+    var projectLinkStyle = new StyleRuleProvider({zIndex: LinkValues.RoadZIndex.ReservedRoadLayer.value});
+    projectLinkStyle.addRules(fillRules);
     projectLinkStyle.addRules(projectLinkRules);
+    projectLinkStyle.addRules(strokeWidthRules);
     projectLinkStyle.addRules(generalLinksRules);
 
     var selectionLinkStyle = new StyleRuleProvider({stroke: {lineCap: 'round', width: 8, color: '#00FF00'}});
