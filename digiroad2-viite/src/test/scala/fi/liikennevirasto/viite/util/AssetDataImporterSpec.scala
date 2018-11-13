ad2e9a1af6cca0007d63814184fe31f68a95b45a
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2018-11-13 11:09:47 +0000

    VIITE-1543
    Trying to resolve the dynamicSession can only be used within a withDynSession block error.

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/util/AssetDataImporterSpec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/util/AssetDataImporterSpec.scala
@@ -35,6 +35,8 @@ class AssetDataImporterSpec extends FunSuite with Matchers {
 
   def withDynTransaction[T](f: => T): T = OracleDatabase.withDynTransaction(f)
 
+  def withDynSession[T](f: => T): T = OracleDatabase.withDynSession(f)
+
   def runWithRollback[T](f: => T): T = {
     Database.forDataSource(OracleDatabase.ds).withDynTransaction {
       val t = f
