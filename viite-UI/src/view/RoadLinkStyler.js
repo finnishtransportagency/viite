aa116955d439b11a58f509c12e96b44eba865df5
Campanha <ricardo.campanha.marques@gi.com>
2018-11-16 17:37:31 +0200

    VIITE-1686 added color for raods (50001 - 59999)

--- viite-UI/src/view/RoadLinkStyler.js
+++ viite-UI/src/view/RoadLinkStyler.js
@@ -91,11 +91,10 @@
         new StyleRule().where('roadClass').is(9).use({stroke: {color: '#FF55DD', lineCap: 'round'}}),
         new StyleRule().where('roadClass').is(10).use({stroke: {color: '#FF55DD', lineCap: 'round'}}),
         new StyleRule().where('roadClass').is(11).use({stroke: {color: '#444444', lineCap: 'round'}}),
+        new StyleRule().where('roadClass').is(12).use({stroke: {color: '#A4A4A2', lineCap: 'round'}}),
         new StyleRule().where('roadClass').is(97).use({stroke: {color: '#1E1E1E', lineCap: 'round'}}),
         new StyleRule().where('roadClass').is(98).use({stroke: {color: '#FAFAFA', lineCap: 'round'}}),
         new StyleRule().where('gapTransfering').is(true).use({stroke: {color: '#00FF00', lineCap: 'round'}}),
-        new StyleRule().where('administrativeClass').is('Municipality').and('roadClass').isNot(7).and('roadClass').isNot(8).and('roadClass').isNot(9).and('roadClass').isNot(99).use({stroke: {color: '#1E1E1E', lineCap: 'round'}}),
-        new StyleRule().where('administrativeClass').is('Private').and('roadClass').isNot(7).and('roadClass').isNot(8).and('roadClass').isNot(9).and('roadClass').isNot(99).use({stroke: {color: '#1E1E1E', lineCap: 'round'}}),
         new StyleRule().where('roadClass').is(99).and('roadLinkSource').isNot(3).use({stroke: {color: '#A4A4A2', lineCap: 'round'}}),
         new StyleRule().where('roadClass').is(99).and('anomaly').is(1).use({stroke: {color: '#1E1E1E', lineCap: 'round'}}),
         new StyleRule().where('floating').is(1).use({stroke: {color: '#F7FE2E', lineCap: 'round'}}),
@@ -114,6 +113,7 @@
        new StyleRule().where('roadClass').is(9).use({stroke: {color:'#fff', lineCap: 'butt', lineDash: [10, 10]}}),
        new StyleRule().where('roadClass').is(10).use({stroke: {color: '#FF55DD', lineCap: 'round'}}),
        new StyleRule().where('roadClass').is(11).use({stroke: {color: '#444444', lineCap: 'round'}}),
+        new StyleRule().where('roadClass').is(12).use({stroke: {color:'#FF55DD', lineCap: 'butt', lineDash: [10, 10]}}),
        new StyleRule().where('roadClass').is(97).use({stroke: {color: '#1E1E1E', lineCap: 'round'}}),
        new StyleRule().where('roadClass').is(98).use({stroke: {color: '#FAFAFA', lineCap: 'round'}}),
        new StyleRule().where('gapTransfering').is(true).use({stroke: {color: '#00FF00', lineCap: 'round'}}),
@@ -122,7 +122,7 @@
         new StyleRule().where('roadClass').is(99).and('anomaly').is(1).and('constructionType').isNot(LinkValues.ConstructionType.UnderConstruction.value).use({stroke: {color: '#1E1E1E', lineCap: 'round'}}),
         new StyleRule().where('roadClass').is(99).and('anomaly').is(1).and('constructionType').is(LinkValues.ConstructionType.UnderConstruction.value).use({stroke: {color:'#ff9900', lineCap: 'butt', lineDash: [10, 10]}}),
         new StyleRule().where('floating').is(1).use({stroke: {color: '#F7FE2E', lineCap: 'round'}}),
-       new StyleRule().where('roadLinkSource').is(3).and('roadClass').is(99).use({stroke: {color: '#D3AFF6', lineCap: 'round'}})
+        new StyleRule().where('roadClass').is(99).and('roadLinkSource').is(3).use({stroke: {color: '#D3AFF6', lineCap: 'round'}})
     ];
 
     var roadLinkStyle = new StyleRuleProvider({});
