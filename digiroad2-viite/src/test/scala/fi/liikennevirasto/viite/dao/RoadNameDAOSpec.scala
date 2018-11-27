3917efb64bf70e5f23a1a3d1c8e5d06eec05e8b8
Campanha <ricardo.campanha.marques@gi.com>
2018-11-27 17:29:49 +0200

    VIITE-1543 fix test

---
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/RoadNameDAOSpec.scala
@@ -0,0 +1,20 @@
+package fi.liikennevirasto.viite.dao
+
+import fi.liikennevirasto.digiroad2.dao.Sequences
+import fi.liikennevirasto.digiroad2.oracle.OracleDatabase
+import org.scalatest.{FunSuite, Matchers}
+import slick.driver.JdbcDriver.backend.Database
+import slick.driver.JdbcDriver.backend.Database.dynamicSession
+import slick.jdbc.StaticQuery.interpolation
+
+class RoadNameDAOSpec extends FunSuite with Matchers {
+
+  def runWithRollback(f: => Unit): Unit = {
+    Database.forDataSource(OracleDatabase.ds).withDynTransaction {
+      f
+      dynamicSession.rollback()
+    }
+  }
+
+
+}
