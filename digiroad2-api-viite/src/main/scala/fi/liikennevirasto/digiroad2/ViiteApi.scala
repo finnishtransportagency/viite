4466283250575493528a9bd6df9ebe3b08325abd
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2018-11-30 10:48:44 +0000

    VIITE-1707 The road name does not fill automatically when creating a new road address / Tiennimi ei päivity automaattisesti uutta tieosoitetta luotaessa
    Changed the backend to receive 2 parameters, linkId and current Project Id.
    Edited the PreFillInfo object to add a parameter that indicates the source of the road name (relevant to the blockage of the name box in the UI).
    parsePreFillData now fetches data from either the road name table (this source will block the name box in the UI) or from the project link (this will not block the box).
    Added the current project id in the forms to be passed to the backend.
    Blockage of the road name box is now dependant of the source.

--- digiroad2-api-viite/src/main/scala/fi/liikennevirasto/digiroad2/ViiteApi.scala
+++ digiroad2-api-viite/src/main/scala/fi/liikennevirasto/digiroad2/ViiteApi.scala
@@ -175,6 +175,20 @@ class ViiteApi(val roadLinkService: RoadLinkService, val vVHClient: VVHClient,
           .map(midPoint)
           .getOrElse(Map("success" -> false, "reason" -> ("ID:" + id + " not found")))
     }
+
+  }
+
+  get("/roadlinks/project/prefillfromvvh") {
+    val linkId = params("linkId").toLong
+    val currentProjectId = params("currentProjectId").toLong
+    time(logger, s"GET request for /roadlinks/project/prefillfromvvh (linkId: $linkId, projectId: $currentProjectId)") {
+      projectService.fetchPreFillFromVVH(linkId, currentProjectId) match {
+        case Right(preFillInfo) => {
+          Map("success" -> true, "roadNumber" -> preFillInfo.RoadNumber, "roadPartNumber" -> preFillInfo.RoadPart, "roadName" -> preFillInfo.roadName, "roadNameSource" -> preFillInfo.roadNameSource.value)
+        }
+        case Left(failureMessage) => Map("success" -> false, "reason" -> failureMessage)
+      }
+    }
   }
 
   get("/roadlinks/adjacent") {
