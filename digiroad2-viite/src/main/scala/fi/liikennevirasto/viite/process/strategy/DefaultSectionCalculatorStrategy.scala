934ef1193d8d66d2405b5fc21e93ecb10e95dad2
Campanha <ricardo.campanha.marques@gi.com>
2019-01-28 14:53:33 +0200

    fix in adjustment between project links for 2 tracks

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/strategy/DefaultSectionCalculatorStrategy.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/strategy/DefaultSectionCalculatorStrategy.scala
@@ -55,7 +55,14 @@ class DefaultSectionCalculatorStrategy extends RoadAddressSectionCalculatorStrat
     }.toSeq
   }
 
-  private def getContinuousTrack(seq: Seq[ProjectLink]): (Seq[ProjectLink], Seq[ProjectLink]) = {
+  private def getContinuousSection(seq: Seq[ProjectLink]): (Seq[ProjectLink], Seq[ProjectLink]) = {
+    val track = seq.headOption.map(_.track).getOrElse(Track.Unknown)
+    val roadwayNumber = seq.headOption.map(_.roadwayNumber).getOrElse(0)
+    val continuousProjectLinks = seq.takeWhile(pl => (pl.track == track && pl.track == Track.Combined) || (pl.track != Track.Combined && pl.roadwayNumber == roadwayNumber))
+    (continuousProjectLinks, seq.drop(continuousProjectLinks.size))
+  }
+
+  private def getContinuousRoadwayNumber(seq: Seq[ProjectLink]): (Seq[ProjectLink], Seq[ProjectLink]) = {
     val roadwayNumber = seq.headOption.map(_.roadwayNumber).getOrElse(0)
     val continuousProjectLinks = seq.takeWhile(pl => pl.roadwayNumber == roadwayNumber)
     (continuousProjectLinks, seq.drop(continuousProjectLinks.size))
@@ -69,8 +76,8 @@ class DefaultSectionCalculatorStrategy extends RoadAddressSectionCalculatorStrat
         (Seq(), Seq())
       } else {
 
-        val (firstRight, restRight) = getContinuousTrack(rightLinks)
-        val (firstLeft, restLeft) = getContinuousTrack(leftLinks)
+        val (firstRight, restRight) = getContinuousSection(rightLinks)
+        val (firstLeft, restLeft) = getContinuousSection(leftLinks)
 
         if (firstRight.isEmpty || firstLeft.isEmpty)
           throw new RoadAddressException(s"Mismatching tracks, R ${firstRight.size}, L ${firstLeft.size}")
