a1c6f8e7ed347f6b297363690f9d120d9e005b1e
Campanha <ricardo.campanha.marques@gi.com>
2019-01-29 17:11:28 +0200

    VIITE-1716 removed duplicated code

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/RoadNetworkService.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/RoadNetworkService.scala
@@ -117,8 +117,6 @@ class RoadNetworkService {
       }
     }
 
-    def runActor = {
-
       withDynTransaction {
         try {
           val roadsInChunk = roadwayDAO.fetchAllByRoadNumbers(options.roadNumbers)
@@ -159,8 +157,11 @@ class RoadNetworkService {
             }
           }
 
-          //validate and create a new published version in case there are no errors now
-          if (options.currNetworkVersion.nonEmpty && !roadNetworkDAO.hasCurrentNetworkErrors) {
+          /*
+          * Used for actor cases only.
+          * Batch should only deal with expiring and publishing of road network after run all chunks of the entire road network
+          */
+          if (options.throughActor && options.currNetworkVersion.nonEmpty && !roadNetworkDAO.hasCurrentNetworkErrors) {
             logger.info(s"No errors found. Creating new publishable version for the road network ")
             roadNetworkDAO.expireRoadNetwork
             roadNetworkDAO.createPublishedRoadNetwork
@@ -168,7 +169,6 @@ class RoadNetworkService {
             roadwayDAO.fetchAllCurrentRoadwayIds.foreach(id => roadNetworkDAO.createPublishedRoadway(newId.get, id))
           }
 
-
         } catch {
           case e: SQLIntegrityConstraintViolationException => logger.error("A road network check is already running")
           case e: SQLException => {
@@ -182,67 +182,6 @@ class RoadNetworkService {
           }
         }
       }
-    }
-
-    def runBatch = {
-
-      withDynTransaction {
-        try {
-          val roadsInChunk = roadwayDAO.fetchAllByRoadNumbers(options.roadNumbers)
-          val linearLocationsInChunk = linearLocationDAO.fetchByRoadways(roadsInChunk.map(_.roadwayNumber).distinct.toSet).groupBy(_.roadwayNumber)
-          val roadways = roadsInChunk.groupBy(g => (g.roadNumber, g.roadPartNumber))
-
-          val errors = roadways.flatMap { group =>
-            val (section, roadway) = group
-
-            val (combinedLeft, combinedRight) = (roadway.filter(t => t.track != Track.RightSide).sortBy(_.startAddrMValue), roadway.filter(t => t.track != Track.LeftSide).sortBy(_.startAddrMValue))
-            val roadwaysErrors = checkRoadways(combinedLeft, combinedRight)
-            logger.info(s" Found ${roadwaysErrors.size} roadway errors for RoadNumber ${section._1} and Part ${section._2}")
-
-            val (combinedRoadways, twoTrackRoadways) = roadway.partition(_.track == Combined)
-            val (leftRoadways, rightRoadways) = twoTrackRoadways.partition(_.track == LeftSide)
-
-            val leftTrackLinearLocations = leftRoadways.flatMap(r => linearLocationsInChunk.get(r.roadwayNumber)).flatten
-            val rightTrackLinearLocations = rightRoadways.flatMap(r => linearLocationsInChunk.get(r.roadwayNumber)).flatten
-            val combinedLinearLocations = combinedRoadways.flatMap(r => linearLocationsInChunk.get(r.roadwayNumber)).flatten
-
-            val leftTrackErrors = checkTwoTrackLinearLocations(leftTrackLinearLocations, leftRoadways)
-            val rightTrackErrors = checkTwoTrackLinearLocations(rightTrackLinearLocations, rightRoadways)
-            val combinedLeftErrors = checkCombinedLinearLocations(combinedLinearLocations ++ leftTrackLinearLocations, combinedRoadways ++ leftRoadways)
-            val combinedRightErrors = checkCombinedLinearLocations(combinedLinearLocations ++ rightTrackLinearLocations, combinedRoadways ++ rightRoadways)
-            val linearLocationErrors = leftTrackErrors ++ rightTrackErrors ++ combinedLeftErrors ++ combinedRightErrors
-
-            logger.info(s" Found ${linearLocationErrors.size} linear locations errors for RoadNumber ${section._1} and Part ${section._2} (twoTrack: ${leftTrackErrors.size + rightTrackErrors.size}) , (combined: ${combinedLeftErrors.size + combinedRightErrors.size})")
-
-            roadwaysErrors ++ linearLocationErrors
-          }
-          if (errors.nonEmpty) {
-            val uniqueErrors = errors.groupBy(g => (g.roadwayId, g.linearLocationId, g.error, g.network_version)).map(_._2.head).toSeq
-            val existingErrors = roadNetworkDAO.getRoadNetworkErrors(AddressError.InconsistentTopology)
-            val newErrors = uniqueErrors.filterNot(r => existingErrors.exists(e => e.roadwayId == r.roadwayId && e.linearLocationId == r.linearLocationId && e.error == r.error && e.network_version == r.network_version))
-            newErrors.sortBy(_.roadwayId).foreach { e =>
-              logger.info(s" Found error for roadway id ${e.roadwayId}, linear location id ${e.linearLocationId}")
-              roadNetworkDAO.addRoadNetworkError(e.roadwayId, e.linearLocationId, e.error, e.network_version)
-            }
-          }
-        } catch {
-          case e: SQLIntegrityConstraintViolationException => logger.error("A road network check is already running")
-          case e: SQLException => {
-            logger.info("SQL Exception")
-            logger.error(e.getMessage)
-            dynamicSession.rollback()
-          }
-          case e: Exception => {
-            logger.error(e.getMessage)
-            dynamicSession.rollback()
-          }
-        }
-      }
-    }
-
-    if (options.throughActor)
-      runActor
-    else runBatch
 
   }
 
