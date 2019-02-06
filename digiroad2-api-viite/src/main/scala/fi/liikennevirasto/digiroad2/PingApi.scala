b0116e3b8b75196da23c35670d08414e34714c1b
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2019-02-06 13:27:56 +0200

    VIITE-1723 Dokumentoi palvelut Swaggerilla / Document services with Swagger
    Removed the swagger from PingAPI.

--- digiroad2-api-viite/src/main/scala/fi/liikennevirasto/digiroad2/PingApi.scala
+++ digiroad2-api-viite/src/main/scala/fi/liikennevirasto/digiroad2/PingApi.scala
@@ -2,22 +2,11 @@ package fi.liikennevirasto.digiroad2
 
 import org.scalatra.{InternalServerError, Ok, ScalatraServlet}
 import org.slf4j.LoggerFactory
-import org.scalatra.swagger._
 
-class PingApi(implicit val swagger: Swagger) extends ScalatraServlet with SwaggerSupport {
-
+class PingApi extends ScalatraServlet {
   val logger = LoggerFactory.getLogger(getClass)
 
-  protected val applicationDescription = "The PingAPI"
-
-  val ping = (
-    apiOperation[Unit]("ping")
-    tags "PingAPI"
-    summary "This allows viite to respond to a simple, empty get request in order to receive a confirmation of Viite being active or not."
-    notes ""
-  )
-
-  get("/", operation(ping)) {
+  get("/") {
     try {
       Digiroad2Context.userProvider.getUser("")
       Ok("OK")
