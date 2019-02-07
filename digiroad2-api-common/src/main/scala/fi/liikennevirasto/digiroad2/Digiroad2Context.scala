e893189a10a1649ed2cf2f96e446146d8194738d
Guilherme Pedrosa <guilherme.pedrosa@cgi.com>
2019-02-07 12:25:24 +0200

    VIITE-1717 Poista kelluvien ja tuntemattomien tarpeeton koodi / Removal of unneeded code about Floating and Unaddressed links
    Removal of a lot of code related to the floating tasks.

--- digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
+++ digiroad2-api-common/src/main/scala/fi/liikennevirasto/digiroad2/Digiroad2Context.scala
@@ -46,13 +46,6 @@ class RoadAddressUpdater(roadAddressService: RoadAddressService) extends Actor {
 //  }
 //}
 
-//TODO check if this is needed at VIITE-1538
-//class RoadAddressFloater(roadAddressService: RoadAddressService) extends Actor {
-//  def receive = {
-//    case w: Set[any] => roadAddressService.checkRoadAddressFloating(w.asInstanceOf[Set[Long]])
-//    case _                    => println("roadAddressUpdater: Received unknown message")
-//  }
-//}
 
 class RoadNetworkChecker(roadNetworkService: RoadNetworkService) extends Actor {
   def receive = {
