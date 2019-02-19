ccf2592dc287cde0a2e589c0d1aeedfd54629247
Sami Kosonen <sami.kosonen@cgi.com>
2019-02-19 08:02:25 +0200

    VIITE-1822 Node sprite to be used in legend and NodeMarker.js to be used in map layer.

--- viite-UI/src/map/NodeMarker.js
+++ viite-UI/src/map/NodeMarker.js
@@ -3,42 +3,38 @@
 
     var createNodeMarker = function (node) {
       var box = new ol.Feature({
-        geometry: new ol.geom.Point([node.x, node.y]),
+        geometry: new ol.geom.Point([node.x, node.y]), // TODO assuming that node has x and y coordinates
         nodeId: node.id,
         type: "marker"
       });
 
       var colorMap =
-        {           //comment includes legend name
-          1: '#db0e0e',         // TODO
-          2: '#ff6600',         // TODO
-          3: '#ff9955',         // TODO
-          4: '#1414db',         // TODO
-          5: '#10bfc4',         // TODO
-          6: '#800080',         // TODO
-          7: '#10bfc4',         // TODO
-          8: '#fc6da0',         // TODO
-          9: '#fc6da0',         // TODO
-          10: '#fc6da0',        // TODO
-          11: '#888888'         // TODO
+        { //   Color                                    Node type
+          1:  {fill: '#e9b28a', stroke: '#be7041'},  // Normaali tasoliittymä
+          3:  {fill: '#b0ce94', stroke: '#5e803f'},  // Kiertoliittymä
+          4:  {fill: '#f6c043', stroke: '#b89131'},  // Y-liittymä
+          5:  {fill: '#fcf1cf', stroke: '#7a601d'},  // Eritasoliittymä
+          7:  {fill: '#e17863', stroke: '#b02218'},  // Maantien / kadun raja
+          8:  {fill: '#689ad0', stroke: '#3f74b0'},  // ELY-raja
+          10: {fill: '#e4efdb', stroke: '#b1ce94'},  // Moniajoratainen liittymä
+          11: {fill: '#bebfbe', stroke: '#b75f29'},  // Pisaraliittymä
+          12: {fill: '#bebebf', stroke: '#515151'},  // Liityntätie
+          13: {fill: '#bfbebe', stroke: '#b79030'},  // Tien loppu
+          14: {fill: '#ebaa9e', stroke: '#eb3223'},  // Silta
+          15: {fill: '#fae6a3', stroke: '#f5c042'},  // Huoltoaukko
+          16: {fill: '#dcb5d1', stroke: '#68389a'}   // Yksityistie- tai katuliittymä
         };
 
-      var nodeColor = function (roadLink) {
-        if (roadLink.status === LinkValues.LinkStatus.New.value) {
-          return '#ff55dd';
-        } else if (roadLink.roadLinkSource === LinkValues.LinkGeomSource.SuravageLinkInterface.value && roadLink.id === 0) {
-          return '#d3aff6';
-        }
-        else if (roadLink.roadClass in colorMap) {
-          return colorMap[roadLink.roadClass];
+      var color = function (node) {
+        if (node.type in colorMap) { // TODO assuming that node has a type attribute
+          return colorMap[node.type];
         } else
-          return '#888888';
+          return {fill: '#888888', stroke: '#555555'};
       };
 
-      var boxStyleNode = function (rl) {
-        var fillColor = nodeColor(rl);
-        var strokeColor = nodeColor(rl);
-        var nodeMarker = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns="http://www.w3.org/2000/svg" class="node" height="24" width="18" viewBox="0 0 100 141.07506"><path style="opacity:1;fill:' + strokeColor + ';fill-opacity:1;stroke:none;" d="M 50,4.4085955 102.90315,70.537531 50,136.66646 -2.9031496,70.537531 Z" class="stroke" /><path class="fill" d="M 50,29.390638 82.917516,70.537531 50,111.68443 17.082484,70.537531 Z" style="opacity:1;fill:' + fillColor + ';fill-opacity:1;stroke:none;" /></svg>';
+      var boxStyleNode = function (node) {
+        var nodeColor = color(node);
+        var nodeMarker = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns="http://www.w3.org/2000/svg" class="node" height="24" width="18" viewBox="0 0 100 141.07506"><path style="opacity:1;fill:' + nodeColor.stroke + ';fill-opacity:1;stroke:none;" d="M 50,4.4085955 102.90315,70.537531 50,136.66646 -2.9031496,70.537531 Z" class="stroke" /><path class="fill" d="M 50,29.390638 82.917516,70.537531 50,111.68443 17.082484,70.537531 Z" style="opacity:1;fill:' + nodeColor.fill + ';fill-opacity:1;stroke:none;" /></svg>';
         return new ol.style.Style({
           image: new ol.style.Icon({
             src: 'data:image/svg+xml;utf8,' + nodeMarker
@@ -54,7 +50,7 @@
     };
 
     return {
-      createNodeMarker: createNodeMarker
+      createNodeMarker: createNodeMarker // TODO Take in use in the new layer: NodeLayer.js
     };
   };
 }(this));
