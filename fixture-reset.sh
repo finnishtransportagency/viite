c169a76b6e90452fb8811a374a428844dc85b1e0
Pedro Lourenco <pedro.m.lourenco@cgi.com>
2019-03-05 17:51:18 +0200

    VIITE-1701 echo debug

--- fixture-reset.sh
+++ fixture-reset.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+echo ${2} ${3} 'project digiroad2-viite' "test:run-main fi.liikennevirasto.viite.util.DataFixture ${1}"
 ./sbt ${2} ${3} 'project digiroad2-viite' "test:run-main fi.liikennevirasto.viite.util.DataFixture ${1}"
