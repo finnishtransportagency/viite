0550058402f1ff0b7c75a2bcdd9d10113dfb1cd0
Pedro Lourenco <pedro.m.lourenco@cgi.com>
2019-03-08 12:21:47 +0200

    VIITE-1701 remove old cap configurations

--- config/deploy/production.rb
+++
@@ -1,3 +0,0 @@
-role :app, %w{web@production1}
-role :web, %w{web@production1}
-server 'production1', user: 'web', roles: %w{web app}
--- config/deploy/staging.rb
+++
@@ -1,3 +0,0 @@
-role :app, %w{web@172.17.204.46}
-role :web, %w{web@172.17.204.46}
-server '172.17.204.46', user: 'web', roles: %w{web app}
