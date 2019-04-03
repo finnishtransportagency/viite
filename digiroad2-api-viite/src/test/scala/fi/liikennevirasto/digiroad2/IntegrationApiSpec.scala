c7f5c71c5e5258f631c1f2f3270e5df0dd0a9507
Sami Kosonen <sami.kosonen@cgi.com>
2019-04-03 12:10:59 +0300

    VIITE-1881 Updated test data (same road numbers than before, but including all parts)

--- digiroad2-api-viite/src/test/scala/fi/liikennevirasto/digiroad2/IntegrationApiSpec.scala
+++ digiroad2-api-viite/src/test/scala/fi/liikennevirasto/digiroad2/IntegrationApiSpec.scala
@@ -65,7 +65,7 @@ class IntegrationApiSpec extends FunSuite with ScalatraSuite with BeforeAndAfter
     val roadAdressLink = RoadAddressLink(63298, 0, 5171208, geometry, GeometryUtils.geometryLength(geometry), Municipality,
       UnknownLinkType, InUse, NormalLinkInterface, RoadType.MunicipalityStreetRoad, Some("Vt5"),
       None, BigInt(0), None, None, Map("linkId" -> 5171208, "segmentId" -> 63298), 5, 205, 1, 0, 0, 0, 6, "2015-01-01",
-      "2016-01-01", 0.0, 0.0, SideCode.TowardsDigitizing, Some(CalibrationPoint(120, 1, 2)), None, Anomaly.None, 0)
+      "2015-12-31", 0.0, 0.0, SideCode.TowardsDigitizing, Some(CalibrationPoint(120, 1, 2)), None, Anomaly.None, 0)
     integrationApi.roadAddressLinksToApi(Seq(roadAdressLink)) should be(Seq(Map(
       "muokattu_viimeksi" -> "",
       "geometryWKT" -> "LINESTRING ZM (0.000 0.000 0.000 0.000, 1.000 0.000 0.500 1.000, 4.000 4.000 1.500 6.000)",
