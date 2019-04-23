ab1dafa1a285781a25cf465fb7803fa228a8f4c3
andre-policarpo <andre.policarpo@cgi.com>
2019-04-23 12:46:54 +0300

    VIITE-1885 some code was missing

--- viite-UI/src/view/ProjectEditForm.js
+++ viite-UI/src/view/ProjectEditForm.js
@@ -248,9 +248,9 @@
         formCommon.replaceAddressInfo(backend, selectedProjectLink, currentProject.project.id);
         checkInputs('.project-');
         changeDropDownValue(selectedProjectLink[0].status);
-        editDropDown(_.filter(currentProject.getF, function(formedLink) {
+        editDropDown(_.filter(currentProject.getFormedParts(), function(formedLink) {
           return formedLink.roadNumber === selectedProjectLink[0].roadNumber && formedLink.roadPartNumber === selectedProjectLink[0].roadPartNumber;
-        }).length);
+        }).size());
         disableFormInputs();
         var selectedDiscontinuity = _.max(selectedProjectLink, function(projectLink){
           return projectLink.endAddressM;
