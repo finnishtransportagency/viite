f56b27c6a81461a0a7362c454c8d1beef0475648
Campanha <ricardo.campanha.marques@gi.com>
2019-04-29 17:40:27 +0300

    VIITE-1916 added logger info

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/RoadNetworkService.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/RoadNetworkService.scala
@@ -196,7 +196,7 @@ class RoadNetworkService {
           val (combinedRoadways, twoTrackRoadways) = roadway.partition(_.track == Combined)
           val (leftRoadways, rightRoadways) = twoTrackRoadways.partition(_.track == LeftSide)
 
-          logger.info(s"linearLocationsInChunk: " + linearLocationsInChunk)
+          logger.info(s"linearLocationsInChunk size: " + linearLocationsInChunk.size)
           val leftTrackLinearLocations = leftRoadways.flatMap(r => linearLocationsInChunk.get(r.roadwayNumber)).flatten
           val rightTrackLinearLocations = rightRoadways.flatMap(r => linearLocationsInChunk.get(r.roadwayNumber)).flatten
           val combinedLinearLocations = combinedRoadways.flatMap(r => linearLocationsInChunk.get(r.roadwayNumber)).flatten
