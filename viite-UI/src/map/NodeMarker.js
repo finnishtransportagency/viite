cb1c4bbb74ef4d68a4878f06ed0c4dafc34419f5
hestrompa <hugo.estrompa@cgi.com>
2019-05-10 13:44:41 +0300

    NodeMarker file changes

--- viite-UI/src/map/NodeMarker.js
+++ viite-UI/src/map/NodeMarker.js
@@ -1,31 +1,23 @@
 (function (root) {
   root.NodeMarker = function () {
-
     var createNodeMarker = function (node) {
-      var box = new ol.Feature({
-        geometry: new ol.geom.Point([node.x, node.y]), // TODO assuming that node has x and y coordinates
-        nodeId: node.id,
-        type: "marker"
+      var marker = new ol.Feature({
+        geometry: new ol.geom.Point([node.coordX, node.coordY])
       });
 
-      var boxStyleNode = function (node) {
-        return new ol.style.Style({
-          image: new ol.style.Icon({
-            src: 'images/node-sprite.svg#' + node.type,
-            anchor: [0.5, 1]
-          }),
-          zIndex: 10
-        });
-      };
+      var nodeMarkerStyle = new ol.style.Style({
+        image: new ol.style.Icon({
+          src: 'images/node-sprite.svg#' + node.type,
+          anchor: [0.5, 1]
+        })
+      });
 
-      box.setStyle(boxStyleNode(node));
-      box.id = node.id;
-      box.linkData = node;
-      return box;
+      marker.setStyle(nodeMarkerStyle);
+      return marker;
     };
 
     return {
-      createNodeMarker: createNodeMarker // TODO Take in use in the new layer: NodeLayer.js
+      createNodeMarker: createNodeMarker
     };
   };
 }(this));
