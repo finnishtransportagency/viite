42f1a9c68af9a47ae8eefa2bf2e05c259667e91b
Campanha <ricardo.campanha.marques@gi.com>
2019-05-16 19:20:33 +0300

    VIITE-1913 solving merge conflicts

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectValidator.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectValidator.scala
@@ -84,7 +84,7 @@ class ProjectValidator {
   }
 
   def checkReservedPartInProject(number: Long, part: Long, currentProject: Project, linkStatus: LinkStatus): Unit = {
-    if (LinkStatus.Transfer.value == linkStatus.value && !currentProject.formedParts.map(fp => (fp.roadNumber, fp.roadPartNumber)).contains((number, part))) {
+    if (LinkStatus.Transfer.value == linkStatus.value && roadAddressService.getRoadAddressesFiltered(number, part).nonEmpty && !currentProject.formedParts.map(fp => (fp.roadNumber, fp.roadPartNumber)).contains((number, part))) {
       val partInCurrentProject = projectReservedPartDAO.fetchProjectReservedPart(number, part, currentProject.id, withProjectId = Some(true))
       if (partInCurrentProject.isEmpty) {
         throw new ProjectValidationException(RoadPartNotReservedInProjectMessage.format(number, part, currentProject.name))
