8f3198311ad7463d8f0dc3b95bf015bdb39d8ad6
Sami Kosonen <sami.kosonen@cgi.com>
2019-06-11 13:22:35 +0300

    VIITE-1944 Using kmtkdevtest hostname instead of ip.

--- ci/config/deploy/kmtkdevtest.rb
+++ ci/config/deploy/kmtkdevtest.rb
@@ -1,3 +1,3 @@
-role :app, %w{web@172.17.204.35}
-role :web, %w{web@172.17.204.35}
-server '172.17.204.35', user: 'web', roles: %w{web app}
+role :app, %w{web@kmtkdevtest}
+role :web, %w{web@kmtkdevtest}
+server 'kmtkdevtest', user: 'web', roles: %w{web app}
