8bbb14881e4bd0d6d850348565caf1bf81ebeeef
andre-policarpo <andre.policarpo@cgi.com>
2019-06-19 19:39:14 +0300

    VIITE-1979 recalculation added when Numbering

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/ProjectSectionMValueCalculator.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/ProjectSectionMValueCalculator.scala
@@ -51,8 +51,8 @@ object ProjectSectionMValueCalculator {
       val addressValue = if (someCalibrationPoint.nonEmpty) someCalibrationPoint.get.addressMValue else m + pl.geometryLength * coEff
       pl.status match {
         case LinkStatus.New => addressValue
-        case LinkStatus.Transfer | LinkStatus.NotHandled | LinkStatus.Terminated => m + pl.addrMLength
-        case LinkStatus.UnChanged | LinkStatus.Numbering => pl.endAddrMValue
+        case LinkStatus.Transfer | LinkStatus.NotHandled | LinkStatus.Terminated | LinkStatus.Numbering => m + pl.addrMLength
+        case LinkStatus.UnChanged => pl.endAddrMValue
         case _ => throw new InvalidAddressDataException(s"Invalid status found at value assignment ${pl.status}, linkId: ${pl.linkId}")
       }
     }
