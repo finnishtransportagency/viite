73d5be88adddc7bccf64a81a17f713799974b993
Sami Kosonen <sami.kosonen@cgi.com>
2019-07-08 20:03:44 +0300

    VIITE-1985 Test for roadway_point unique constraint.

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/RoadwayPointDAO.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/RoadwayPointDAO.scala
@@ -1,11 +1,11 @@
 package fi.liikennevirasto.viite.dao
 
 import fi.liikennevirasto.digiroad2.dao.Sequences
+import fi.liikennevirasto.viite._
 import org.joda.time.DateTime
 import slick.driver.JdbcDriver.backend.Database.dynamicSession
 import slick.jdbc.StaticQuery.interpolation
 import slick.jdbc.{GetResult, PositionedResult, StaticQuery => Q}
-import fi.liikennevirasto.viite._
 
 case class RoadwayPoint(id: Long, roadwayNumber: Long, addrMValue: Long, createdBy: String, createdTime: Option[DateTime] = None, modifiedBy: Option[String] = None, modifiedTime: Option[DateTime] = None)
 
@@ -51,7 +51,7 @@ class RoadwayPointDAO extends BaseDAO {
 
     val ps = dynamicSession.prepareStatement("update ROADWAY_POINT SET ADDR_M = ?, MODIFIED_BY = ?, MODIFIED_TIME = SYSDATE WHERE ID = ?")
 
-//    disableRoadwayPoint_UK
+    //    disableRoadwayPoint_UK
     roadwayPoints.foreach {
       rwPoint =>
         ps.setLong(1, rwPoint._1)
@@ -61,7 +61,7 @@ class RoadwayPointDAO extends BaseDAO {
     }
     ps.executeBatch()
     ps.close()
-//    enableRoadwayPoint_UK
+    //    enableRoadwayPoint_UK
     roadwayPoints.map(_._1)
   }
 
@@ -71,17 +71,6 @@ class RoadwayPointDAO extends BaseDAO {
       """.execute
   }
 
-  def disableRoadwayPoint_UK = {
-    sqlu"""
-          ALTER TABLE ROADWAY_POINT DISABLE CONSTRAINT ROADWAY_POINT_UK1
-      """.execute
-  }
-
-  def enableRoadwayPoint_UK = {
-    sqlu"""
-          ALTER TABLE ROADWAY_POINT ENABLE CONSTRAINT ROADWAY_POINT_UK1
-      """.execute
-  }
   def fetch(id: Long): RoadwayPoint = {
     sql"""
       SELECT ID, ROADWAY_NUMBER, ADDR_M, CREATED_BY, CREATED_TIME, MODIFIED_BY, MODIFIED_TIME
