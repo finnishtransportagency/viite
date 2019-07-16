6632a8f4361cd90d7e79647a71e47a52f463d5b1
Sami Kosonen <sami.kosonen@cgi.com>
2019-07-16 14:40:54 +0300

    Formatting

--- viite-UI/src/map/JunctionPointTemplateMarker.js
+++ viite-UI/src/map/JunctionPointTemplateMarker.js
@@ -3,12 +3,11 @@
     var createJunctionPointTemplateMarker = function (junctionPoint, roadLink) {
       var point = [];
 
-      if((roadLink.sideCode === LinkValues.SideCode.TowardsDigitizing.value && junctionPoint.addrM === roadLink.endAddressM) ||(roadLink.sideCode === LinkValues.SideCode.AgainstDigitizing.value && junctionPoint.addrM === roadLink.startAddressM)){
-        point = roadLink.points[roadLink.points.length -1];
-      }
-      else
+      if ((roadLink.sideCode === LinkValues.SideCode.TowardsDigitizing.value && junctionPoint.addrM === roadLink.endAddressM) || (roadLink.sideCode === LinkValues.SideCode.AgainstDigitizing.value && junctionPoint.addrM === roadLink.startAddressM)) {
+        point = roadLink.points[roadLink.points.length - 1];
+      } else {
         point = roadLink.points[0];
-
+      }
 
       var marker = new ol.Feature({
         geometry: new ol.geom.Point([point.x, point.y])
