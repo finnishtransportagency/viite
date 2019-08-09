2e310e5a30f616abecf22e43e3ab5efce8e668c3
andre-policarpo <andre.policarpo@cgi.com>
2019-08-09 16:03:01 +0300

    VIITE-2032 major refactor to openLayers features and node tool mode

--- viite-UI/src/model/SelectedNodePointTemplate.js
+++ viite-UI/src/model/SelectedNodePointTemplate.js
@@ -1,14 +1,47 @@
 (function(root) {
-  root.selectedNodePointTemplate = function(nodeCollection) {
+  root.SelectedNodePointTemplate = function(nodeCollection) {
     var current = [];
     var me = this;
 
     var open = function (node) {
-      alert('a node has been clicked!');
+      setCurrent(node);
+      alert('a node has been clicked! ' + node.node_number);
+    };
+
+    var setCurrent = function(newSelection) {
+      current = newSelection;
+    };
+
+    var getCurrent = function () {
+      return current;
+    };
+
+    var isDirty = function() {
+      return dirty;
+    };
+
+    var setDirty = function(value) {
+      dirty = value;
+    };
+
+
+    var clean = function() {
+      current = [];
+    };
+
+    var close = function() {
+      clean();
+      eventbus.trigger('layer:enableButtons', true);
     };
 
     return {
-      open: open
+      open: open,
+      setCurrent: setCurrent,
+      getCurrent: getCurrent,
+      isDirty: isDirty,
+      setDirty: setDirty,
+      clean: clean,
+      close: close
     };
 
   };
