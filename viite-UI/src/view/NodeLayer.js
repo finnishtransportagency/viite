1d23204bb882860a2ac0effbb427d7e8eeb2f935
Campanha <ricardo.campanha.marques@gi.com>
2019-10-09 13:47:06 +0300

    Fix new lodash alias unique -> uniq

--- viite-UI/src/view/NodeLayer.js
+++ viite-UI/src/view/NodeLayer.js
@@ -160,7 +160,7 @@
       });
 
       var selectNode = function(selectedNode) {
-        var node = _.head(_.unique(_.map(selectedNode, "nodeInfo"), "id"));
+        var node = _.head(_.uniq(_.map(selectedNode, "nodeInfo"), "id"));
         selectedNodeAndJunctionPoint.openNode(node);
         highlightNode(node.id);
         highlightJunctions(node.id);
@@ -170,11 +170,11 @@
       };
 
       var selectNodePointTemplate = function(selectedNodePointTemplates) {
-        selectedNodeAndJunctionPoint.openNodePointTemplate(_.unique(_.map(selectedNodePointTemplates, "nodePointTemplateInfo"), "id"));
+        selectedNodeAndJunctionPoint.openNodePointTemplate(_.uniq(_.map(selectedNodePointTemplates, "nodePointTemplateInfo"), "id"));
       };
 
       var selectJunctionTemplate = function (selectedJunctionPointTemplates) {
-        selectedNodeAndJunctionPoint.openJunctionTemplate(_.head(_.unique(_.map(selectedJunctionPointTemplates, "junctionTemplateInfo"), "junctionId")));
+        selectedNodeAndJunctionPoint.openJunctionTemplate(_.head(_.uniq(_.map(selectedJunctionPointTemplates, "junctionTemplateInfo"), "junctionId")));
       };
 
       var selectFeaturesToHighlight = function (vector, featuresToHighlight, otherFeatures) {
