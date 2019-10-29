7adf22f4b1dc943f1e9a1eac119493d294d8dace
Campanha <ricardo.campanha.marques@gi.com>
2019-10-29 20:45:38 +0200

    Downgrade grunt libs

--- gruntfile.js
+++ gruntfile.js
@@ -70,15 +70,13 @@ module.exports = function(grunt) {
           port: 9003,
           base: ['dist', '.', 'viite-UI'],
           middleware: function(connect, opts) {
-            var serveStatic = require('serve-static');
-            var serveIndex = require('serve-index');
             var config = [
-              // Serve static files
-              serveStatic(opts.base[0]),
-              serveStatic(opts.base[1]),
-              serveStatic(opts.base[2]),
+              // Serve static files.
+              connect.static(opts.base[0]),
+              connect.static(opts.base[1]),
+              connect.static(opts.base[2]),
               // Make empty directories browsable.
-              serveIndex(opts.base[2], {'icons': true})
+              connect.directory(opts.base[2])
             ];
             var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
             config.unshift(proxy);
