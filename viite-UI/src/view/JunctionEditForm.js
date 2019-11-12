6ed525660c27514de8752b7f82bbd6dbbb810fc4
Sami Kosonen <sami.kosonen@cgi.com>
2019-11-12 14:51:37 +0200

    Fixed Junction form Junction number input field style.

--- viite-UI/src/view/JunctionEditForm.js
+++ viite-UI/src/view/JunctionEditForm.js
@@ -23,7 +23,7 @@
       return '<div class="form-group-junction-input-metadata">' +
         '<p class="form-control-static asset-junction-data">' +
         '<label class="required">' + labelText + '</label>' +
-        '<input type="text" class="form-control-static asset-input-junction-data" id = "' + id + '"' + lengthLimit + ' placeholder = "' + placeholder + '" value="' + value + '" disabled/>' +
+        '<input type="text" class="form-control asset-input-junction-data" id = "' + id + '"' + lengthLimit + ' placeholder = "' + placeholder + '" value="' + value + '" disabled/>' +
         '</p>' +
         '</div>';
     };
