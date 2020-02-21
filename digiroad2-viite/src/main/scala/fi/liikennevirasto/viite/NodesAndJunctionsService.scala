c0a6bcb88cd28428134e1a99da27b3c58fc84f6f
Campanha <ricardo.campanha.marques@gi.com>
2020-02-21 19:51:12 +0200

    VIIET-2249 refactor

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/NodesAndJunctionsService.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/NodesAndJunctionsService.scala
@@ -693,6 +693,7 @@ class NodesAndJunctionsService(roadwayDAO: RoadwayDAO, roadwayPointDAO: RoadwayP
               Seq.empty[JunctionPoint]
             else
               junctionPointsToCheck
+          case _ => Seq.empty[JunctionPoint]
         }
         affectedJunctionsPoints
       }
