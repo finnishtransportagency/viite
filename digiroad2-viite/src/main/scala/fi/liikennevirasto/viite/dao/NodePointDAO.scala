0ec82fae0be350e62371c5869dde40fc609c723e
Sami Kosonen <sami.kosonen@cgi.com>
2020-03-04 16:50:32 +0200

    VIITE-2327 Fixed tests.
    (cherry picked from commit 785fcdebaecd6e40f2a073a8c73e3c93842dc83a)

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/NodePointDAO.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/NodePointDAO.scala
@@ -242,8 +242,8 @@ class NodePointDAO extends BaseDAO {
          FROM NODE_POINT NP
          JOIN ROADWAY_POINT RP ON (RP.ID = ROADWAY_POINT_ID)
          JOIN LINEAR_LOCATION LL ON (LL.ROADWAY_NUMBER = RP.ROADWAY_NUMBER AND LL.VALID_TO IS NULL)
-         JOIN ROADWAY RW ON (RP.ROADWAY_NUMBER = RW.ROADWAY_NUMBER)
-         where NP.id = $id AND NP.node_number is null and NP.valid_to is null and RW.end_date is null
+         JOIN ROADWAY RW ON (RP.ROADWAY_NUMBER = RW.ROADWAY_NUMBER AND RW.end_date is NULL AND RW.VALID_TO IS NULL)
+         where NP.id = $id AND NP.node_number is null and NP.valid_to is null
        """
     queryList(query).headOption
   }
@@ -262,8 +262,8 @@ class NodePointDAO extends BaseDAO {
           FROM NODE_POINT NP
           JOIN ROADWAY_POINT RP ON (RP.ID = ROADWAY_POINT_ID)
           JOIN LINEAR_LOCATION LL ON (LL.ROADWAY_NUMBER = RP.ROADWAY_NUMBER AND LL.VALID_TO IS NULL)
-          LEFT JOIN ROADWAY RW ON (RP.ROADWAY_NUMBER = RW.ROADWAY_NUMBER)
-          where $boundingBoxFilter AND NP.node_number is null and NP.valid_to is null and RW.end_date is null
+          JOIN ROADWAY RW ON (RP.ROADWAY_NUMBER = RW.ROADWAY_NUMBER AND RW.VALID_TO IS NULL)
+          where $boundingBoxFilter AND NP.node_number is null and NP.valid_to is null
         """
       queryList(query)
     }
