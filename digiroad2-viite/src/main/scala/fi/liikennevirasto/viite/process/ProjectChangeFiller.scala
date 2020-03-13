53ac98586ecff6c72e002ed31f8194eb81283c1a
Campanha <ricardo.campanha.marques@gi.com>
2020-03-13 22:31:09 +0200

    VIITE-2336 terminated links should not be mapped since road addresses will not exist

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/ProjectChangeFiller.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/ProjectChangeFiller.scala
@@ -1,7 +1,7 @@
 package fi.liikennevirasto.viite.process
 
 import fi.liikennevirasto.viite.ProjectRoadLinkChange
-import fi.liikennevirasto.viite.dao.{ProjectLink, ProjectRoadwayChange, RoadAddress}
+import fi.liikennevirasto.viite.dao.{LinkStatus, ProjectLink, ProjectRoadwayChange, RoadAddress}
 
 object ProjectChangeFiller {
   def mapLinksToChanges(roadwayChanges: List[ProjectRoadwayChange], roadwayProjectLinkIds: Seq[(Long, Long)], projectLinks: Seq[ProjectLink], projectLinkChanges: Seq[ProjectRoadLinkChange]): Seq[(ProjectRoadwayChange, Seq[ProjectLink])] = {
@@ -18,14 +18,16 @@ object ProjectChangeFiller {
   }
 
   def mapAddressProjectionsToLinks(roadwayLinks: Seq[ProjectLink], projectLinkChanges: Seq[ProjectRoadLinkChange], mappedRoadAddressesProjection: Seq[RoadAddress]): (Seq[ProjectLink], Seq[ProjectRoadLinkChange]) = {
-    val enrichedProjectLinks = roadwayLinks.map{ l=>
+    val (terminatedRoadwayLinks, validRoadwayLinks) = roadwayLinks.partition(_.status == LinkStatus.Terminated)
+    val enrichedProjectLinks = validRoadwayLinks.map{ l=>
       val ra = mappedRoadAddressesProjection.find(_.linearLocationId == l.linearLocationId).get
       l.copy(startAddrMValue = ra.startAddrMValue, endAddrMValue = ra.endAddrMValue)
-    }
-    val enrichedProjectRoadLinkChanges = projectLinkChanges.map{ rlc =>
+    } ++ terminatedRoadwayLinks
+    val (terminatedProjectLinkChanges, validProjectLinkChanges) = projectLinkChanges.partition(_.status == LinkStatus.Terminated)
+    val enrichedProjectRoadLinkChanges = validProjectLinkChanges.map{ rlc =>
       val pl: ProjectLink = enrichedProjectLinks.find(_.id == rlc.id).get
       rlc.copy(linearLocationId = pl.linearLocationId, newStartAddr = pl.startAddrMValue, newEndAddr = pl.endAddrMValue)
-    }
+    } ++ terminatedProjectLinkChanges
     (enrichedProjectLinks, enrichedProjectRoadLinkChanges)
   }
 }
