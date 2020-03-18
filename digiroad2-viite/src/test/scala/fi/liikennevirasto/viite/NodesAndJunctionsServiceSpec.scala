693c918ada56e1b832acc5c3974b9a293bd8adc8
Campanha <ricardo.campanha.marques@gi.com>
2020-03-18 14:24:24 +0200

    VIITE-2313 added test

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/NodesAndJunctionsServiceSpec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/NodesAndJunctionsServiceSpec.scala
@@ -4150,7 +4150,7 @@ class NodesAndJunctionsServiceSpec extends FunSuite with Matchers with BeforeAnd
     runWithRollback {
       val node = Node(NewIdValue, Sequences.nextNodeNumber, Point(0, 0), Some("Node name"), NodeType.EndOfRoad,
         DateTime.now().withTimeAtStartOfDay(), None, DateTime.now(), None, "user", Some(DateTime.now()))
-      nodesAndJunctionsService.addOrUpdateNode(node, node.createdBy) should be (node.nodeNumber)
+      nodesAndJunctionsService.addOrUpdateNode(node, false, node.createdBy) should be (node.nodeNumber)
       val fetched = nodeDAO.fetchByNodeNumber(node.nodeNumber).getOrElse(fail("No node found"))
       fetched.startDate should be(node.startDate)
       fetched.nodeType should be(node.nodeType)
@@ -4173,7 +4173,7 @@ class NodesAndJunctionsServiceSpec extends FunSuite with Matchers with BeforeAnd
         DateTime.now().withTimeAtStartOfDay(), None, DateTime.now(), None, "user", Some(DateTime.now()))
       nodeDAO.create(Seq(node), node.createdBy)
       nodeDAO.fetchById(node.id) should not be None
-      nodesAndJunctionsService.addOrUpdateNode(node.copy(coordinates = Point(1, 1)), node.createdBy) should be (node.nodeNumber)
+      nodesAndJunctionsService.addOrUpdateNode(node.copy(coordinates = Point(1, 1)), false, node.createdBy) should be (node.nodeNumber)
       nodeDAO.fetchById(node.id) should be(None)
       val updated = nodeDAO.fetchByNodeNumber(node.nodeNumber).getOrElse(fail("Node not found"))
       updated.id should not be node.id
@@ -4194,7 +4194,7 @@ class NodesAndJunctionsServiceSpec extends FunSuite with Matchers with BeforeAnd
         DateTime.now().withTimeAtStartOfDay(), None, DateTime.now(), None, "user", Some(DateTime.now()))
       nodeDAO.create(Seq(node), node.createdBy)
       nodeDAO.fetchById(node.id) should not be None
-      nodesAndJunctionsService.addOrUpdateNode(node.copy(coordinates = Point(1, 1), nodeType = NodeType.Bridge), node.createdBy) should be (node.nodeNumber)
+      nodesAndJunctionsService.addOrUpdateNode(node.copy(coordinates = Point(1, 1), nodeType = NodeType.Bridge), false, node.createdBy) should be (node.nodeNumber)
       nodeDAO.fetchById(node.id) should be(None)
       val updated = nodeDAO.fetchByNodeNumber(node.nodeNumber).getOrElse(fail("Node not found"))
       updated.id should not be node.id
@@ -4218,7 +4218,7 @@ class NodesAndJunctionsServiceSpec extends FunSuite with Matchers with BeforeAnd
       nodeDAO.create(Seq(node), node.createdBy)
       nodeDAO.fetchById(node.id) should not be None
       nodesAndJunctionsService.addOrUpdateNode(node.copy(startDate = DateTime.now().plusDays(1).withTimeAtStartOfDay(),
-        nodeType = NodeType.Bridge), node.createdBy) should be (node.nodeNumber)
+        nodeType = NodeType.Bridge), false, node.createdBy) should be (node.nodeNumber)
       nodeDAO.fetchById(node.id) should be(None)
       val updated = nodeDAO.fetchByNodeNumber(node.nodeNumber).getOrElse(fail("Node not found"))
       updated.id should not be node.id
@@ -4231,6 +4231,19 @@ class NodesAndJunctionsServiceSpec extends FunSuite with Matchers with BeforeAnd
       val historyRowEndDate = sql"""SELECT END_DATE from NODE
         WHERE NODE_NUMBER = ${node.nodeNumber} and valid_to is null and end_date is not null""".as[Date].firstOption
       historyRowEndDate should not be None
+    }
+  }
+
+  test("Test addOrUpdate When detached all junctions and nodePoints Then existing node is expired and new created new to history") {
+    runWithRollback {
+      val node = Node(Sequences.nextNodeId, Sequences.nextNodeNumber, Point(0, 0), Some("Node name"), NodeType.EndOfRoad,
+        DateTime.now().withTimeAtStartOfDay(), None, DateTime.now(), None, "user", Some(DateTime.now()))
+      nodeDAO.create(Seq(node), node.createdBy)
+      val junctions = Seq()
+      val nodePoints = Seq()
+      nodeDAO.fetchById(node.id) should not be None
+      nodesAndJunctionsService.addOrUpdate(node.copy(coordinates = Point(1, 1)), junctions, nodePoints, node.createdBy)
+      nodeDAO.fetchById(node.id) should be(None)
     }
   }
 
