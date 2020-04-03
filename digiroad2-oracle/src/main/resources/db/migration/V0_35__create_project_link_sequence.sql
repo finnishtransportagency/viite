85ad4fccd9db56e72fbf2fd5508fa04ed26ceec5
Sami Kosonen <sami.kosonen@cgi.com>
2020-04-03 19:52:30 +0300

    Fixed sequence name.

--- digiroad2-oracle/src/main/resources/db/migration/V0_35__create_project_link_sequence.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V0_35__create_project_link_sequence.sql
@@ -3,5 +3,5 @@ declare
 begin
   select MAX(ID) + 1 into nextId from PROJECT_LINK;
   if nextId IS NULL then nextId := 1; end if;
-  execute immediate 'CREATE SEQUENCE VIITE_PROJECT_LINK_SEQ START WITH ' || nextId || ' CACHE 100 INCREMENT BY 1';
+  execute immediate 'CREATE SEQUENCE PROJECT_LINK_SEQ START WITH ' || nextId || ' CACHE 100 INCREMENT BY 1';
 END;
