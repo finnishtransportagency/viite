fb4896952c28b35740ead69b853895471297688c
Sami Kosonen <sami.kosonen@cgi.com>
2020-04-07 14:14:45 +0300

    Resetting road_name_seq in fixture reset.

--- digiroad2-oracle/sql/test_fixture_sequences.sql
+++ digiroad2-oracle/sql/test_fixture_sequences.sql
@@ -142,3 +142,12 @@ create sequence JUNCTION_POINT_SEQ
   increment by 1
   cache 100
   cycle;
+
+drop sequence ROAD_NAME_SEQ;
+create sequence ROAD_NAME_SEQ
+  minvalue 1
+  maxvalue 999999999999999999999999999
+  start with 1000000
+  increment by 1
+  cache 100
+  cycle;
