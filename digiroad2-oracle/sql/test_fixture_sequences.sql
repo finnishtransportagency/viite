bc44cd03dd6f687366003525ba93ff414a9685db
Sami Kosonen <sami.kosonen@cgi.com>
2020-04-07 16:15:43 +0300

    Resetting PROJECT_CAL_POINT_ID_SEQ in fixture reset.

--- digiroad2-oracle/sql/test_fixture_sequences.sql
+++ digiroad2-oracle/sql/test_fixture_sequences.sql
@@ -44,6 +44,15 @@ create sequence project_link_name_seq
   cache 100
   cycle;
 
+drop sequence PROJECT_CAL_POINT_ID_SEQ;
+create sequence PROJECT_CAL_POINT_ID_SEQ
+  minvalue 1
+  maxvalue 999999999999999999999999999
+  start with 1000000
+  increment by 1
+  cache 100
+  cycle;
+
 drop sequence viite_general_seq;
 create sequence viite_general_seq
   minvalue 1
