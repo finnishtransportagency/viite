de8fd198b0506b50930d7ef9d7d1686a36de371d
Sami Kosonen <sami.kosonen@cgi.com>
2020-04-24 16:09:45 +0300

    Now junction point calibration points are taken in account when calculating addresses.

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadwayAddressMapper.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/RoadwayAddressMapper.scala
@@ -144,8 +144,7 @@ class RoadwayAddressMapper(roadwayDAO: RoadwayDAO, linearLocationDAO: LinearLoca
   private def recursiveMapRoadAddresses(roadway: Roadway, linearLocations: Seq[LinearLocation]): Seq[RoadAddress] = {
 
     def getUntilCalibrationPoint(seq: Seq[LinearLocation]): (Seq[LinearLocation], Seq[LinearLocation]) = {
-      // user calibration points must be considered here in the future
-      val linearLocationsUntilCp = seq.takeWhile(l => l.endCalibrationPoint.isEmpty || l.endCalibrationPoint.isJunctionPointCP)
+      val linearLocationsUntilCp = seq.takeWhile(l => l.endCalibrationPoint.isEmpty)
       val rest = seq.drop(linearLocationsUntilCp.size)
       if (rest.headOption.isEmpty)
         (linearLocationsUntilCp, rest)
