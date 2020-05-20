b3ab8cd22e05972256cdfc4ccfcfc5a3a2fd42d8
Sami Kosonen <sami.kosonen@cgi.com>
2020-05-20 13:32:41 +0300

    VIITE-1905 Working on task deploy scripts and task definition json.

---
+++ aws-deploy-dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Before running this script you must authenticate through Väylä SAML:
+#
+# python3 ~/bin/vaylaAssumeRoleAWSCLI.py --username <Your Väylä username> --account 783354560127 --role ViiteAdmin --region eu-west-1
+#
+set -e
+echo "Building Docker image"
+./aws-build-image.sh
+echo "Logging in to AWS ACR with Docker"
+aws ecr get-login-password --profile vaylaapp --region eu-west-1 | docker login --username AWS --password-stdin 783354560127.dkr.ecr.eu-west-1.amazonaws.com
+echo "Tagging image"
+docker tag vayla/viite:latest 783354560127.dkr.ecr.eu-west-1.amazonaws.com/viite:latest
+echo "Pushing image"
+docker push 783354560127.dkr.ecr.eu-west-1.amazonaws.com/viite:latest
+echo "Registering task definition"
+./aws-register-task-definition-dev.sh
+
+echo "Updating service"
+# Using simply --force-new-deployment, because the service is using always the latest viite container
+aws ecs update-service --profile vaylaapp --region eu-west-1 --cluster VIITE-ECS-Cluster --service viite-dev --force-new-deployment
+echo "Done."
