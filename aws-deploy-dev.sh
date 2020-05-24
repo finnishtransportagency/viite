99991337772f0abd290a3b5abd831c5d5a810aef
Sami Kosonen <sami.kosonen@cgi.com>
2020-05-24 13:49:59 +0300

    VIITE-1905 Health check for task definition.

--- aws-deploy-dev.sh
+++ aws-deploy-dev.sh
@@ -6,7 +6,7 @@
 set -e
 echo "Building Docker image"
 ./aws-build-image.sh
-echo "Logging in to AWS ACR with Docker"
+echo "Logging in to AWS ECR with Docker"
 aws ecr get-login-password --profile vaylaapp --region eu-west-1 | docker login --username AWS --password-stdin 783354560127.dkr.ecr.eu-west-1.amazonaws.com
 echo "Tagging image"
 docker tag vayla/viite:latest 783354560127.dkr.ecr.eu-west-1.amazonaws.com/viite:latest
