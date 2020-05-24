4dc6c1c9ff63e04ba406b3e789614bf84b192f86
Sami Kosonen <sami.kosonen@cgi.com>
2020-05-24 23:25:19 +0300

    VIITE-1905 AdminAPI.

--- digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/DatabaseMigration.scala
+++ digiroad2-oracle/src/test/scala/fi/liikennevirasto/digiroad2/util/DatabaseMigration.scala
@@ -1,10 +1,14 @@
 package fi.liikennevirasto.digiroad2.util
 
-import org.flywaydb.core.Flyway
 import fi.liikennevirasto.digiroad2.oracle.OracleDatabase._
+import org.flywaydb.core.Flyway
 
 object DatabaseMigration {
-  def main(args: Array[String]) : Unit = {
+  def main(args: Array[String]): Unit = {
+    migrate
+  }
+
+  def migrate: Unit = {
     val configuration = Flyway.configure.
       dataSource(ds).
       locations("db.migration")
