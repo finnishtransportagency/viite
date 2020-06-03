83b5a35a2ace248291273e1708ab96048a577728
Sami Kosonen <sami.kosonen@cgi.com>
2020-06-03 12:47:45 +0300

    VIITE-2431 Parsing user from the JWT.

--- digiroad2-api-common/src/test/scala/fi/liikennevirasto/digiroad2/authentication/JWTReaderSpec.scala
+++ digiroad2-api-common/src/test/scala/fi/liikennevirasto/digiroad2/authentication/JWTReaderSpec.scala
@@ -1,40 +1,12 @@
 package fi.liikennevirasto.digiroad2.authentication
 
 import org.scalatest.{FunSuite, Matchers}
-import pdi.jwt.{Jwt, JwtAlgorithm}
 
-// TODO Prepare proper private and public keys https://kjur.github.io/jsjws/tool_jwt.html
 class JWTReaderSpec extends FunSuite with Matchers {
 
-  val secretKey = "secretKeySecretKey"
-
-  test("Test getUsername When empty Then is failure.") {
-    JWTReader.decodeToken("", secretKey).isFailure should be(true)
+  test("Test parseUsernameFromJWTPayloadJSONString When valid JSON Then return username") {
+    val json = """{"custom:rooli":"int_kayttajat,Extranet_Kayttaja,arn:aws:iam::117531223221:role/ViiteAdmin\\,arn:aws:iam::117531223221:saml-provider/VaylaTestOAM","sub":"2b5a2b65-ca06-46e2-8a52-a5190b495d12","email_verified":"false","custom:uid":"K567997","email":"sami.kosonen@cgi.com","username":"vaylatestoam_sami.kosonen@cgi.com","exp":1591117019,"iss":"https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_oNzPsiXEJ"}"""
+    JWTReader.parseUsernameFromJWTPayloadJSONString(json) should be("K567997")
   }
 
-  test("Test getUsername When wrong secret key Then is failure.") {
-    val token = Jwt.encode("""{"user":1}""", "anotherSecretKey", JwtAlgorithm.RS256)
-    assertThrows[java.security.spec.InvalidKeySpecException] {
-      JWTReader.decodeToken(token, secretKey).isFailure should be(true)
-    }
-  }
-
-  test("Test getUsername When wrong algorithm Then is failure.") {
-    val token = Jwt.encode("""{"user":1}""", secretKey, JwtAlgorithm.RS384)
-    assertThrows[java.lang.IllegalArgumentException] {
-      JWTReader.decodeToken(token, secretKey).isFailure should be(true)
-    }
-  }
-
-  test("Test getUsername When token given Then return user.") {
-    val token = Jwt.encode("""{"user":"test"}""", secretKey, JwtAlgorithm.RS256)
-    JWTReader.decodeToken(token, secretKey) should be("""{"user":"test"}""")
-  }
-
-  test("Test getAccessToken When header given Then return access token") {
-    val header = "eyJraWQiOiJaK3R2MTBSQXNDcnRjU2U0YTlUR3NPTTdHc3ltMEU2VUwzT3JLZm9BYVFrPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIyYjVhMmI2NS1jYTA2LTQ2ZTItOGE1Mi1hNTE5MGI0OTVkMTIiLCJjb2duaXRvOmdyb3VwcyI6WyJldS13ZXN0LTFfb056UHNpWEVKX1ZheWxhVGVzdE9BTSJdLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCIsImF1dGhfdGltZSI6MTU5MDc1OTY4NywiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmV1LXdlc3QtMS5hbWF6b25hd3MuY29tXC9ldS13ZXN0LTFfb056UHNpWEVKIiwiZXhwIjoxNTkwNzYzMjg3LCJpYXQiOjE1OTA3NTk2ODcsInZlcnNpb24iOjIsImp0aSI6IjFmMTVkNzQyLTY4YzctNGExYS04ODNhLWIxNDg5YWYzY2I0OCIsImNsaWVudF9pZCI6IjNjdGMyMGQzaTRnaHY5NGtzMHNlbXQ0ZTE1IiwidXNlcm5hbWUiOiJ2YXlsYXRlc3RvYW1fc2FtaS5rb3NvbmVuQGNnaS5jb20ifQ.N4Qg2ZfKGlWFwGZprBp_l3tzHX22k3IGqgv00ZJJckPtXfo87t88kAA-5itJq0-JCsAT9fzc0-syuCx_WbngzKppHH_De6NuAkMZuFk22AsOvsqcGldsX2z-wu8VM_GAYmCVTLV6Yf_6zWT5VtoxY_8Ng-hqEWDKra4-9UD8_iORE3kRX1SM3UXREjZ5I9nCh-CKrem0ea0BWAb4pTyNmAFFf7WlcWvIuaJEd-KknVGKrHQDx0cB-aUv0kRN6DeakQX6OCqoMbPFRvRMmKJAKa5RIISFsTI7m_BUxQ6vmN7JsN6mVgcprcQHNtfymngg3KmGJfew-W-_Zinf1lhCLQ"
-    val accessToken = JWTReader.getAccessToken(header)
-    accessToken.alg should be("RS256")
-    accessToken.kid should be("Z+tv10RAsCrtcSe4a9TGsOM7Gsym0E6UL3OrKfoAaQk=")
-  }
 }
