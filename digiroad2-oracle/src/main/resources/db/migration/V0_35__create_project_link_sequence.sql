e9800d9b01991c697981e0e54cc50b438a91d01e
Sami Kosonen <sami.kosonen@cgi.com>
2020-06-24 11:39:34 +0300

    VIITE-2456 Started Flyway history from the beginning.

--- digiroad2-oracle/src/main/resources/db/migration/V0_35__create_project_link_sequence.sql
+++
@@ -1,7 +0,0 @@
-declare
-  nextId number;
-begin
-  select MAX(ID) + 1 into nextId from PROJECT_LINK;
-  if nextId IS NULL then nextId := 1; end if;
-  execute immediate 'CREATE SEQUENCE PROJECT_LINK_SEQ START WITH ' || nextId || ' CACHE 100 INCREMENT BY 1';
-END;
