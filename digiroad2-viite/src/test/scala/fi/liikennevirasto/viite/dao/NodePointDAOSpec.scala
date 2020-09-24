2c14bed94edbe3420e01f47d56c52d2d98fb9c98
Sami Kosonen <skosonen@users.noreply.github.com>
2020-09-24 10:42:25 +0300

    Formatting

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/NodePointDAOSpec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/NodePointDAOSpec.scala
@@ -38,7 +38,7 @@ class NodePointDAOSpec extends FunSuite with Matchers {
   val testNode1 = Node(NewIdValue, NewIdValue, Point(100, 100), Some("Test node 1"), NodeType.NormalIntersection,
     DateTime.parse("2019-01-01"), None, DateTime.parse("2019-01-01"), None, "Test", None, registrationDate = new DateTime())
 
-  val testLinearLocation1 = LinearLocation(NewIdValue, 1, 1000l, KMTKID("1000",0), 0.0, 2.8, SideCode.TowardsDigitizing, 10000000000l,
+  val testLinearLocation1 = LinearLocation(NewIdValue, 1, 1000l, KMTKID("1000", 0), 0.0, 2.8, SideCode.TowardsDigitizing, 10000000000l,
     (CalibrationPointReference.None, CalibrationPointReference.None), Seq(Point(99.0, 99.0), Point(101.0, 101.0)), LinkGeomSource.NormalLinkInterface, -1)
 
 
