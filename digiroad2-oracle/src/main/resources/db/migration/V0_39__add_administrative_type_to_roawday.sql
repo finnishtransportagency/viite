91b6e0fc35e77ddf2e9b90465a682d29cee9b969
Lauri Litmanen <lauri.litmanen@sitowise.com>
2021-02-23 14:09:56 +0200

    Modified migration script. Changed the column name to administrative_class instead of administrative_type, Fixed the script to update administrative class values correctly on roadway table,  create old_administrative_class and new_administrative_class columns on roadway_changes table. Update road_type values to be 1, 3 or 5.

--- digiroad2-oracle/src/main/resources/db/migration/V0_39__add_administrative_type_to_roawday.sql
+++ digiroad2-oracle/src/main/resources/db/migration/V0_39__add_administrative_type_to_roawday.sql
@@ -1,20 +1,68 @@
--- Add ADMINISTRATIVE_TYPE column to ROADWAY table.
+-- Add ADMINISTRATIVE_CLASS column to ROADWAY table.
 ALTER TABLE "ROADWAY"
-    ADD (ADMINISTRATIVE_TYPE NUMBER(2,0));
-COMMENT ON COLUMN ROADWAY.ADMINISTRATIVE_TYPE IS 'Tells who is responsible of the roadkeeping. Allowed values: 1=Kunta, 2=Valtio, 3=Yksityinen.  Replaces functionality of the old ROAD_TYPE since 2021-02.';
+    ADD (ADMINISTRATIVE_CLASS NUMBER(2,0));
+COMMENT ON COLUMN ROADWAY.ADMINISTRATIVE_CLASS IS 'Tells who is responsible of the roadkeeping. Allowed values: 1=Valtio, 2=Kunta, 3=Yksityinen.  Replaces functionality of the old ROAD_TYPE since 2021-02.';
 
--- We may not have NULL values. Set ADMINISTRATIVE_TYPE column values based on old ROAD_TYPE column values.
+-- We may not have NULL values. Set ADMINISTRATIVE_CLASS column values based on old ROAD_TYPE column values.
 UPDATE "ROADWAY"
-SET ADMINISTRATIVE_TYPE = 1 -- Kunta
-WHERE ROAD_TYPE IN (3); -- kunnan katuosuus (3)
-
-UPDATE "ROADWAY"
-SET ADMINISTRATIVE_TYPE = 2 -- Valtio
+SET ADMINISTRATIVE_CLASS = 1 -- Valtio
 WHERE ROAD_TYPE IN (1,2,4); -- maantie (1), lauttaväylä maantiellä (2), maantien työmaa (4)
 
 UPDATE "ROADWAY"
-SET ADMINISTRATIVE_TYPE = 3 -- Yksityinen
+SET ADMINISTRATIVE_CLASS = 2 -- Kunta
+WHERE ROAD_TYPE IN (3); -- kunnan katuosuus (3)
+
+UPDATE "ROADWAY"
+SET ADMINISTRATIVE_CLASS = 3 -- Yksityinen
 WHERE ROAD_TYPE IN (5,9,99); -- yksityistie (5), omistaja selvittämättä (9), ei määritelty (99)
 
--- Last, set NOT NULL constraint
-ALTER TABLE "ROADWAY" MODIFY (ADMINISTRATIVE_TYPE NUMBER(2,0) NOT NULL);
+-- set NOT NULL constraint
+ALTER TABLE "ROADWAY" MODIFY (ADMINISTRATIVE_CLASS NUMBER(2,0) NOT NULL);
+
+-- Update ROAD_TYPE values to be inline with new logic
+UPDATE "ROADWAY"
+SET ROAD_TYPE = 1 -- Maantie
+WHERE ROAD_TYPE IN (2,4); -- Lauttaväylä maantiellä (2), Maantien työmaa (4)
+
+UPDATE "ROADWAY"
+SET ROAD_TYPE = 5 -- Yksityistie
+WHERE ROAD_TYPE IN (9,99); -- Omistaja selvittämättä (9), Ei määritelty (99)
+
+-- Add OLD_ADMINISTRATIVE_CLASS column to ROADWAY_CHANGES table
+ALTER TABLE "ROADWAY_CHANGES"
+    ADD (OLD_ADMINISTRATIVE_CLASS NUMBER(2,0));
+COMMENT ON COLUMN ROADWAY_CHANGES.OLD_ADMINISTRATIVE_CLASS IS 'Tells who is responsible of the roadkeeping. Allowed values: 1=Valtio, 2=Kunta, 3=Yksityinen.  Replaces functionality of the old ROAD_TYPE since 2021-02.';
+
+-- We may not have NULL values. Set OLD_ADMINISTRATIVE_CLASS column values based on old OLD_ROAD_TYPE column values.
+UPDATE "ROADWAY_CHANGES"
+SET OLD_ADMINISTRATIVE_CLASS = 1 -- Valtio
+WHERE OLD_ROAD_TYPE IN (1,2,4); -- maantie (1), lauttaväylä maantiellä (2), maantien työmaa (4)
+
+UPDATE "ROADWAY_CHANGES"
+SET OLD_ADMINISTRATIVE_CLASS = 2 -- Kunta
+WHERE OLD_ROAD_TYPE IN (3); -- kunnan katuosuus (3)
+
+UPDATE "ROADWAY_CHANGES"
+SET OLD_ADMINISTRATIVE_CLASS = 3 -- Yksityinen
+WHERE OLD_ROAD_TYPE IN (5,9,99); -- yksityistie (5), omistaja selvittämättä (9), ei määritelty (99)
+
+-- Add NEW_ADMINISTRATIVE_CLASS column to ROADWAY_CHANGES table
+ALTER TABLE "ROADWAY_CHANGES"
+    ADD (NEW_ADMINISTRATIVE_CLASS NUMBER(2,0));
+COMMENT ON COLUMN ROADWAY_CHANGES.NEW_ADMINISTRATIVE_CLASS IS 'Tells who is responsible of the roadkeeping. Allowed values: 1=Valtio, 2=Kunta, 3=Yksityinen.  Replaces functionality of the old ROAD_TYPE since 2021-02.';
+
+-- We may not have NULL values. Set OLD_ADMINISTRATIVE_TYPE column values based on old OLD_ROAD_TYPE column values.
+UPDATE "ROADWAY_CHANGES"
+SET NEW_ADMINISTRATIVE_CLASS = 1 -- Valtio
+WHERE NEW_ROAD_TYPE IN (1,2,4); -- maantie (1), lauttaväylä maantiellä (2), maantien työmaa (4)
+
+UPDATE "ROADWAY_CHANGES"
+SET NEW_ADMINISTRATIVE_CLASS = 2 -- Kunta
+WHERE NEW_ROAD_TYPE IN (3); -- kunnan katuosuus (3)
+
+UPDATE "ROADWAY_CHANGES"
+SET NEW_ADMINISTRATIVE_CLASS = 3 -- Yksityinen
+WHERE NEW_ROAD_TYPE IN (5,9,99); -- yksityistie (5), omistaja selvittämättä (9), ei määritelty (99)
+
+-- set NOT NULL constraint
+ALTER TABLE "ROADWAY_CHANGES" MODIFY (NEW_ADMINISTRATIVE_CLASS NUMBER(2,0) NOT NULL);
