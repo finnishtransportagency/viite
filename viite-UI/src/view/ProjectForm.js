005da68f562d33f225c9651bf3111e9390064928
Lauri Litmanen <lauri.litmanen@sitowise.com>
2021-04-07 10:03:52 +0300

    test commit to trigger aws codepipeline

--- viite-UI/src/view/ProjectForm.js
+++ viite-UI/src/view/ProjectForm.js
@@ -385,9 +385,9 @@
       };
 
       var disableAutoComplete = function () {
-        $('[id=nimi]').attr('autocomplete', 'false');
-        $('[id=alkupvm]').attr('autocomplete', 'false');
-        $('[id=lisatiedot]').attr('autocomplete', 'false');
+        $('[id=nimi]').attr('autocomplete', 'off');
+        $('[id=alkupvm]').attr('autocomplete', 'off');
+        $('[id=lisatiedot]').attr('autocomplete', 'off');
       };
 
       eventbus.on('roadAddress:newProject', function () {
