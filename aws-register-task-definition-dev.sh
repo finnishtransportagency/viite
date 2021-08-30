bd99e79f3e0555c3085542dc841d45e2102e92f4
marjukka.kokkonen <marjukka.kokkonen@sitowise.com>
2021-08-30 09:24:25 +0300

    updated documentation: aws-register-task-definition

--- aws-register-task-definition-dev.sh
+++ aws-register-task-definition-dev.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 #
 # Registers new version of the task definition.
-# After running this you need to take this new task definition in use in service.
+# Before this you must be authenticated. After running this you need to take the new task definition in use for the AWS service.
 # If only the Docker container has changed, you don't need to run this.
 # Usually you need to run this if environment variables have changed.
-#
-# Before running this script you must authenticate through Väylä SAML:
+
+
+# === pre-requisite: Authenticate ===
+# Before running this script you must be authenticated for the AWS CLI through Väylä SAML. From your project root, run:
+#  (Note: when using this from windows, e.g. Bash seems to just hang -> use CMD instead)
 #
 # python3 aws/login/vaylaAssumeRoleAWSCLI.py --username <Your Väylä username> --account 783354560127 --role ViiteAdmin --region eu-west-1
-#
+# python aws/login/vaylaAssumeRoleAWSCLI.py --username K407165 --account 783354560127 --role ViiteAdmin --region eu-west-1
+
+
+# === the registration: ===
 aws ecs register-task-definition --profile vaylaapp --region eu-west-1 --cli-input-json file://aws/task-definition/dev/task-definition.json
 
-# After running this script, you can update the service to use this new task definition.
-# Replace the <VERSION> with the new version. You can find the new version number from the JSON returned by the previous command:
+
+# === after registration: take the new task definition into use for the service ===
+# After running the registarion script, you can update the service to use this new task definition.
+# Replace the <VERSION> with the new version. You can find the new version number from the start of the JSON returned by the previous command:
 #         "taskDefinitionArn": "arn:aws:ecs:eu-west-1:783354560127:task-definition/Viite-dev:<VERSION>",
 #
-# aws ecs update-service --profile vaylaapp --region eu-west-1 --cluster VIITE-ECS-Cluster --service viite-dev --task-definition Viite-dev:<VERSION> --force-new-deployment
-#
-# VIITE-1906, To be possibly refined, for the automatic AWS Viite-dev-ALB stack
+# Viite-dev-ALB stack:
 # aws ecs update-service --profile vaylaapp --region eu-west-1 --cluster Viite-ECS-Cluster-Private --service Viite-ECS-Service-Private --task-definition Viite:<VERSION> --force-new-deployment
+#
+# Old, manually crafted Viite service stack:
+# aws ecs update-service --profile vaylaapp --region eu-west-1 --cluster VIITE-ECS-Cluster --service viite-dev --task-definition Viite-dev:<VERSION> --force-new-deployment
