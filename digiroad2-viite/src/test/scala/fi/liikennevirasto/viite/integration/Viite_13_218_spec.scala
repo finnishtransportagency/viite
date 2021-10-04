9f89c919e281736db9eb19f41898752290dc2172
marjukka.kokkonen <marjukka.kokkonen@sitowise.com>
2021-10-04 07:47:29 +0300

    VIITE-2619 TR removal - cosmetics: shortening some rows

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/integration/Viite_13_218_spec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/integration/Viite_13_218_spec.scala
@@ -213,7 +213,10 @@ class Viite_13_218_spec extends FunSuite with Matchers with BeforeAndAfter {
             (t._1, "")
           }) else tt
           val tttt = ttt.map(t => {
-            if (t._1 == "endRoadPartNumber" && t._2.toString.nonEmpty) ("length", ttt.find(_._1 == "endAddressM").get._2.asInstanceOf[Long] - ttt.find(_._1 == "startAddressM").get._2.asInstanceOf[Long]) else t
+            if (t._1 == "endRoadPartNumber" && t._2.toString.nonEmpty)
+              ("length", ttt.find(_._1 == "endAddressM").get._2.asInstanceOf[Long] - ttt.find(_._1 == "startAddressM").get._2.asInstanceOf[Long])
+            else
+              t
           })
           val t    = List(tttt.head, tttt(6), tttt(5), tttt(2), tttt(8), tttt(3), tttt(7), tttt(1), tttt(4))
 
