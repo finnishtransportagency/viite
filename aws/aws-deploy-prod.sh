15ea958fe9efa74c4a133c9a2329c96038571c8b
Lauri Litmanen <lauri.litmanen@sitowise.com>
2022-02-02 15:57:53 +0200

    VIITE-2672 add echo

--- aws/aws-deploy-prod.sh
+++ aws/aws-deploy-prod.sh
@@ -15,5 +15,6 @@ docker tag 783354560127.dkr.ecr.eu-west-1.amazonaws.com/viite-qa:latest 43459927
 echo "Pushing the tagged image to prod ecr repository"
 docker push 434599271542.dkr.ecr.eu-west-1.amazonaws.com/viite-prod:latest
 echo "Done."
-# Ask centralized service to update the service (notes on how to do that are in the readme)
+echo "Ask centralized service to update the service (notes on how to do that are in the readme)"
+
 
