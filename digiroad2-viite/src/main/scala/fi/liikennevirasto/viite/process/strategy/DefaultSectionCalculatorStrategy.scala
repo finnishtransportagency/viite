ce3ea134a4699689fa50f4297c039b15be3ea7bf
a_sitowise <ari.orre@sitowise.com>
2022-07-08 11:16:45 +0300

    Fixes after merge. Removed unchanged validation changes.

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/strategy/DefaultSectionCalculatorStrategy.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/process/strategy/DefaultSectionCalculatorStrategy.scala
@@ -5,14 +5,13 @@ import fi.liikennevirasto.digiroad2.asset.SideCode
 import fi.liikennevirasto.digiroad2.dao.Sequences
 import fi.liikennevirasto.digiroad2.util.{MissingRoadwayNumberException, MissingTrackException, RoadAddressException, Track}
 import fi.liikennevirasto.digiroad2.util.Track.LeftSide
-import fi.liikennevirasto.viite.NewIdValue
+import fi.liikennevirasto.viite.{NewIdValue, UnsuccessfulRecalculationMessage}
 import fi.liikennevirasto.viite.dao.{Discontinuity, _}
 import fi.liikennevirasto.viite.dao.CalibrationPointDAO.CalibrationPointType.UserDefinedCP
 import fi.liikennevirasto.viite.dao.Discontinuity.Continuous
 import fi.liikennevirasto.viite.dao.ProjectCalibrationPointDAO.UserDefinedCalibrationPoint
 import fi.liikennevirasto.viite.process._
 import fi.liikennevirasto.viite.process.strategy.FirstRestSections.{getUpdatedContinuousRoadwaySections, lengthCompare}
-import fi.liikennevirasto.viite.process.ProjectSectionCalculator.throwExceptionWithErrorInfo
 import fi.liikennevirasto.viite.util.TwoTrackRoadUtils
 import fi.liikennevirasto.viite.util.TwoTrackRoadUtils._
 import org.slf4j.LoggerFactory
@@ -150,6 +149,37 @@ class DefaultSectionCalculatorStrategy extends RoadAddressSectionCalculatorStrat
     val assignedContinuousSection =
       assignRoadwayNumbersInContinuousSection(continuousProjectLinksWithDiscontinuity, givenRoadwayNumber)
     (assignedContinuousSection, seq.drop(continuousProjectLinksWithDiscontinuity.size))
+  }
+
+  /***
+   * Fetch project name by project id for logging purpose.
+   * @param projectId
+   * @return Project name for the projectId.
+   */
+  def getProjectNameForLogger(projectId: Long): String = {
+    val projectDAO: ProjectDAO = new ProjectDAO
+    val project                = projectDAO.fetchById(projectId)
+    project match {
+      case Some(p) => p.name
+      case None => ""
+    }
+  }
+
+  /***
+   * Helper function for logging throwing an exception.
+   * Used by calculation helper functions.
+   * @param errorLinks ProjectLinks causing the error.
+   * @param msg Error message for debug console.
+   */
+  def throwExceptionWithErrorInfo(errorLinks: Iterable[ProjectLink], msg: String): Nothing = {
+    val projectId   = errorLinks.head.projectId
+    val projectName = getProjectNameForLogger(projectId)
+    logger.error(
+      s"""$msg
+         |$projectName $projectId
+         | Error links:
+         | $errorLinks""".stripMargin)
+    throw new RoadAddressException(UnsuccessfulRecalculationMessage)
   }
 
   /***
