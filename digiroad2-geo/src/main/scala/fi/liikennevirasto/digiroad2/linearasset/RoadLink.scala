fd9b8475966abdbc47204d700e08d0b9842e2f23
a_sitowise <ari.orre@sitowise.com>
2022-09-22 11:46:49 +0300

    VIITE-2855 Renamed RoadLink vvhTimeStamp to roadLinkTimeStamp

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/RoadLink.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/linearasset/RoadLink.scala
@@ -4,36 +4,21 @@ import fi.liikennevirasto.digiroad2.Point
 import fi.liikennevirasto.digiroad2.asset._
 import org.joda.time.DateTime
 
-import scala.util.Try
-
 trait RoadLinkLike extends PolyLine {
   def linkId:              String
   def municipalityCode:    Int
   def length:              Double
   def administrativeClass: AdministrativeClass
   def trafficDirection:    TrafficDirection
-  def roadNumber:          Option[String]
   def linkSource:          LinkGeomSource
   def attributes:          Map[String, Any]
   def lifecycleStatus:     LifecycleStatus
-  def vvhTimeStamp:        Long
+  def roadLinkTimeStamp:   Long
   def modifiedAt:          Option[String]
 }
 
 case class RoadLink(linkId: String, geometry: Seq[Point], length: Double, administrativeClass: AdministrativeClass, trafficDirection: TrafficDirection, modifiedAt: Option[String] = None, modifiedBy: Option[String], attributes: Map[String, Any] = Map(), lifecycleStatus: LifecycleStatus = LifecycleStatus.InUse, linkSource: LinkGeomSource = LinkGeomSource.NormalLinkInterface, municipalityCode: Int)
-    extends RoadLinkLike {
+  extends RoadLinkLike {
 
-  /* vvhTimeStamp Long format for old vvh Long-type linkId compatibility. */
-  /* TODO: Simplify this
-    * after vvh not used anymore. */ val vvhTimeStamp: Long = Try(new DateTime(attributes.getOrElse("versionstarttime", attributes.get("starttime")).asInstanceOf[String]).getMillis).getOrElse(attributes.getOrElse("LAST_EDITED_DATE", attributes.getOrElse("CREATED_DATE", BigInt(0))).asInstanceOf[BigInt].toLong)
-
-//  def municipalityCode: Int = attributes.getOrElse("municipalitycode", attributes.get("MUNICIPALITYCODE")) match {
-//    /* TODO:
-//          * Simplify this after vvh not used anymore. */ case m: Int => m
-//    case m: String => m.asInstanceOf[String].toInt
-//    case m: BigInt => m.asInstanceOf[BigInt].intValue()
-//    case m: Some[Any] => m.get.toString.toInt
-//  }
-
-  def roadNumber: Option[String] = attributes.getOrElse("roadnumber", attributes.get("ROADNUMBER")).asInstanceOf[Option[String]]
+  def roadLinkTimeStamp: Long = new DateTime(modifiedAt.getOrElse(throw new RuntimeException("Roadlink timestamp no available because ModifiedAt was not defined.") )).getMillis
 }
