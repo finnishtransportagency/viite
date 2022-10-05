cf8d327f43ec7a5647caea35da30ba6e62da77e2
a_sitowise <ari.orre@sitowise.com>
2022-10-05 14:47:41 +0300

    VIITE-2860 Fix LocationInputParser for kgv linkId

--- viite-UI/src/utils/LocationInputParser.js
+++ viite-UI/src/utils/LocationInputParser.js
@@ -4,13 +4,14 @@
     var coordinateRegex = /^\s*(\d+)\s*,\s*(\d+)\s*$/;
     var wildLetterRegex = /^(\s*[*]*[A-Za-zÀ-ÿ].*)/;
     var roadNumberRegex = /^\s*\d*\s*\d*\s*\d*\s*\d+$/;
+    const linkIdRegex   = /^(\w+-\w+-\w+-\w+-\w+:\d+)$/;
 
     var matchedCoordinates = input.match(coordinateRegex);
     if (matchedCoordinates) {
       return parseCoordinates(matchedCoordinates);
     } else if (input.match(wildLetterRegex)) {
       return {type: 'street', search: input};
-    } else if (input.match(roadNumberRegex)) {
+    } else if (input.match(roadNumberRegex) || input.match(linkIdRegex)) {
       return {type: 'road', search: input};
     } else {
       return {type: 'invalid'};
