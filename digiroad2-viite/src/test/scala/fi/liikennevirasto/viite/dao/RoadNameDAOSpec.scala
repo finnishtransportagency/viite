4097d48075441f4830404008a5a344e5a7e8820e
Lauri Litmanen <lauri.litmanen@sitowise.com>
2022-11-22 14:14:42 +0200

    VIITE-2835 [RoadAddressBrowser] add test for road name query

--- digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/RoadNameDAOSpec.scala
+++ digiroad2-viite/src/test/scala/fi/liikennevirasto/viite/dao/RoadNameDAOSpec.scala
@@ -53,5 +53,74 @@ class RoadNameDAOSpec extends FunSuite with Matchers {
     }
   }
 
+  test("Test fetchRoadNamesForRoadAddressBrowser when situation date is equal to the end date of the history road name and start date of the current road name, then return the current road name with the correct ely number") {
+    /**
+      *                                               Ely & Road name changes
+      *                          TIME                 v
+      * ------------------------------------------------------------->
+      * 1990                                    2022-12-15
+      *
+      *                       old name
+      *                       ely 1                       new name
+      *                       history rw                  ely 9
+      *  -------------------------------------------->    new rw
+      *                                               --------------->
+      */
 
+
+    runWithRollback {
+      val rwHistoryStartDate = "1990-01-01"
+      val rwHistoryEndDate = "2022-12-14"
+      val rwCurrentStartDate = "2022-12-15"
+      val roadNameStartAndEndDate = "2022-12-15"
+      val validFrom = "2022-01-01"
+      val roadNumber = 99
+      val roadPartNumber = 1
+      val roadwayNumber = Sequences.nextRoadwayNumber
+      val ely1 = 1L
+      val ely9 = 9L
+      val situationDate = "2022-12-15"
+      val situationDateDayBeforeChanges = "2022-12-14"
+
+      // ely changes from 1 to 9
+      val historyRoadway = Roadway(Sequences.nextRoadwayId, roadwayNumber, roadNumber, roadPartNumber, AdministrativeClass.State, Track.Combined, Discontinuity.EndOfRoad, 0L, 1000L, false, DateTime.parse(rwHistoryStartDate),Some(DateTime.parse(rwHistoryEndDate)), "test", Some("oldName"),ely1, validFrom = DateTime.parse(validFrom), validTo = None)
+      val currentRoadway = Roadway(Sequences.nextRoadwayId, roadwayNumber, roadNumber, roadPartNumber, AdministrativeClass.State, Track.Combined, Discontinuity.EndOfRoad, 0L, 1000L, false, DateTime.parse(rwCurrentStartDate), None, "test", Some("newName"),ely9, validFrom = DateTime.parse(validFrom), validTo = None)
+
+      // create roadways
+      roadwayDAO.create(
+        Seq(
+          historyRoadway,
+          currentRoadway
+        )
+      )
+
+      // road name changes from oldName -> newName
+      val historyRoadName =  RoadName(Sequences.nextRoadNameId, roadNumber, "oldName", Some(DateTime.parse(rwHistoryStartDate)), Some(DateTime.parse(roadNameStartAndEndDate)), Some(DateTime.parse(validFrom)), None, "test")
+      val currentRoadName =  RoadName(Sequences.nextRoadNameId, roadNumber, "newName", Some(DateTime.parse(roadNameStartAndEndDate)), None, Some(DateTime.parse(validFrom)), None, "test")
+
+      // create road names
+      RoadNameDAO.create(
+        Seq(
+          historyRoadName,
+          currentRoadName
+        )
+      )
+
+      // result for situation date after changes
+      val res = RoadNameDAO.fetchRoadNamesForRoadAddressBrowser(Some(situationDate), None, Some(roadNumber), None, None)
+      res.size should be (1)
+      res.head shouldBe a [RoadNameForRoadAddressBrowser]
+      res.head.ely should be (9)
+      res.head.roadName should be ("newName")
+      println(res)
+
+      // result for history situation date
+      val res2 = RoadNameDAO.fetchRoadNamesForRoadAddressBrowser(Some(situationDateDayBeforeChanges), None, Some(roadNumber), None, None)
+      res2.size should be (1)
+      res2.head shouldBe a [RoadNameForRoadAddressBrowser]
+      res2.head.ely should be (1)
+      res2.head.roadName should be ("oldName")
+      println(res2)
+    }
+  }
 }
