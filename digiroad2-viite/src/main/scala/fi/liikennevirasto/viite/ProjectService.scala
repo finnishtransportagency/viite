08d327c4cdd20511c78537bbb1ba1473c4a66643
ao-sitowise <70687511+ao-sitowise@users.noreply.github.com>
2022-12-01 15:02:26 +0200

    VIITE-2535 Fix re-fetch to reserverd road to project

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectService.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectService.scala
@@ -233,10 +233,15 @@ class ProjectService(roadAddressService: RoadAddressService, roadLinkService: Ro
       }
   }
 
+  /* Return if road parts are reservable.
+  *  If road part is not reserved ProjectReservedPart
+  *  If road part is already reserved return error message
+  *  If road part is already reserved for the projectId return ProjectReservedPart
+  * */
   def checkRoadPartsReservable(roadNumber: Long, startPart: Long, endPart: Long, projectId: Long): Either[String, (Seq[ProjectReservedPart], Seq[ProjectReservedPart])] = {
     (startPart to endPart).foreach { part =>
       projectReservedPartDAO.fetchProjectReservedPart(roadNumber, part) match {
-        case Some(name) => return Left(s"Tie $roadNumber osa $part ei ole vapaana projektin alkupäivämääränä. Tieosoite on jo varattuna projektissa: $name.")
+        case Some((id,name)) => if (id != projectId) return Left(s"Tie $roadNumber osa $part ei ole vapaana projektin alkupäivämääränä. Tieosoite on jo varattuna projektissa: $name.")
         case _ =>
       }
       val projectsWithCommonJunctions = projectReservedPartDAO.fetchProjectReservedJunctions(roadNumber, part, projectId)
