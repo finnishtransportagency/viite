8528f323d799d1e03451416b8d64ecfef6ee7c96
a_sitowise <ari.orre@sitowise.com>
2022-12-16 15:59:10 +0200

    VIITE-2879 fix message ordering

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/package.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/package.scala
@@ -75,11 +75,11 @@ package object viite {
   val ErrorWithNewAction = "Uusi-toimenpidettä ei voi tallentaa, koska se koskettaa toisen projektin käytössä olevaa tielinkkiä."
   //VIITE-453 Not implemented yet (2)
   val SharedLinkIdsExistMessage = "Linkillä on voimassa oleva tieosoite tämän projektin alkupäivämäärällä."
-  val ContactViiteSupportMessage = "\nOta yhteys Viite tukeen."
-  val UnsuccessfulRecalculationMessage = "Etäisyysarvojen laskenta epäonnistui."
-  val ContinuousAddressCapErrorMessage = UnsuccessfulRecalculationMessage + " Tieosoitteet eivät muodostu jatkuvaksi." + ContactViiteSupportMessage
-  val NegativeLengthErrorMessage       = UnsuccessfulRecalculationMessage + " Linkille %s muodostuu negatiivinen pituus." + ContactViiteSupportMessage
-  val LengthMismatchErrorMessage       = UnsuccessfulRecalculationMessage + " Linkin %s uusi pituus eroaa yli sallitun rajan (%d) vanhaan pituuteen verrattuna." + ContactViiteSupportMessage
+  val ContactViiteSupportMessage = "Ota yhteys Viite tukeen."
+  val UnsuccessfulRecalculationMessage = "Etäisyysarvojen laskenta epäonnistui. " + ContactViiteSupportMessage
+  val ContinuousAddressCapErrorMessage = UnsuccessfulRecalculationMessage + "\nTieosoitteet eivät muodostu jatkuvaksi."
+  val NegativeLengthErrorMessage       = UnsuccessfulRecalculationMessage + "\nLinkille %s muodostuu negatiivinen pituus."
+  val LengthMismatchErrorMessage       = UnsuccessfulRecalculationMessage + "\nLinkin %s uusi pituus eroaa yli sallitun rajan (%d) vanhaan pituuteen verrattuna."
 
   val NoContinuityCodesAtEndMessage = "Tieosan lopusta puuttuu jatkuvuuskoodi."
   val ConnectedDiscontinuousMessage = "Jatkuva tielinkki on merkitty epäjatkuvaksi, korjaa jatkuu-koodi."
