f4992cca5ff705c10af97fcd078f724fb32bac7d
Marjukka Kokkonen <36668823+marjukka-kokkonen-sitowise@users.noreply.github.com>
2023-01-17 11:14:47 +0200

    Code documentation (#1390)
    * Documenting some geometry functions
    * More articulate aws deploy commentaries
    * (nil) Case of accronym letters :)

--- aws/aws-deploy-prod.sh
+++ aws/aws-deploy-prod.sh
@@ -5,14 +5,14 @@
 #
 set -e
 
-echo "Logging in to AWS ECR with Docker"
+echo "Logging in to both dev/QA, and Prod AWS ECRs with Docker"
 aws ecr get-login-password --profile vaylaapp --region eu-west-1 | docker login --username AWS --password-stdin 783354560127.dkr.ecr.eu-west-1.amazonaws.com
 aws ecr get-login-password --profile vaylaapp --region eu-west-1 | docker login --username AWS --password-stdin 434599271542.dkr.ecr.eu-west-1.amazonaws.com
-echo " Pull QA image"
+echo "Pull QA image"
 docker pull 783354560127.dkr.ecr.eu-west-1.amazonaws.com/viite-qa:latest
-echo "Tagging QA image to Prod image"
+echo "Tagging QA image into the Prod image"
 docker tag 783354560127.dkr.ecr.eu-west-1.amazonaws.com/viite-qa:latest 434599271542.dkr.ecr.eu-west-1.amazonaws.com/viite-prod:latest
-echo "Pushing the tagged image to prod ecr repository"
+echo "Push the tagged image to Prod ECR repository"
 docker push 434599271542.dkr.ecr.eu-west-1.amazonaws.com/viite-prod:latest
 echo "Done."
 echo "Ask centralized service to update the service (notes on how to do that are in the readme)"
