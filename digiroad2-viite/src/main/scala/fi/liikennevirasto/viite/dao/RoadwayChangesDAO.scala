4bed54907d4773552013195f2fa697e19657cdcb
Valtteri Vistiaho <valtteri.vistiaho@sitowise.com>
2023-02-03 13:42:46 +0200

    VIITE-2961 fix grammar

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/RoadwayChangesDAO.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/dao/RoadwayChangesDAO.scala
@@ -599,7 +599,7 @@ SELECT
                    |  AND rn.valid_to IS NULL
                    |  AND rn.start_date <= p.start_date
                    |  -- End date should be null if the change is not a termination (5).
-                   |  -- If the road is terminated, the end date is the same as the roads' end date (if the whole road was terminated in this project) or null if the road name's start date is earlier than the project's start date
+                   |  -- If the road is terminated, the end date is the same as the end date of the road  (if the whole road was terminated in this project) or null if the start date of the road name start date is earlier than the start date of the project
                    |  AND ((rc.change_type != 5 and rn.end_date IS null) OR (rc.change_type = 5 and (rn.end_date = (p.start_date - INTERVAL '1 DAY') or (rn.end_date is null and rn.start_date < p.start_date))))
                    """.stripMargin
       val filteredQuery = queryFilter(query)
