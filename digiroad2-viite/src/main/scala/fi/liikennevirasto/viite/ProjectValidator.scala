7f82759e9cd12f1922f3ce263ccaaf2b5b9aa391
ao-sitowise <70687511+ao-sitowise@users.noreply.github.com>
2023-02-15 14:26:26 +0200

    VIITE-2970 Fix head of empty list in ProjectValidator

--- digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectValidator.scala
+++ digiroad2-viite/src/main/scala/fi/liikennevirasto/viite/ProjectValidator.scala
@@ -791,7 +791,7 @@ class ProjectValidator {
                                              errorLinks   : Seq[ProjectLink] = Seq()
                                             ): Option[ValidationErrorDetails] = {
       if (roadPartLinks.isEmpty) {
-        error(project.id, ValidationErrorList.TrackGeometryLengthDeviation, errorLinks.head.linkId)(errorLinks)
+        error(project.id, ValidationErrorList.TrackGeometryLengthDeviation)(errorLinks)
       } else {
         val sortedRoadPartLinks  = roadPartLinks.sortBy(pl => (pl.track.value, pl.startAddrMValue))
         val sortedTrackInterval1 = getTrackInterval(sortedRoadPartLinks, Track.LeftSide)
