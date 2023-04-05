a9061b22ce901473ed1454ea91fc1409dc513208
LauriLitmanenSW <71322170+LauriLitmanenSW@users.noreply.github.com>
2023-04-05 08:21:15 +0300

    VIITE-1685 dont show project and node buttons to view mode users (#1461)
    * VIITE-1685 don't show project and node buttons to view mode users

--- viite-UI/src/view/LinkPropertyForm.js
+++ viite-UI/src/view/LinkPropertyForm.js
@@ -1,5 +1,5 @@
 (function (root) {
-  root.LinkPropertyForm = function (selectedLinkProperty, roadNamingTool, projectListModel, roadAddressBrowser, roadAddressChangesBrowser) {
+  root.LinkPropertyForm = function (selectedLinkProperty, roadNamingTool, projectListModel, roadAddressBrowser, roadAddressChangesBrowser, startupParameters) {
     var selectionType = ViiteEnumerations.SelectionType;
     var decodedAttributes = [
       {
@@ -270,16 +270,27 @@
     var addActionButtons = function () {
       var rootElement = $('#feature-attributes');
       rootElement.empty();
-      var emptyFormDiv =
-        '<p class="form form-horizontal"></p>' +
-        '<div class="form-initial-state" id="emptyFormDiv">' +
-        '<button id="formProjectButton" class="action-mode-btn btn btn-block btn-primary">Tieosoiteprojektit</button>' +
-        '<button id="formNameToolButton" class="open-tool-mode-btn btn btn-block btn-primary" style="margin-top: 5px;">Tiennimen ylläpito</button>' +
-        '<button id="formNodesAndJunctionsButton" class="open-tool-mode-btn btn btn-block btn-primary" style="margin-top: 5px;">Solmut ja liittymät</button>' +
-        '<button id="formRoadAddressBrowserButton" class="open-tool-mode-btn btn btn-block btn-primary" style="margin-top: 5px;">Tieosoitteiden katselu</button>' +
-        '<button id="formRoadAddressChangesBrowserButton" class="open-tool-mode-btn btn btn-block btn-primary" style="margin-top: 5px;">Tieosoitemuutosten katselu</button>' +
-        '</div>';
-      rootElement.append(emptyFormDiv);
+
+      var projectModeButton =  '<button id="formProjectButton" class="action-mode-btn btn btn-block btn-primary">Tieosoiteprojektit</button>';
+      var roadNameToolButton = '<button id="formNameToolButton" class="open-tool-mode-btn btn btn-block btn-primary" style="margin-top: 5px;">Tiennimen ylläpito</button>';
+      var nodesAndJunctionsButton = '<button id="formNodesAndJunctionsButton" class="open-tool-mode-btn btn btn-block btn-primary" style="margin-top: 5px;">Solmut ja liittymät</button>';
+      var roadAddressBrowserButton = '<button id="formRoadAddressBrowserButton" class="open-tool-mode-btn btn btn-block btn-primary" style="margin-top: 5px;">Tieosoitteiden katselu</button>';
+      var roadAddressChangesBrowserButton = '<button id="formRoadAddressChangesBrowserButton" class="open-tool-mode-btn btn btn-block btn-primary" style="margin-top: 5px;">Tieosoitemuutosten katselu</button>';
+
+      var toolButtonsDiv =
+          $('<div class="form-initial-state" id="emptyFormDiv">' +
+              nodesAndJunctionsButton +
+              roadAddressBrowserButton +
+              roadAddressChangesBrowserButton +
+          '</div>');
+
+      // add buttons for the view mode tools
+      rootElement.append(toolButtonsDiv);
+
+      // if the user has "viite" role then add the buttons for project mode and road name tool
+      if (_.includes(startupParameters.roles, 'viite'))
+        toolButtonsDiv.prepend(projectModeButton + roadNameToolButton);
+
       $('[id=formProjectButton]').click(function () {
         if (applicationModel.isProjectOpen()) {
           new ModalConfirm("Projektin muokkaus on kesken. Tallenna muutokset ja/tai poistu Peruuta-painikkeella.");
@@ -288,18 +299,22 @@
         }
         return false;
       });
+
       $('[id=formNameToolButton]').click(function () {
         roadNamingTool.toggle();
         return false;
       });
+
       $('[id=formNodesAndJunctionsButton]').click(function () {
         eventbus.trigger('nodesAndJunctions:open');
         return false;
       });
+
       $('[id=formRoadAddressBrowserButton]').click(function () {
         roadAddressBrowser.toggle();
         return false;
       });
+
       $('[id=formRoadAddressChangesBrowserButton]').click(function () {
         roadAddressChangesBrowser.toggle();
         return false;
