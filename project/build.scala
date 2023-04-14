c661c2f633652c702384eccce7684a28b3422ff4
Marjukka Kokkonen <36668823+marjukka-kokkonen-sitowise@users.noreply.github.com>
2023-04-14 16:32:26 +0300

    Viite 3009 library updates  logback  merge strategy (#1476)
    VIITE-3009 Logback+slf4j mergeStrategy, to avoid duplicates

--- project/build.scala
+++ project/build.scala
@@ -266,6 +266,7 @@ object Digiroad2Build extends Build {
       case x if x.endsWith("mime.types") => MergeStrategy.last
       case PathList("META-INF", "io.netty.versions.properties") => MergeStrategy.discard
       case PathList("META-INF", "maven", "com.fasterxml.jackson.core", "jackson-core", _*) => MergeStrategy.discard
+      case x if x.endsWith("module-info.class") => MergeStrategy.discard // for logback, and slf4j-api
       case x => old(x)
     } }
   )
