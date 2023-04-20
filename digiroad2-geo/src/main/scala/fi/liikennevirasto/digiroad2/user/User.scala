796aa03da45347ab2bdc78cc4069c36670266852
LauriLitmanen <lauri.litmanen@sitowise.com>
2023-04-20 14:40:54 +0300

    VIITE-3016 remove obsolete configuration from service user table and codebase
    municipalityNumber and authorizedMunicipalities are no longer used.

--- digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/user/User.scala
+++ digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/user/User.scala
@@ -4,13 +4,10 @@ case class Configuration(
                         zoom: Option[Long] = None,
                         east: Option[Long] = None,
                         north: Option[Long] = None,
-                        municipalityNumber: Option[Int]  = None,
-                        authorizedMunicipalities: Set[Int] = Set(),
                         roles: Set[String] = Set(),
                         authorizedElys: Set[Int] = Set()
                         )
 case class User(id: Long, username: String, configuration: Configuration) {
-  def hasWriteAccess() = !isViewer()
 
   def isViewer() = configuration.roles(Role.Viewer)
 
@@ -26,17 +23,6 @@ case class User(id: Long, username: String, configuration: Configuration) {
     id == 0
   }
 
-  def hasEarlyAccess(): Boolean = {
-    configuration.roles(Role.Premium) || configuration.roles(Role.Operator)
-  }
-
-  def isAuthorizedToRead(municipalityCode: Int): Boolean = isAuthorizedFor(municipalityCode) || isViewer()
-
-  def isAuthorizedToWrite(municipalityCode: Int): Boolean = isAuthorizedFor(municipalityCode)
-
-  private def isAuthorizedFor(municipalityCode: Int): Boolean =
-    isOperator() || configuration.authorizedMunicipalities.contains(municipalityCode)
-
   def getAuthorizedElys : Set[Int] = {
     configuration.authorizedElys
   }
@@ -44,8 +30,6 @@ case class User(id: Long, username: String, configuration: Configuration) {
 
 object Role {
   val Operator = "operator"
-  val Administrator = "administrator"
-  val Premium = "premium"
   val Viewer = "viewer"
   val ViiteUser = "viite"
 }
