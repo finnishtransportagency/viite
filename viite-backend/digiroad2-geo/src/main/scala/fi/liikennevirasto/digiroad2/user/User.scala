5f0cd755da0df1b436b47c068b5ba20fc5c72cdd
Marjukka Kokkonen <marjukka.kokkonen@sitowise.com>
2023-05-23 13:53:09 +0300

    VIITE-3037 moved backend modules inside viite-backend folder, and gatling under viite-integrationtest folder

---
+++ viite-backend/digiroad2-geo/src/main/scala/fi/liikennevirasto/digiroad2/user/User.scala
@@ -0,0 +1,35 @@
+package fi.liikennevirasto.digiroad2.user
+
+case class Configuration(
+                        zoom: Option[Long] = None,
+                        east: Option[Long] = None,
+                        north: Option[Long] = None,
+                        roles: Set[String] = Set(),
+                        authorizedElys: Set[Int] = Set()
+                        )
+case class User(id: Long, username: String, configuration: Configuration) {
+
+  def isViewer() = configuration.roles(Role.Viewer)
+
+  def isViiteUser(): Boolean = configuration.roles(Role.ViiteUser)
+
+  def hasViiteWriteAccess(): Boolean = configuration.roles(Role.ViiteUser)
+
+  def isOperator(): Boolean = {
+    configuration.roles(Role.Operator)
+  }
+
+  def isNotInViite(): Boolean = {
+    id == 0
+  }
+
+  def getAuthorizedElys : Set[Int] = {
+    configuration.authorizedElys
+  }
+}
+
+object Role {
+  val Operator = "operator"
+  val Viewer = "viewer"
+  val ViiteUser = "viite"
+}
