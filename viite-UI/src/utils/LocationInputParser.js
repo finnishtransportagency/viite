8446a5cae97d6d4f3f073a5d4b78bb87bbba4b6a
LauriLitmanen <lauri.litmanen@sitowise.com>
2024-07-01 15:59:34 +0300

    VIITE-3114 upgrade mocha library
     and switch to server-side testing (previously browser-side) as browser-side not needed at the moment.
    
     Get rid of chai as it is obsolete library for the (small amount of) UI tests that Viite currently has.

--- viite-UI/src/utils/LocationInputParser.js
+++ viite-UI/src/utils/LocationInputParser.js
@@ -1,5 +1,15 @@
 /* eslint-disable prefer-named-capture-group */
-(function (root) {
+(function (root, factory) {
+  // UMD (Universal Module Definition) so the code is compatible with both Node.js and browser environments
+  if (typeof module === 'object' && module.exports) {
+    // Node.js environment
+    module.exports = factory(require('lodash'));
+  } else {
+    // Browser environment
+    root.LocationInputParser = factory(root._);
+  }
+}(this, function (_) {
+
   var parse = function (input) {
     var coordinateRegex = /^\s*(\d+)\s*,\s*(\d+)\s*$/;
     var wildLetterRegex = /^(\s*[*]*[A-Za-zÀ-ÿ].*)/;
@@ -22,7 +32,7 @@
     return {type: 'coordinate', lat: _.parseInt(coordinates[1]), lon: _.parseInt(coordinates[2])};
   };
 
-  root.LocationInputParser = {
+  return {
     parse: parse
   };
-}(window));
+}));
