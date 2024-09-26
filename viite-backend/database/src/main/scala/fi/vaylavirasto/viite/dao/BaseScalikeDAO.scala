f0695f3e5e99e5bf6b4df8d353ee30ebecda1c51
jouni.johansson <jouni.johansson@sitowise.com>
2024-09-26 08:57:10 +0300

    Viite-3094 Add logging to sql script run methods

--- viite-backend/database/src/main/scala/fi/vaylavirasto/viite/dao/BaseScalikeDAO.scala
+++ viite-backend/database/src/main/scala/fi/vaylavirasto/viite/dao/BaseScalikeDAO.scala
@@ -9,13 +9,25 @@ trait ScalikeJDBCBaseDAO {
   // Implicit session for database operations
   implicit val session: DBSession = AutoSession
 
+  // TODO Logging enabled for dev purposes
   // Method to run update queries
   def runUpdateToDb(updateQuery: SQL[Nothing, NoExtractor]): Int = {
+    logger.debug(s"Executing update SQL: ${updateQuery.statement}")
     updateQuery.update.apply()
   }
 
   // Method to run select queries
   def runSelectQuery[A](query: SQL[A, HasExtractor]): List[A] = {
-    query.list.apply()
+    logger.info(s"Executing select SQL: ${query.statement}")
+    try {
+      val results = query.list.apply()
+      logger.info(s"Select query returned ${results.size} results")
+      results
+    } catch {
+      case e: Exception =>
+        logger.error(s"Error executing query: ${e.getMessage}", e)
+        List.empty
+    }
   }
+
 }
