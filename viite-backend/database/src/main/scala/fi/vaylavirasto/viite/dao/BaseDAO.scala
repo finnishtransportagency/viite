608e5bf58fb158ce4012420659d75d40573c7e7e
jouni.johansson <jouni.johansson@sitowise.com>
2024-10-25 15:33:51 +0300

    Viite-3094 ScalikeJDBC base DAO trait and session handling - Modified BaseDAO.scala to have necessary helper methods wiht ScalikeJDBC - Created SessionProvider.scala to mimic the Slicks withDynSession implementation - Created GeometryDbUtilsScalike.scala file that has the db helper methods for geometry that are incorrectly in the PostGISDatabase file in the Slick version. - Modified DbUtils.scala to have ScalikeJDBC implementation

--- viite-backend/database/src/main/scala/fi/vaylavirasto/viite/dao/BaseDAO.scala
+++ viite-backend/database/src/main/scala/fi/vaylavirasto/viite/dao/BaseDAO.scala
@@ -1,25 +1,45 @@
 package fi.vaylavirasto.viite.dao
 
-import slick.driver.JdbcDriver.backend.Database.dynamicSession
-import slick.jdbc.StaticQuery.interpolation
+import scalikejdbc._
 import org.slf4j.{Logger, LoggerFactory}
+import fi.vaylavirasto.viite.postgis.SessionProvider.session // As this is imported here, it is available in all classes that extend this trait
 
-
-class BaseDAO {
+// Methods to run queries using ScalikeJDBC and session provider
+trait BaseDAO {
   protected def logger: Logger = LoggerFactory.getLogger(getClass)
 
-  /* OLD Slick 3.0.0 way to run direct SQL update queries. */
-  def runUpdateToDb(updateQuery: String): Int = {
-    sqlu"""#$updateQuery""".buildColl.toList.head //sqlu"""#$updateQuery""".execute
+  def runUpdateToDb(updateQuery: SQL[Nothing, NoExtractor]): Int = {
+    updateQuery.update.apply()
   }
 
-//  def queryTemplate[T](queryString: String): List[T] = {
-//    val query: DBIO[Seq[T]] = sql"""$queryString""".as[T]
-//    val f_result: Future[Seq[T]] = db.run(query)
-//
-//    var longValues: List[T] = List()
-//    // You use futures like this..?
-//    f_result.onSuccess { case longs => longValues = longs.toList }
-//    longValues
-//  }
+  def runSelectQuery[A](query: SQL[A, HasExtractor]): List[A] = {
+    query.list().apply()
+  }
+
+  def runSelectSingle[A](query: SQL[A, HasExtractor]): Option[A] = {
+    query.single().apply()
+  }
+
+  def runBatchUpdateToDb(query: SQL[Nothing, NoExtractor], batchParams: Seq[Seq[Any]]): List[Int] = {
+    query.batch(batchParams: _*).apply()
+  }
+
+  /**
+   * Run a select query and return the first result as an Option
+   * Example usage: runSelectSingleFirstOptionWithType[Long](query)
+   * @param query The query to run
+   * @param mapper A function to map the result to the desired type
+   * @tparam T The type of the result
+   * @return The first result as an Option
+   */
+  def runSelectSingleFirstOptionWithType[T](query: SQL[Nothing, NoExtractor])(implicit mapper: WrappedResultSet => T): Option[T] = {
+    query.map(mapper).single().apply()
+  }
+
+  // Implicit conversions for common types
+  implicit val longMapper: WrappedResultSet => Long = _.long(1)
+  implicit val stringMapper: WrappedResultSet => String = _.string(1)
+  //implicit val linkMapper: WrappedResultSet => Link = _.link(1)
+  // Add more implicit mappers as needed
+
 }
