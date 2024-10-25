016f7b67f1828a1ab3ff22d29579ea0aaf30b244
jouni.johansson <jouni.johansson@sitowise.com>
2024-10-25 15:33:51 +0300

    Viite-3094 ScalikeJDBC base DAO trait and session handling - Modified BaseDAO.scala to have necessary helper methods wiht ScalikeJDBC - Created SessionProvider.scala to mimic the Slicks withDynSession implementation - Created GeometryDbUtilsScalike.scala file that has the db helper methods for geometry that are incorrectly in the PostGISDatabase file in the Slick version. - Modified DbUtils.scala to have ScalikeJDBC implementation

--- viite-backend/database/src/main/scala/fi/vaylavirasto/viite/postgis/DbUtils.scala
+++ viite-backend/database/src/main/scala/fi/vaylavirasto/viite/postgis/DbUtils.scala
@@ -1,18 +1,15 @@
 package fi.vaylavirasto.viite.postgis
 
 //import fi.liikennevirasto.digiroad2.postgis.PostGISDatabase
-import slick.driver.JdbcDriver.backend.Database.dynamicSession
-import slick.jdbc.StaticQuery.interpolation
-
-import java.sql.Date
+import fi.vaylavirasto.viite.postgis.SessionProvider.session
+import scalikejdbc.{NoExtractor, SQL}
 
 /** BaseDAO functionality: duplicate for import/referencing problems.
-  * Trade usage to BaseDAO functions where ever possible. */
+ * Trade usage to BaseDAO functions where ever possible. */
 object DbUtils {
 
-  /* OLD Slick 3.0.0 way to run direct SQL update queries. */
-  def runUpdateToDb(updateQuery: String) = {
-    sqlu"""#$updateQuery""".buildColl.toList.head
+  def runUpdateToDb(updateQuery: SQL[Nothing, NoExtractor]): Int = {
+    updateQuery.update.apply()
   }
 
 }
