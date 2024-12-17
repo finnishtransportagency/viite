f8d70eaf98bb955aa725ce9fe4d1c4d5e882dd2f
jouni.johansson <jouni.johansson@sitowise.com>
2024-12-17 10:11:34 +0200

    VIITE-3094/VIITE-3215: - Modified getUser in PostGISUserProvider to return Option as it should - Modified PostGISUserProviderSpec to use ScalikeJDBC

--- viite-backend/database/src/test/scala/fi/vaylavirasto/viite/dao/PostGISUserProviderSpec.scala
+++ viite-backend/database/src/test/scala/fi/vaylavirasto/viite/dao/PostGISUserProviderSpec.scala
@@ -2,10 +2,10 @@ package fi.vaylavirasto.viite.dao
 
 import fi.liikennevirasto.digiroad2.user.Configuration
 import fi.vaylavirasto.viite.postgis.DbUtils.runUpdateToDb
-import fi.vaylavirasto.viite.postgis.PostGISDatabase
-import fi.vaylavirasto.viite.postgis.PostGISDatabase.runWithRollback
+import fi.vaylavirasto.viite.postgis.PostGISDatabaseScalikeJDBC.{runWithAutoCommit, runWithRollback, runWithTransaction}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import scalikejdbc.scalikejdbcSQLInterpolationImplicitDef
 
 class PostGISUserProviderSpec extends AnyFunSuite with Matchers {
 
@@ -18,8 +18,13 @@ class PostGISUserProviderSpec extends AnyFunSuite with Matchers {
     "When trying to find a specific user name and creating a user for that user name " +
     "Then getUser() should return 'None' before creating, and the created user after creating it.") {
     runWithRollback {
-      PostGISDatabase.withDynSession {
-        runUpdateToDb(s"""DELETE FROM service_user WHERE username = '${TestUserName.toLowerCase()}'""")
+      runWithAutoCommit {
+        runUpdateToDb(
+          sql"""
+               DELETE FROM service_user
+               WHERE username = ${TestUserName.toLowerCase()}
+               """
+        )
       }
       provider.getUser(TestUserName) shouldBe None
       provider.createUser(TestUserName, Configuration(north = Some(1000)))
