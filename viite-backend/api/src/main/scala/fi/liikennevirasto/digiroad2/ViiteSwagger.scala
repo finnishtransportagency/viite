6bfe53531a65fbefbb98dcbe709a762be2475265
LauriLitmanen <lauri.litmanen@sitowise.com>
2025-03-17 15:50:03 +0200

    VIITE-3182 fix swagger

--- viite-backend/api/src/main/scala/fi/liikennevirasto/digiroad2/ViiteSwagger.scala
+++ viite-backend/api/src/main/scala/fi/liikennevirasto/digiroad2/ViiteSwagger.scala
@@ -1,15 +1,15 @@
 package fi.liikennevirasto.digiroad2
 
 import org.scalatra.ScalatraServlet
-import org.scalatra.swagger.{ApiInfo, NativeSwaggerBase, Swagger}
+import org.scalatra.swagger.{ApiInfo, ContactInfo, LicenseInfo, NativeSwaggerBase, Swagger}
 
 class ResourcesApp(implicit val swagger: Swagger) extends ScalatraServlet with NativeSwaggerBase
 
 object ViiteApiInfo extends ApiInfo(title = "VIITE API",
   description = "Docs for VIITE API",
   termsOfServiceUrl = "",
-  contact = null,
-  license = null
+  contact = ContactInfo(name = "", url = "", email = ""),  // Empty but valid ContactInfo
+  license = LicenseInfo(name = "", url = "")  // Empty but valid LicenseInfo
 )
 
 class ViiteSwagger extends Swagger(Swagger.SpecVersion, "1.0.0", ViiteApiInfo)
