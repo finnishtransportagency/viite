def47e643917732c7fdf40778e0ad7cd58e7fd59
Marjukka Kokkonen <36668823+marjukka-kokkonen-sitowise@users.noreply.github.com>
2025-04-24 12:24:14 +0300

    VIITE-3417 ArealRoadMaintainer base trait (#1791)

---
+++ viite-backend/base/src/test/scala/fi/vaylavirasto/viite/model/ArealRoadMaintainerSpec.scala
@@ -0,0 +1,15 @@
+package fi.vaylavirasto.viite.model
+
+import fi.vaylavirasto.viite.util.ViiteException
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class ArealRoadMaintainerSpec extends AnyFunSuite with Matchers {
+
+  test("Test ArealRoadMaintainer: Cannot create a random ArealRoadMaintainer. Or even one with a proper name. Creation throws an exception.") {
+    intercept[Exception](ArealRoadMaintainer("asdf"))    shouldBe a[ViiteException]
+    intercept[Exception](ArealRoadMaintainer("Uusimaa")) shouldBe a[ViiteException]
+    intercept[Exception](ArealRoadMaintainer("1"))       shouldBe a[ViiteException]
+  }
+
+}
