d565cacbebe70666566758864ef450bcedc44fd1
LauriLitmanen <lauri.litmanen@sitowise.com>
2025-05-22 15:01:08 +0300

    VIITE-3015 relocate revision.properties file to fix deploy date not showing on UI regression bug

--- gruntfile.js
+++ gruntfile.js
@@ -253,7 +253,7 @@ module.exports = function (grunt) {
   grunt.registerTask('save_deploy_info',
     function () {
       var options = this.options({
-        file: 'viite-backend/conf/revision.properties'
+        file: 'viite-backend/base/src/main/resources/revision.properties'
       });
 
       var data = ('latestDeploy=' + grunt.template.today('dd-mm-yyyy HH:MM:ss'));
