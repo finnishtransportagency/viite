edcfca6652b21c2554e3b739c54ee7228fc79da7
Juhani Manninen <juhani.manninen@twoday.com>
2025-08-08 09:20:17 +0300

    VIITE-3541 Add button to hide background map

--- viite-UI/src/view/TileMapCollection.js
+++ viite-UI/src/view/TileMapCollection.js
@@ -1,79 +1,77 @@
 (function (root) {
   root.TileMapCollection = function () {
-    var layerConfig = {
-      // minResolution: ?,
-      // maxResolution: ?,
+    const layerConfig = {
       visible: false,
       extent: [-548576, 6291456, 1548576, 8388608]
     };
 
-    var propertyLayerConfig = {
+    const propertyLayerConfig = {
       maxResolution: 5,
       visible: true,
       extent: [-548576, 6291456, 1548576, 8388608]
     };
 
-    var sourceConfig = {
+    const sourceConfig = {
       cacheSize: 4096,
       projection: 'EPSG:3067',
       tileSize: [256, 256]
     };
 
-    var tileGridConfig = {
+    const tileGridConfig = {
       extent: [-548576, 6291456, 1548576, 8388608],
       origin: [-548576, 8388608],
       projection: 'EPSG:3067'
     };
 
-    var resolutionConfig = {
+    const resolutionConfig = {
       resolutions: [8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5]
     };
 
-    var aerialMapConfig = _.merge({}, sourceConfig, {
+    const aerialMapConfig = _.merge({}, sourceConfig, {
       url: 'wmts/maasto/1.0.0/ortokuva/default/ETRS-TM35FIN/{z}/{y}/{x}.jpg'
     });
 
-    var backgroundMapConfig = _.merge({}, sourceConfig, {
+    const backgroundMapConfig = _.merge({}, sourceConfig, {
       url: 'wmts/maasto/1.0.0/taustakartta/default/ETRS-TM35FIN/{z}/{y}/{x}.png'
     });
 
-    var propertyBorderMapConfig = _.merge({}, sourceConfig, {
+    const propertyBorderMapConfig = _.merge({}, sourceConfig, {
       url: 'wmts/kiinteisto/1.0.0/kiinteistojaotus/default/ETRS-TM35FIN/{z}/{y}/{x}.png'
     });
 
-    var terrainMapConfig = _.merge({}, sourceConfig, {
+    const terrainMapConfig = _.merge({}, sourceConfig, {
       url: 'wmts/maasto/1.0.0/maastokartta/default/ETRS-TM35FIN/{z}/{y}/{x}.png'
     });
 
-    var aerialMapLayer = new ol.layer.Tile(_.merge({
+    const aerialMapLayer = new ol.layer.Tile(_.merge({
       source: new ol.source.XYZ(_.merge({
         tileGrid: new ol.tilegrid.TileGrid(_.merge({}, tileGridConfig, resolutionConfig))
       }, aerialMapConfig))
     }, layerConfig));
     aerialMapLayer.set('name', 'aerialMapLayer');
 
-    var backgroundMapLayer = new ol.layer.Tile(_.merge({
+    const backgroundMapLayer = new ol.layer.Tile(_.merge({
       source: new ol.source.XYZ(_.merge({
         tileGrid: new ol.tilegrid.TileGrid(_.merge({}, tileGridConfig, resolutionConfig))
       }, backgroundMapConfig))
     }, layerConfig));
     backgroundMapLayer.set('name', 'backgroundMapLayer');
 
-    var propertyBorderLayer = new ol.layer.Tile(_.merge({
+    const propertyBorderLayer = new ol.layer.Tile(_.merge({
       source: new ol.source.XYZ(_.merge({
         tileGrid: new ol.tilegrid.TileGrid(_.merge({}, tileGridConfig, resolutionConfig))
       }, propertyBorderMapConfig))
     }, propertyLayerConfig));
     propertyBorderLayer.set('name', 'propertyBorderLayer');
 
-    var terrainMapLayer = new ol.layer.Tile(_.merge({
+    const terrainMapLayer = new ol.layer.Tile(_.merge({
       source: new ol.source.XYZ(_.merge({
         tileGrid: new ol.tilegrid.TileGrid(_.merge({}, tileGridConfig, resolutionConfig))
       }, terrainMapConfig))
     }, layerConfig));
     terrainMapLayer.set('name', 'terrainMapLayer');
 
-    var tileMapLayers = {
+    const tileMapLayers = {
       background: backgroundMapLayer,
       aerial: aerialMapLayer,
       terrain: terrainMapLayer,
@@ -86,10 +84,10 @@
       });
     };
 
-    var togglePropertyBorderVisibility = function (showPropertyBorder) {
+
+    const togglePropertyBorderVisibility = function (showPropertyBorder) {
       propertyBorderLayer.setVisible(showPropertyBorder);
     };
-
 
     selectMap('background');
     eventbus.on('tileMap:selected', selectMap);
