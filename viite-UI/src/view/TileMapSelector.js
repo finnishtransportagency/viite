3ab00660011559506437ccdc3569069ca3b3f216
JuhaniManninen <juhani.manninen@twoday.com>
2025-08-11 10:12:02 +0300

    VIITE-3541 Add button to hide background map (#1850)

--- viite-UI/src/view/TileMapSelector.js
+++ viite-UI/src/view/TileMapSelector.js
@@ -1,59 +1,86 @@
 (function (root) {
   root.TileMapSelector = function (container, applicationModel) {
-    var element =
-      '<div class="tile-map-selector">' +
-      '<ul>' +
-      '<li data-layerid="terrain" title="Maastokartta">Maastokartta</li>' +
-      '<li data-layerid="aerial" title="Ortokuvat">Ortokuvat</li>' +
-      '<li data-layerid="background" title="Taustakarttasarja" class="selected">Taustakarttasarja</li>' +
-      '</ul>' +
-      '<div class="property-boundaries-visible-wrapper">' +
-      '<div class="checkbox">' +
-      '<label><input type="checkbox" name="propertyBoundariesVisible" value="propertyBoundariesVisible"  id="propertyBoundariesVisibleCheckbox">Näytä kiinteistörajat</label>' +
-      '</div>' +
-      '</div>' +
-      '<div class="noroadaddress-visible-wrapper">' +
-      '<div class="checkbox">' +
-      '<label><input type="checkbox" name="unAddressedRoadsVisible" value="unAddressedRoadsVisible" checked="false" id="unAddressedRoadsVisibleCheckbox">Näytä tieosoitteettomat-linkit</label>' +
-      '</div>' +
-      '</div>' +
-      '<div class="underconstruction-visible-wrapper">' +
-      '<div class="checkbox">' +
-      '<label><input type="checkbox" name="underConstructionVisible" value="underConstructionVisible" checked="true" id="underConstructionVisibleCheckbox">Näytä rakenteilla-linkit</label>' +
-      '</div>' +
-      '</div>' +
+    const element = `
+      <div class="tile-map-selector">
+        <ul>
+          <li data-layerid="terrain" title="Maastokartta">Maastokartta</li>
+          <li data-layerid="aerial" title="Ortokuvat">Ortokuvat</li>
+          <li data-layerid="background" title="Taustakarttasarja" class="selected">Taustakarttasarja</li>
+          <li data-layerid="none" title="Piilota kartta">Piilota kartta</li>
+        </ul>
 
-      '<div class="roads-visible-wrapper">' +
-      '<div class="checkbox">' +
-      '<label><input type="checkbox" name="roadsVisible" value="roadsVisible" checked="true" id="roadsVisibleCheckbox">Näytä tieosoiteverkko</label>' +
-      '</div>' +
-      '</div>' +
-      '</div>';
+        <div class="property-boundaries-visible-wrapper">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" id="propertyBoundariesVisibleCheckbox">
+              Näytä kiinteistörajat
+            </label>
+          </div>
+        </div>
+
+        <div class="noroadaddress-visible-wrapper">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" id="unAddressedRoadsVisibleCheckbox">
+              Näytä tieosoitteettomat-linkit
+            </label>
+          </div>
+        </div>
+
+        <div class="underconstruction-visible-wrapper">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" id="underConstructionVisibleCheckbox" checked>
+              Näytä rakenteilla-linkit
+            </label>
+          </div>
+        </div>
+
+        <div class="roads-visible-wrapper">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" id="roadsVisibleCheckbox" checked>
+              Näytä tieosoiteverkko
+            </label>
+          </div>
+        </div>
+      </div>
+    `;
 
     container.append(element);
-    container.find('li').click(function (event) {
+
+    // Handle tile map selection
+    container.find('li[data-layerid]').on('click', event => {
       container.find('li.selected').removeClass('selected');
-      var selectedTileMap = $(event.target);
+      const selectedTileMap = $(event.target);
       selectedTileMap.addClass('selected');
-      eventbus.trigger('tileMap:selected', selectedTileMap.attr('data-layerid'));
+      eventbus.trigger('tileMap:selected', selectedTileMap.data('layerid'));
     });
-    $('#propertyBoundariesVisibleCheckbox').change(function () {
+
+
+    // Toggle map visibility
+    $('#toggleMapVisibility').on('click', function () {
+      const map = $('#map');
+      map.toggle();
+      const isHidden = map.is(':hidden');
+      $(this).text(isHidden ? 'Näytä kartta' : 'Piilota kartta');
+    });
+
+    $('#propertyBoundariesVisibleCheckbox').on('change', function () {
       eventbus.trigger('tileMap:togglepropertyBorder', this.checked);
     });
-    $('#unAddressedRoadsVisibleCheckbox').change(function () {
+    $('#unAddressedRoadsVisibleCheckbox').on('change', function () {
       eventbus.trigger('unAddressedRoads:toggleVisibility', this.checked);
-      eventbus.trigger("unAddressedProjectRoads:toggleVisibility", this.checked);
+      eventbus.trigger('unAddressedProjectRoads:toggleVisibility', this.checked);
     });
-    $('#underConstructionVisibleCheckbox').change(function () {
+    $('#underConstructionVisibleCheckbox').on('change', function () {
       eventbus.trigger('underConstructionRoads:toggleVisibility', this.checked);
-      eventbus.trigger("underConstructionProjectRoads:toggleVisibility", this.checked);
+      eventbus.trigger('underConstructionProjectRoads:toggleVisibility', this.checked);
     });
-    $('#roadsVisibleCheckbox').change(function () {
+    $('#roadsVisibleCheckbox').on('change', function () {
       applicationModel.toggleRoadVisibility();
       eventbus.trigger('linkProperty:visibilityChanged');
       eventbus.trigger('roadAddressProject:visibilityChanged');
-
     });
-
   };
 }(this));
