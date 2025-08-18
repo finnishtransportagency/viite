1804f80957fbcb9a2b418e6d4f632c17cd4c6de0
Juhani Manninen <juhani.manninen@twoday.com>
2025-08-18 15:41:55 +0300

    VIITE-3563 Add max duration for spinner

--- viite-UI/src/model/ApplicationModel.js
+++ viite-UI/src/model/ApplicationModel.js
@@ -197,17 +197,28 @@
       }
     };
 
-    const addSpinner = function () {
-      jQuery('.container').append(
-          $('<div></div>')
-              .addClass("spinner-overlay modal-overlay")
-              .append($('<div></div>').addClass("spinner"))
-      );
+    const addSpinner = function() {
+      // Prevent duplicate spinners
+      if ($('.spinner-overlay').length) return;
+
+      const $spinnerOverlay = $('<div></div>')
+          .addClass('spinner-overlay modal-overlay')
+          .append($('<div></div>').addClass('spinner'));
+
+      $('.container').append($spinnerOverlay);
+
+      // Auto-remove after 8 seconds
+      setTimeout(() => {
+        if ($spinnerOverlay.is(':visible')) {
+          removeSpinner();
+        }
+      }, 8000);
     };
 
-    const removeSpinner = function () {
-      jQuery('.spinner-overlay').remove();
+    const removeSpinner = function() {
+      $('.spinner-overlay').remove();
     };
+
 
     eventbus.on("userData:fetched", function (userData) {
       sessionUsername = userData.userName;
