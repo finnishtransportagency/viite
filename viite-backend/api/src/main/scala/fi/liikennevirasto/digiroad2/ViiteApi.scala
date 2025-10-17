77e56743336c98f491cd02f2dd08ab702b5c1b06
Erkko Pellinen <erkko.pellinen@twoday.com>
2025-10-17 09:05:31 +0300

    Removed reference to ELY from project change table target part

--- viite-backend/api/src/main/scala/fi/liikennevirasto/digiroad2/ViiteApi.scala
+++ viite-backend/api/src/main/scala/fi/liikennevirasto/digiroad2/ViiteApi.scala
@@ -1149,7 +1149,7 @@ class ViiteApi(val roadLinkService: RoadLinkService,           val KGVClient: Kg
     }
   }
 
-  private def mapChangeInfoToUI(change: RoadwayChangeSection) = {
+  private def mapSourceChangeInfoToUI(change: RoadwayChangeSection) = {
     Map(
       "roadNumber" -> change.roadNumber,
       "trackCode" -> change.trackCode,
@@ -1159,6 +1159,20 @@ class ViiteApi(val roadLinkService: RoadLinkService,           val KGVClient: Kg
       "administrativeClass" -> change.administrativeClass,
       "discontinuity" -> change.discontinuity,
       "ely" -> change.ely,
+      "elinvoimakeskus" -> change.roadMaintainer.map(rm => ArealRoadMaintainer.getELYOrElinvoimakeskusNumber(rm, elyContext = false))
+    )
+  }
+
+  private def mapTargetChangeInfoToUI(change: RoadwayChangeSection) = {
+    Map(
+      "roadNumber" -> change.roadNumber,
+      "trackCode" -> change.trackCode,
+      "startRoadPartNumber" -> change.startRoadPartNumber,
+      "endRoadPartNumber" -> change.endRoadPartNumber,
+      "addrMRange" -> change.addrMRange,
+      "administrativeClass" -> change.administrativeClass,
+      "discontinuity" -> change.discontinuity,
+      //     "ely" -> change.ely,
       "elinvoimakeskus" -> change.roadMaintainer.map(rm => ArealRoadMaintainer.getELYOrElinvoimakeskusNumber(rm, elyContext = false))
     )
   }
@@ -1184,8 +1198,8 @@ class ViiteApi(val roadLinkService: RoadLinkService,           val KGVClient: Kg
           "changeDate" -> project.changeDate,
           "changeInfoSeq" -> project.changeInfoSeq.map(changeInfo =>
             Map("changetype" -> changeInfo.changeType.value, "roadType" -> changeInfo.administrativeClass.asRoadTypeValue,
-              "discontinuity" -> changeInfo.discontinuity.value, "source" -> mapChangeInfoToUI(changeInfo.source),
-              "target" -> mapChangeInfoToUI(changeInfo.target), "reversed" -> changeInfo.reversed)))
+              "discontinuity" -> changeInfo.discontinuity.value, "source" -> mapSourceChangeInfoToUI(changeInfo.source),
+              "target" -> mapTargetChangeInfoToUI(changeInfo.target), "reversed" -> changeInfo.reversed)))
       ).getOrElse(None)
       Map("changeTable" -> changeTableData, "warningMessage" -> warningMessage)
     }
