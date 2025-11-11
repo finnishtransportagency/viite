9e194ce6600aa11662bd574bb2e4acf3a07919f1
Eino Korte <eino.korte@twoday.com>
2025-11-11 14:51:43 +0200

    hotfix 2

--- viite-backend/database/src/main/scala/fi/vaylavirasto/viite/dao/SequenceResetterDAO.scala
+++ viite-backend/database/src/main/scala/fi/vaylavirasto/viite/dao/SequenceResetterDAO.scala
@@ -1,19 +1,18 @@
 package fi.vaylavirasto.viite.dao
 
-import scalikejdbc.scalikejdbcSQLInterpolationImplicitDef
+import scalikejdbc._
 
 class SequenceResetterDAO extends BaseDAO {
 
   def resetSequenceToNumber(seqName: String, seqNumber: Long): Unit = {
-    runUpdateToDb(sql"""DROP SEQUENCE #$seqName """)
+    runUpdateToDb(sql"""DROP SEQUENCE IF EXISTS #$seqName""")
     runUpdateToDb(
       sql"""
-           CREATE SEQUENCE #$seqName
-           START WITH #$seqNumber
-           CACHE 20
-           INCREMENT BY 1
-           """
+        CREATE SEQUENCE #$seqName
+        START WITH $seqNumber
+        INCREMENT BY 1
+        CACHE 20
+      """
     )
   }
 }
-
