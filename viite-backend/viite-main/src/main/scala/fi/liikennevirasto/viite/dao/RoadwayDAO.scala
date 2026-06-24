ee0145b523a5764ec7bced88243c171d82547e5d
Juhani Manninen <juhani.manninen@twoday.com>
2026-06-24 10:16:26 +0300

    Revert VIITE-3962 validation for past dates

--- viite-backend/viite-main/src/main/scala/fi/liikennevirasto/viite/dao/RoadwayDAO.scala
+++ viite-backend/viite-main/src/main/scala/fi/liikennevirasto/viite/dao/RoadwayDAO.scala
@@ -1070,7 +1070,7 @@ class RoadwayDAO extends BaseDAO {
       SELECT DISTINCT ra.road_part_number
       FROM roadway ra
       WHERE road_number = $roadNumber AND valid_to IS NULL AND START_DATE <= $projectStartDate
-          AND (end_date IS NULL OR end_date >= $projectStartDate)
+          AND end_date IS NULL
           AND ra.road_part_number NOT IN (
               SELECT DISTINCT pl.road_part_number
               FROM project_link pl
