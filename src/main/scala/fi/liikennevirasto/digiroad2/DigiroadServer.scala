7d8745a7975fd0357506645f9b6019be32274ec3
JuhaniManninen <juhani.manninen@twoday.com>
2026-09-09 10:20:52 +0300

    Remove GZIP (#2076)

--- src/main/scala/fi/liikennevirasto/digiroad2/DigiroadServer.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/DigiroadServer.scala
@@ -1,6 +1,7 @@
 package fi.liikennevirasto.digiroad2
 
 import java.lang.management.ManagementFactory
+
 import fi.liikennevirasto.digiroad2.util.ViiteProperties
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import org.eclipse.jetty.client.api.Request
@@ -9,7 +10,6 @@ import org.eclipse.jetty.jmx.MBeanContainer
 import org.eclipse.jetty.proxy.ProxyServlet
 import org.eclipse.jetty.server._
 import org.eclipse.jetty.server.handler.ContextHandlerCollection
-import org.eclipse.jetty.server.handler.gzip.GzipHandler
 import org.eclipse.jetty.servlet.{DefaultServlet, ServletHolder}
 import org.eclipse.jetty.util.ssl.SslContextFactory
 import org.eclipse.jetty.webapp.WebAppContext
@@ -26,9 +26,7 @@ trait DigiroadServer {
     server.addBean(mbContainer)
     val handlers = Array(createViiteContext())
     handler.setHandlers(handlers.map(_.asInstanceOf[Handler]))
-    val gzipHandler = new GzipHandler()
-    gzipHandler.setHandler(handler)
-    server.setHandler(gzipHandler)
+    server.setHandler(handler)
     server.start()
     server.join()
   }
