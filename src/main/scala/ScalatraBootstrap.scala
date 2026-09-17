2f765500248a56e735c6f83eb30f6f5be2c0394b
Juhani Manninen <juhani.manninen@twoday.com>
2026-09-17 10:47:10 +0300

    Fix velho api

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -23,6 +23,7 @@ class ScalatraBootstrap extends LifeCycle {
     context.mount(new IntegrationApi(Digiroad2Context.roadAddressService, Digiroad2Context.roadNameService, swagger), "/api/viite/integration/*")
     context.mount(new ChangeApi(Digiroad2Context.roadAddressService, Digiroad2Context.nodesAndJunctionsService, swagger), "/api/viite/changes/*")
     context.mount(new SearchApi(Digiroad2Context.roadAddressService, swagger), "/api/viite/search/*")
+    context.mount(new VelhoApi, "/api/viite/velho/*")
     context.mount(new ViiteApi(Digiroad2Context.roadLinkService, Digiroad2Context.kgvRoadLinkClient,
       Digiroad2Context.roadAddressService, Digiroad2Context.projectService,
       Digiroad2Context.roadNameService, Digiroad2Context.nodesAndJunctionsService,
